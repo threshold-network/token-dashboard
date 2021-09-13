aa7d552cbecfbaf487577bb4b06cc60b78faed1b
George Weiler <georgejweiler@gmail.com>
2021-09-13 15:44:05 -0700

    add storybook configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Heading } from '@chakra-ui/react';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <div>
+        <Heading>Threshold Token Dashboard</Heading>
+      </div>
+    </ChakraProvider>
   );
 }
 
