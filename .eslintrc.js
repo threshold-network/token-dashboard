6f278f177965b962ce6259a579d7fa2dc257d5de
George Weiler <georgejweiler@gmail.com>
2021-09-14 13:04:20 -0700

    chore: Update eslint and prettier configs to use keep plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,32 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  root: true,
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint-config-keep",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+    "react/display-name": 0,
+    "no-invalid-this": 0,
+  },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-};
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-};
+  ...require("@keep-network/prettier-config-keep"),
+}
