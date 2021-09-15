626331f915e0037dfb5a7e9028f150a4831951a4
George Weiler <georgejweiler@gmail.com>
2021-09-15 12:06:01 -0700

    chore: Update chakra theme for badge component

--- src/theme.ts
+++ src/theme.ts
@@ -23,8 +23,12 @@ const colors = {
   },
 }
 
-const getColorFromProps = (props: any, weight: number) => {
-  return props?.colorScheme !== "brand"
+const getColorFromProps = (
+  props: any,
+  weight: number,
+  defaultColorScheme = "brand"
+) => {
+  return props?.colorScheme !== defaultColorScheme
     ? props?.theme?.colors[props?.colorScheme][weight]
     : null
 }
@@ -96,11 +100,38 @@ const Button = {
 const Badge = {
   defaultProps: {
     variant: "solid",
+    size: "md",
+    colorScheme: "brand",
+  },
+  sizes: {
+    sm: {
+      fontSize: "14px",
+      lineHeight: "16px",
+      px: "8px",
+      py: "4px",
+    },
+    md: {
+      fontSize: "16px",
+      lineHeight: "24px",
+      px: "12px",
+      py: "6px",
+    },
   },
   variants: {
-    solid: {
+    solid: (props: any) => ({
       color: "white",
+      backgroundColor: getColorFromProps(props, 500) || "black",
+    }),
+    outline: (props: any) => {
+      return {
+        boxShadow: `inset 0 0 0px 1px ${
+          getColorFromProps(props, 500) || "black"
+        }`,
+      }
     },
+  },
+  baseStyle: {
+    borderRadius: "full",
   },
 }
 
