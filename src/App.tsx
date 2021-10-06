b2bd7ca3d9a03ad9690fa3d888bf4f6889f33cdc
George Weiler <georgejweiler@gmail.com>
2021-10-06 14:41:08 -0700

    Clean up App.tsx and add boxshadow to wallet connection alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { FC } from "react"
-import { Alert, ChakraProvider, Container, Heading } from "@chakra-ui/react"
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react"
 import { Provider as ReduxProvider } from "react-redux"
-import { useWeb3React, Web3ReactProvider } from "@web3-react/core"
+import { Web3ReactProvider } from "@web3-react/core"
 import theme from "./theme"
 import reduxStore from "./store"
 import ModalRoot from "./components/Modal"
 import getLibrary from "./web3/library"
 import Navbar from "./components/Navbar"
-
-const ModalButton = () => {
-  const { active, account } = useWeb3React()
-  return (
-    <Alert mt={6}>
-      {active ? `You are connected to ${account}` : "You are not connected"}
-    </Alert>
-  )
-}
 
 const App: FC = () => {
   return (
@@ -26,7 +17,6 @@ const App: FC = () => {
           <Navbar />
           <Container maxW="6xl">
             <Heading>Threshold Token Dashboard?</Heading>
-            <ModalButton />
           </Container>
         </ChakraProvider>
       </ReduxProvider>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,13 @@
-import { FC } from "react"
-import { Box, Button, Container } from "@chakra-ui/react"
+import { FC, useState } from "react"
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Box,
+  Button,
+  CloseButton,
+  Container,
+} from "@chakra-ui/react"
 import { BiWalletAlt } from "react-icons/all"
 import { useModal } from "../../hooks/useModal"
 import { ModalType } from "../../enums"
@@ -11,10 +19,16 @@ import { Ethereum } from "../../static/icons/Ethereum"
 const Navbar: FC = () => {
   const { openModal } = useModal()
   const { account, active, chainId } = useWeb3React()
+  const [hideAlert, setHideAlert] = useState(false)
 
   return (
-    <Box bg="lightgray" p={4}>
-      <Container display="flex" justifyContent="flex-end" maxW="6xl">
+    <Box p={4}>
+      <Container
+        display="flex"
+        justifyContent="flex-end"
+        maxW="6xl"
+        position="relative"
+      >
         {active && account ? (
           <>
             <Button mr={4} variant="secondary" leftIcon={<Ethereum />}>
@@ -29,6 +43,25 @@ const Navbar: FC = () => {
           >
             Connect Wallet
           </Button>
+        )}
+        {!active && !hideAlert && (
+          <Alert
+            status="warning"
+            position="absolute"
+            w="400px"
+            top="55px"
+            right="16px"
+            boxShadow="md"
+          >
+            <AlertIcon />
+            <AlertDescription>Connect your wallet</AlertDescription>
+            <CloseButton
+              position="absolute"
+              right="8px"
+              top="8px"
+              onClick={() => setHideAlert(true)}
+            />
+          </Alert>
         )}
       </Container>
     </Box>
