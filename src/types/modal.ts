fec8942107ddd8dbddc15469511c6c88ed62a1ac
George Weiler <georgejweiler@gmail.com>
2021-10-07 10:18:17 -0700

    Fix types compile issues

--- src/types/modal.ts
+++ src/types/modal.ts
@@ -9,3 +9,23 @@ export const MODAL_TYPES: Record<ModalType, ElementType> = {
 export interface BaseModalProps {
   closeModal: () => void
 }
+
+export interface OpenModal {
+  payload: {
+    modalType: ModalType
+    props: any
+  }
+}
+
+export interface CloseModal {}
+
+export type ModalActionTypes = OpenModal | CloseModal
+
+export interface UseModal {
+  (): {
+    modalType: ModalType | null
+    modalProps: any
+    openModal: (type: ModalType, props?: any) => ModalActionTypes
+    closeModal: () => ModalActionTypes
+  }
+}
