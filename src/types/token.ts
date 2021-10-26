5cd131bec154b56e02fe2f64820bd3bd462c4cd3
George Weiler <georgejweiler@gmail.com>
2021-10-26 13:14:13 +0100

    Create a token context and token reducer to manage erc20 balance state

---
+++ src/types/token.ts
@@ -0,0 +1,38 @@
+import { Token } from "../enums"
+
+export interface ReduxTokenInfo {
+  loading: boolean
+  balance: number
+}
+
+export interface SetTokenBalanceActionPayload {
+  token: Token
+  balance: number
+}
+
+export interface SetTokenLoadingActionPayload {
+  token: Token
+  loading: boolean
+}
+
+export interface SetTokenBalance {
+  payload: SetTokenBalanceActionPayload
+}
+
+export interface SetTokenLoading {
+  payload: SetTokenLoadingActionPayload
+}
+
+export type TokenActionTypes = SetTokenBalance | SetTokenLoading
+
+export interface UseReduxToken {
+  (): {
+    keep: ReduxTokenInfo
+    setTokenBalance: (token: Token, balance: number) => TokenActionTypes
+    setTokenLoading: (token: Token, loading: boolean) => TokenActionTypes
+  }
+}
+
+export interface UseTokenContext {
+  (): { fetchKeepBalance: () => void }
+}
--- src/web3/hooks/useKeep.ts
+++ src/web3/hooks/useKeep.ts
@@ -1,29 +1,27 @@
 import { useErc20TokenContract } from "./useERC20"
 import { useWeb3React } from "@web3-react/core"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect } from "react"
 
-// const KEEP_ROPSTEN = "0x3725969e788a76fa7b5991decb6a0c27a88973c7"
+// TODO grab these from env?
+const KEEP_MAINNET = "0x3725969e788a76fa7b5991decb6a0c27a88973c7"
 const KEEP_ROPSTEN = "0xab584929f7e0d994617209d7207527b5ed8da57e"
 
 export const useKeep = () => {
   const { account } = useWeb3React()
   const keepContract = useErc20TokenContract(KEEP_ROPSTEN)
-  const [balance, setBalance] = useState(0)
-  const [balanceLoading, setBalanceLoading] = useState(false)
 
   const fetchBalance = useCallback(async () => {
     if (account && keepContract) {
       try {
-        setBalanceLoading(true)
         const rawWalletBalance = await keepContract.balanceOf(account as string)
         // TODO do not hard code decimals
-        setBalanceLoading(false)
-        setBalance(rawWalletBalance / 10 ** 18)
+        return rawWalletBalance / 10 ** 18
       } catch (error) {
-        setBalanceLoading(false)
         console.log(`Error: Fetching KEEP balance failed for ${account}`, error)
       }
     }
+
+    return 0
   }, [account, keepContract])
 
   useEffect(() => {
@@ -34,7 +32,5 @@ export const useKeep = () => {
 
   return {
     fetchBalance,
-    balance,
-    balanceLoading,
   }
 }
