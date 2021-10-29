7d77b751347cdcf9097c3ea37d3d23410ff96472
George Weiler <georgejweiler@gmail.com>
2021-10-29 14:36:00 -0700

    Install a basic router provider to unblock ephemeral deployments

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { FC } from "react"
 import { ChakraProvider, Container, Heading } from "@chakra-ui/react"
 import { Provider as ReduxProvider } from "react-redux"
+import { BrowserRouter as Router } from "react-router-dom"
 import theme from "./theme"
 import reduxStore from "./store"
 import ModalRoot from "./components/Modal"
 
 const App: FC = () => {
   return (
-    <ReduxProvider store={reduxStore}>
-      <ChakraProvider theme={theme}>
-        <ModalRoot />
-        <Container data-cy="app-container">
-          <Heading>Threshold Token Dashboard?</Heading>
-        </Container>
-      </ChakraProvider>
-    </ReduxProvider>
+    <Router basename={`${process.env.PUBLIC_URL}`}>
+      <ReduxProvider store={reduxStore}>
+        <ChakraProvider theme={theme}>
+          <ModalRoot />
+          <Container data-cy="app-container">
+            <Heading>Threshold Token Dashboard?</Heading>
+          </Container>
+        </ChakraProvider>
+      </ReduxProvider>
+    </Router>
   )
 }
 
