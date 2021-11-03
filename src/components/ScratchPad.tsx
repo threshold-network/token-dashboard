3c2ca913d684fabb95175eb2ed504692912f5fa3
George Weiler <georgejweiler@gmail.com>
2021-11-03 15:11:33 -0700

    Add nucyper to redux tokens

--- src/components/ScratchPad.tsx
+++ src/components/ScratchPad.tsx
@@ -1,16 +1,50 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Button,
+  Skeleton,
+} from "@chakra-ui/react"
 import { useReduxToken } from "../hooks/useReduxToken"
 import { useKeep } from "../web3/hooks/useKeep"
+import { useNu } from "../web3/hooks/useNu"
 
 export const ScratchPad = ({}) => {
-  const { keep } = useReduxToken()
-  const { fetchBalance } = useKeep()
+  const { keep, nu } = useReduxToken()
+  const { fetchBalance: fetchKeepBalance } = useKeep()
+  const { fetchBalance: fetchNuBalance } = useNu()
 
-  if (keep.loading) {
-    return <div>LOADING</div>
-  }
   return (
-    <div>
-      {keep.balance} <button onClick={fetchBalance}>Fetch again</button>{" "}
-    </div>
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Token</Th>
+          <Th>Balance</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>Keep</Td>
+          <Td>
+            <Skeleton isLoaded={!keep.loading}>{keep.balance}</Skeleton>
+          </Td>
+          <Td>
+            <Button onClick={fetchKeepBalance}>Fetch</Button>
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>Nu</Td>
+          <Td>
+            <Skeleton isLoaded={!nu.loading}>{nu.balance}</Skeleton>
+          </Td>
+          <Td>
+            <Button onClick={fetchNuBalance}>Fetch</Button>
+          </Td>
+        </Tr>
+      </Tbody>
+    </Table>
   )
 }
