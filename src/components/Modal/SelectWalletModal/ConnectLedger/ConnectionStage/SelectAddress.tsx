100ac5f39d84e9979054db58232e7b8bf398c6a8
George Weiler <georgejweiler@gmail.com>
2021-11-03 13:42:44 -0700

    Add pagination controls to the ledger connection modal

--- src/components/Modal/SelectWalletModal/ConnectLedger/ConnectionStage/SelectAddress.tsx
+++ src/components/Modal/SelectWalletModal/ConnectLedger/ConnectionStage/SelectAddress.tsx
@@ -1,11 +1,18 @@
-import { FC, useState } from "react"
+import { ChangeEvent, FC, useState } from "react"
 import {
-  Button,
+  HStack,
   Icon,
+  IconButton,
+  Link,
   Radio,
   RadioGroup,
-  Spinner,
-  Stack,
+  Select,
+  Skeleton,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Tr,
   useColorModeValue,
 } from "@chakra-ui/react"
 import { LedgerWhite } from "../../../../../static/icons/LedgerWhite"
@@ -13,14 +20,17 @@ import { Ledger } from "../../../../../static/icons/Ledger"
 import shortenAddress from "../../../../../utils/shortenAddress"
 import { WalletConnectionModalProps } from "../../../../../types"
 import { WalletConnectionModalBase } from "../../components"
-import { AiOutlinePlus } from "react-icons/ai"
+import { AiOutlineLink, HiChevronLeft, HiChevronRight } from "react-icons/all"
+import createEtherscanLink, {
+  ExplorerDataType,
+} from "../../../../../utils/createEtherscanLink"
 
 interface SelectAddressProps extends WalletConnectionModalProps {
   ledgerAddress: string
   setLedgerAddress: (address: string) => void
   onContinue: () => void
   ledgerAddresses: string[]
-  loadAdditionalAddresses: () => void
+  loadAdditionalAddresses: (count: number, offset: number) => void
 }
 
 const SelectAddress: FC<SelectAddressProps> = ({
@@ -33,14 +43,33 @@ const SelectAddress: FC<SelectAddressProps> = ({
   loadAdditionalAddresses,
 }) => {
   const [loading, setLoading] = useState(false)
-  const [loadedExtra, setLoadedExtra] = useState(false)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+  const [offsetIdx, setOffsetIdx] = useState(0)
 
-  const loadMore = async () => {
+  const loadMore = async (count: number, offset: number) => {
     setLoading(true)
-    await loadAdditionalAddresses()
+    await loadAdditionalAddresses(count, offset)
     setLoading(false)
-    setLoadedExtra(true)
   }
+
+  const handleRowPerPageChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setRowsPerPage(+e.target.value)
+    loadMore(+e.target.value, offsetIdx)
+  }
+
+  const paginateForward = () => {
+    const updatedOffset = offsetIdx + rowsPerPage
+    setOffsetIdx(updatedOffset)
+    loadMore(rowsPerPage, updatedOffset)
+  }
+
+  const paginateBack = () => {
+    const updatedOffset = offsetIdx - rowsPerPage
+    setOffsetIdx(updatedOffset)
+    loadMore(rowsPerPage, updatedOffset)
+  }
+
+  const confirmAddress = loading || !ledgerAddress ? () => {} : onContinue
 
   return (
     <WalletConnectionModalBase
@@ -56,38 +85,76 @@ const SelectAddress: FC<SelectAddressProps> = ({
       }
       title="Ledger"
       subTitle="Choose an address below."
-      onContinue={onContinue}
+      onContinue={confirmAddress}
     >
-      <RadioGroup
-        onChange={setLedgerAddress}
-        value={ledgerAddress}
-        maxW="130px"
-        mx="auto"
-        my={6}
-      >
-        <Stack>
-          {ledgerAddresses.map((address) => (
-            <Radio key={address} value={address}>
-              {shortenAddress(address)}
-            </Radio>
-          ))}
-        </Stack>
-        {!loadedExtra && (
-          <Button
-            variant="link"
-            mt={8}
-            ml="5px"
-            leftIcon={loading ? <Spinner /> : <AiOutlinePlus />}
-            onClick={loadMore}
-            isDisabled={loading}
-            _disabled={{
-              backgroundColor: "none",
-            }}
+      {loading ? (
+        Array.from(Array(rowsPerPage).keys()).map((address) => (
+          <Skeleton h="12px" key={address} mb={4} />
+        ))
+      ) : (
+        <RadioGroup onChange={setLedgerAddress} value={ledgerAddress} my={6}>
+          <Table variant="unstyled" w="100%" size="sm">
+            <Tbody>
+              {ledgerAddresses.map((address) => (
+                <Tr key={address}>
+                  <Td textAlign="center">
+                    <Radio key={address} value={address} />
+                  </Td>
+                  <Td textAlign="center">{shortenAddress(address)}</Td>
+                  <Td textAlign="center">
+                    <Link
+                      _hover={{ color: "blue.500" }}
+                      href={createEtherscanLink(
+                        1,
+                        address,
+                        ExplorerDataType.ADDRESS
+                      )}
+                      target="_blank"
+                    >
+                      <Icon as={AiOutlineLink} />
+                    </Link>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </RadioGroup>
+      )}
+
+      <HStack justify="space-between">
+        <HStack>
+          <Text size="sm" color="gray.500" w="115px">
+            Rows Per Page
+          </Text>
+          <Select
+            onChange={handleRowPerPageChange}
+            value={rowsPerPage}
+            disabled={loading}
+            maxW="70px"
           >
-            {loading ? "Loading..." : "See More"}
-          </Button>
-        )}
-      </RadioGroup>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+          </Select>
+        </HStack>
+        <HStack spacing={2}>
+          <IconButton
+            onClick={paginateBack}
+            disabled={loading || offsetIdx === 0}
+            aria-label={"paginate-back"}
+            variant="outline"
+            fontSize="20px"
+            icon={<HiChevronLeft />}
+          />
+          <IconButton
+            onClick={paginateForward}
+            disabled={loading}
+            aria-label={"paginate-fwd"}
+            variant="outline"
+            fontSize="20px"
+            icon={<HiChevronRight />}
+          />
+        </HStack>
+      </HStack>
     </WalletConnectionModalBase>
   )
 }
