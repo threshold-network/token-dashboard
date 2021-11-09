5035695743454b0b22d950e86d933e739607e373
George Weiler <georgejweiler@gmail.com>
2021-11-09 08:51:38 -0700

    Add footer links to the sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,43 +1,24 @@
-import {
-  Box,
-  Button,
-  Icon,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react"
+import { Box, useColorModeValue } from "@chakra-ui/react"
 import { useSidebar } from "../../hooks/useSidebar"
-import { BiChevronRight, BiChevronLeft } from "react-icons/all"
+import ExpanderIcon from "./ExpanderIcon"
+import SidebarFooter from "./SidebarFooter"
 
 const Sidebar = () => {
-  const { isOpen, openSidebar, closeSidebar } = useSidebar()
+  const { isOpen } = useSidebar()
 
   return (
     <Box
       h="100vh"
       display="flex"
-      w={isOpen ? "200px" : "80px"}
-      transition="width 1s"
+      w={isOpen ? "200px" : "85px"}
+      transition="width 0.3s"
       position="relative"
       borderRight="1px solid"
       borderColor={useColorModeValue("gray.100", "gray.700")}
       bg={useColorModeValue("white", "gray.800")}
     >
-      <Icon
-        as={isOpen ? BiChevronLeft : BiChevronRight}
-        aria-label="toggle-sidebar"
-        position="absolute"
-        right="-12px"
-        top="10px"
-        borderRadius="50%"
-        bg={useColorModeValue("white", "gray.800")}
-        color="gray.700"
-        border="1px solid"
-        borderColor={useColorModeValue("gray.100", "gray.700")}
-        h={6}
-        w={6}
-        onClick={isOpen ? closeSidebar : openSidebar}
-        cursor="pointer"
-      />
+      <ExpanderIcon />
+      <SidebarFooter />
     </Box>
   )
 }
