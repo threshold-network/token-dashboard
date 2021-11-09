9947f30add972f98e18e1c7a080e39c345f9db34
George Weiler <georgejweiler@gmail.com>
2021-11-09 16:22:13 -0700

    Add coingecko USD conversion rates to the redux token store

--- src/contexts/TokenContext.tsx
+++ src/contexts/TokenContext.tsx
@@ -2,6 +2,8 @@ import React, { createContext } from "react"
 import { useWeb3React } from "@web3-react/core"
 import { useKeep } from "../web3/hooks/useKeep"
 import { useNu } from "../web3/hooks/useNu"
+import { useReduxToken } from "../hooks/useReduxToken"
+import { Token } from "../enums"
 
 const TokenContext = createContext({})
 
@@ -10,6 +12,16 @@ export const TokenContextProvider: React.FC = ({ children }) => {
   const { fetchBalance: fetchKeepBalance } = useKeep()
   const { fetchBalance: fetchNuBalance } = useNu()
   const { active, chainId } = useWeb3React()
+  const { fetchTokenPriceUSD } = useReduxToken()
+
+  React.useEffect(() => {
+    for (const token in Token) {
+      if (token) {
+        // @ts-ignore
+        fetchTokenPriceUSD(Token[token])
+      }
+    }
+  }, [])
 
   React.useEffect(() => {
     if (active) {
--- src/enums/token.ts
+++ src/enums/token.ts
@@ -2,3 +2,8 @@ export enum Token {
   Keep = "KEEP",
   Nu = "NU",
 }
+
+export enum CoingeckoID {
+  KEEP = "keep-network",
+  NU = "nucypher",
+}
