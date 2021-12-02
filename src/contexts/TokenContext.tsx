72a9e2f7c8488e1c32964f0b86eb2ee414d0ff43
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-12-02 12:45:34 +0100

    Refactor `TokenContextProvider`
    Pass the KEEP and NU token object to the provider to be able to use them
    in other places in the dapp.

--- src/contexts/TokenContext.tsx
+++ src/contexts/TokenContext.tsx
@@ -1,16 +1,23 @@
 import React, { createContext } from "react"
+import { Contract } from "@ethersproject/contracts"
 import { useWeb3React } from "@web3-react/core"
 import { useKeep } from "../web3/hooks/useKeep"
 import { useNu } from "../web3/hooks/useNu"
 import { useReduxToken } from "../hooks/useReduxToken"
 import { Token } from "../enums"
 
-const TokenContext = createContext({})
+export const TokenContext = createContext<{
+  [key in Token]: any
+}>({
+  [Token.Keep]: {} as { contract: Contract },
+  [Token.Nu]: {} as { contract: Contract },
+})
 
-// Context that handles data fetching when a user connects their wallet or switches their network
+// Context that handles data fetching when a user connects their wallet or
+// switches their network
 export const TokenContextProvider: React.FC = ({ children }) => {
-  const { fetchKeepBalance } = useKeep()
-  const { fetchNuBalance } = useNu()
+  const keep = useKeep()
+  const nu = useNu()
   const { active, chainId } = useWeb3React()
   const { fetchTokenPriceUSD, setTokenBalance } = useReduxToken()
 
@@ -25,8 +32,8 @@ export const TokenContextProvider: React.FC = ({ children }) => {
 
   React.useEffect(() => {
     if (active) {
-      fetchKeepBalance()
-      fetchNuBalance()
+      keep.fetchKeepBalance()
+      nu.fetchNuBalance()
     } else {
       // set all token balances to 0 if the user disconnects the wallet
       for (const token in Token) {
@@ -38,5 +45,14 @@ export const TokenContextProvider: React.FC = ({ children }) => {
     }
   }, [active, chainId])
 
-  return <TokenContext.Provider value={{}}>{children}</TokenContext.Provider>
+  return (
+    <TokenContext.Provider
+      value={{
+        [Token.Keep]: keep,
+        [Token.Nu]: nu,
+      }}
+    >
+      {children}
+    </TokenContext.Provider>
+  )
 }
