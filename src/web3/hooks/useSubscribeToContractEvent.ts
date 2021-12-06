12694f9bd576407a4f44c617b79a8ed15658ee63
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-12-06 11:03:53 +0100

    Improve `useSubscribeToContractEvent` hook
    We can't add `indexedFilterParams` array as dependency in `useEffect`
    hook because this variable will always point to a new array on every
    render. Because we know that an event in solidity can have up to 3
    indexed parameters, we can extract the first three parameters from array
    and use them in `useEffect` dependency array. Thanks to this the
    `useSubscribeToContractEvent` hook will be triggered when one of these
    params changes instead of on every render.

--- src/web3/hooks/useSubscribeToContractEvent.ts
+++ src/web3/hooks/useSubscribeToContractEvent.ts
@@ -10,9 +10,15 @@ export const useSubscribeToContractEvent = (
   // to filter the `Transfer` event of ERC20 contract, which is implemented as
   // `event Transfer(address indexed from, address indexed to, uint256 value);`,
   // by `to` param we need to pass `[null, <address>]`.
-  indexedFilterParams: any[] = []
+  indexedFilterParams: string[] = []
 ) => {
   const callbackRef = useRef<(args: any[]) => void>()
+  const indexedFilterParamsLength = indexedFilterParams.length
+  // An event can have up to 3 indexed params. We want to extract these values
+  // from an array and use them in `useEffect` dependency array.
+  const firstIndexedParam = indexedFilterParams[0] || null
+  const secondIndexedParam = indexedFilterParams[1] || null
+  const thirdIndexedParam = indexedFilterParams[2] || null
 
   useEffect(() => {
     callbackRef.current = callback // Update ref to the latest callback.
@@ -22,21 +28,37 @@ export const useSubscribeToContractEvent = (
     if (!contract) {
       return
     }
+
     // @ts-ignore
     function cb(...args) {
       // @ts-ignore
       callbackRef.current(...args)
     }
-
+    const fileterParams = [
+      firstIndexedParam,
+      secondIndexedParam,
+      thirdIndexedParam,
+    ]
+    // Remove unnecessary params, otherwise encoding topic filter will fail. For
+    // example, we can't pass `[<address>, null]` if we want to filter the
+    // `Transfer` event only by `from`.
+    fileterParams.length = indexedFilterParamsLength
     const eventNameOrFilter: string | EventFilter =
-      indexedFilterParams.length === 0
+      indexedFilterParamsLength === 0
         ? eventName
-        : contract.filters[eventName](...indexedFilterParams)
+        : contract.filters[eventName](fileterParams)
 
     contract.on(eventNameOrFilter, cb)
 
     return () => {
       contract.off(eventNameOrFilter, cb)
     }
-  }, [contract, indexedFilterParams, eventName])
+  }, [
+    contract,
+    eventName,
+    firstIndexedParam,
+    secondIndexedParam,
+    thirdIndexedParam,
+    indexedFilterParamsLength,
+  ])
 }
