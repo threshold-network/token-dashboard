4635704d6e3af807248b67d5bed6c4a4da70123c
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-12-13 11:45:17 +0100

    Center the icon in spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,27 +1,21 @@
-import { HStack, Icon, Spinner as ChakraSpinner } from "@chakra-ui/react"
+import { Flex, Icon, Spinner as ChakraSpinner } from "@chakra-ui/react"
 import Threshold from "../static/icons/Ttoken"
 
 // Maybe we should build this component in Chakra way and make it more
 // reusable.
 const Spinner = () => {
   return (
-    <HStack position="relative" justify="center">
+    <Flex position="relative" alignItems="center" justifyContent="center">
       <ChakraSpinner
         thickness="8px"
-        speed="1.3s"
+        speed="0"
         emptyColor="gray.200"
         color="brand.500"
         height="120px"
         width="120px"
       />
-      <Icon
-        position="absolute"
-        left="163px"
-        height="60px"
-        w="60px"
-        as={Threshold}
-      />
-    </HStack>
+      <Icon position="absolute" height="60px" w="60px" as={Threshold} />
+    </Flex>
   )
 }
 
