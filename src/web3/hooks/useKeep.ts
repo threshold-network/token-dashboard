fac410db70a677ad21100e498173e6d136afdca6
George Weiler <georgejweiler@gmail.com>
2021-12-17 12:43:27 -0700

    Clean up hard-coded local config vars

--- src/web3/hooks/useKeep.ts
+++ src/web3/hooks/useKeep.ts
@@ -4,8 +4,6 @@ import { Token } from "../../enums"
 import { TransactionType } from "../../enums/transactionType"
 import { Contract } from "@ethersproject/contracts"
 
-const TETHER_ROPSTEN = "0x110a13FC3efE6A245B50102D2d79B3E76125Ae83"
-
 // The artifacts from `@keep-network/keep-core` for a given build only support a
 // single network id.
 const networks = Object.keys(KeepToken.networks) as Array<
@@ -17,8 +15,6 @@ const KEEP_TOKEN_ADDRESS =
     ? (KeepToken.networks[networks[0]] as { address: string }).address
     : null
 
-console.log("KeepToken", KeepToken)
-
 export interface UseKeep {
   (): {
     approveKeep: () => void
@@ -29,8 +25,7 @@ export interface UseKeep {
 
 export const useKeep: UseKeep = () => {
   const { balanceOf, approve, contract } = useErc20TokenContract(
-    TETHER_ROPSTEN,
-    // KEEP_TOKEN_ADDRESS as string,
+    KEEP_TOKEN_ADDRESS as string,
     undefined,
     KeepToken.abi
   )
--- src/web3/hooks/useUpgradeToT.ts
+++ src/web3/hooks/useUpgradeToT.ts
@@ -1,16 +1,8 @@
 import { useCallback } from "react"
-// import VendingMachineKeep from "@threshold-network/solidity-contracts/artifacts/VendingMachineKeep.json"
-// import VendingMachineNuCypher from "@threshold-network/solidity-contracts/artifacts/VendingMachineNuCypher.json"
-import { Token } from "../../enums"
 import { useToken } from "../../hooks/useToken"
 import { useSendTransaction } from "./useSendTransaction"
 import { useVendingMachineContract } from "./useVendingMachineContract"
 import { UpgredableToken } from "../../types"
-
-// const TOKEN_TO_VENDING_MACHINE_ARTIFACT = {
-//   [Token.KeepCircleBrand]: VendingMachineKeep,
-//   [Token.NuCircleBrand]: VendingMachineNuCypher,
-// }
 
 export const useUpgradeToT = (from: UpgredableToken) => {
   const { contract } = useToken(from)
@@ -23,8 +15,6 @@ export const useUpgradeToT = (from: UpgredableToken) => {
 
   const upgradeToT = useCallback(
     async (amount: string) => {
-      // const vendingMachineArtifact = TOKEN_TO_VENDING_MACHINE_ARTIFACT[from]
-      // await sendTransaction(vendingMachineArtifact.address, amount, [])
       await sendTransaction(vendingMachineContractAddress, amount, [])
     },
     [sendTransaction, from, vendingMachineContractAddress]
