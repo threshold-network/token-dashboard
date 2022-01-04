c386c5a397c9a8bac2b8a817d311fb7069f3a1c6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-01-04 15:31:43 +0100

    Update installation script
    Deploy contracts from `@keep-network/tbtc`,
    `@keep-network/coverage-pools` and `@keep-network/ecdsa` pacakges to run
    the dapp locally. Also update the `Prerequisites` section in readme.

--- scripts/start_dashboard.sh
+++ scripts/start_dashboard.sh
@@ -7,21 +7,49 @@ LOG_END='\n\e[0m' # new line + reset color
 DASHBOARD_PATH=$PWD
 MULTICALL_PATH="$DASHBOARD_PATH/multicall"
 THRESHOLD_CONTRACTS_PATH="$PWD/../solidity-contracts"
-KEEP_CORE_SOL_PATH="$PWD/../keep-core/solidity-v1"
 
+KEEP_CORE_PATH="$PWD/../keep-core"
+KEEP_CORE_SOL_PATH="$KEEP_CORE_PATH/solidity-v1"
+
+KEEP_ECDSA_PATH="$PWD/../keep-ecdsa"
+KEEP_ECDSA_SOL_PATH="$KEEP_ECDSA_PATH/solidity"
+
+COV_POOLS_PATH="$PWD/../coverage-pools"
+
+TBTC_PATH="$PWD/../tbtc/"
+TBTC_SOL_PATH="$TBTC_PATH/solidity"
+
+printf "${LOG_START}Migrating contracts for keep-core...${LOG_END}"
+cd "$KEEP_CORE_PATH"
+./scripts/install.sh --network local --contracts-only
 cd "$KEEP_CORE_SOL_PATH"
-printf "${LOG_START}Installing Keep core dependencies...${LOG_START}"
-npm install
-printf "${LOG_START}Deploying contracts for keep-core...${LOG_START}"
-npx truffle migrate --reset --network local
-rm -rf artifacts
-cp -r build/contracts artifacts
+yarn link
+
+printf "${LOG_START}Migrating contracts for keep-ecdsa...${LOG_END}"
+cd "$KEEP_ECDSA_PATH"
+./scripts/install.sh --network local --contracts-only
+cd "$KEEP_ECDSA_SOL_PATH"
+yarn link
+
+printf "${LOG_START}Migrating contracts for tBTC...${LOG_END}"
+cd "$TBTC_PATH"
+./scripts/install.sh
+cd "$TBTC_SOL_PATH"
+yarn link
+
+cd $COV_POOLS_PATH
+printf "${LOG_START}Migrating contracts for coverage pools...${LOG_END}"
+yarn
+yarn link @keep-network/keep-core @keep-network/tbtc
+yarn deploy --network development --reset
+./scripts/prepare-artifacts.sh --network development
 yarn link
 
 cd "$THRESHOLD_CONTRACTS_PATH"
 printf "${LOG_START}Installing Threshold network solidity contracts dependencies...${LOG_START}"
 yarn
 yarn link @keep-network/keep-core
+./scripts/prepare-dependencies.sh
 printf "${LOG_START}Deploying contracts for threshold solidity contracts...${LOG_START}"
 yarn deploy --network development --reset
 ./scripts/prepare-artifacts.sh --network development
@@ -38,6 +66,10 @@ printf "${LOG_START}Multicall address deployed at ${MULTICALL_ADDRESS}${LOG_END}
 cd "$DASHBOARD_PATH"
 printf "${LOG_START}Installing T Token Dashboard...${LOG_END}"
 yarn
-yarn link @keep-network/keep-core @threshold-network/solidity-contracts
+yarn link @threshold-network/solidity-contracts \
+    @keep-network/keep-core \
+    @keep-network/keep-ecdsa \
+    @keep-network/tbtc \
+    @keep-network/coverage-pools
 printf "${LOG_START}Starting dashboard...${LOG_END}"
 MULTICALL_ADDRESS="${MULTICALL_ADDRESS}" yarn start
