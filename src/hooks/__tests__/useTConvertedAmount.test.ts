b20c079e61a73c43d7058d0c69a88b382d17cefd
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-01-12 10:52:24 +0100

    Update useTConvertedAmount tests
    Add a case where amount to convert is as `number`.

--- src/hooks/__tests__/useTConvertedAmount.test.ts
+++ src/hooks/__tests__/useTConvertedAmount.test.ts
@@ -12,20 +12,38 @@ describe("Test `useTConvertedAmount` hook", () => {
   const mockedNuRatio = "4500000000000000" // 0.0045
   const amountToConvert = "1000000000000000000" // 1
   const expectedTAmount = "4500000000000000000" // 4.5
+  const expectedformattedAmount = "4.50"
+
+  const amountToConvert2 = 1000000000000000 // 0.001
+  const expectedTAmount2 = "4500000000000000" // 0.0045
+  const expectedformattedAmount2 = "0.00"
 
   beforeEach(() => {
     ;(useVendingMachineRatio as jest.Mock).mockReturnValue(mockedNuRatio)
   })
 
-  test("should convert token amount to T", () => {
-    const { result } = renderHook(() =>
-      useTConvertedAmount(token, amountToConvert)
-    )
+  test.each`
+    value               | expectedValue       | formattedValue
+    ${amountToConvert}  | ${expectedTAmount}  | ${expectedformattedAmount}
+    ${amountToConvert2} | ${expectedTAmount2} | ${expectedformattedAmount2}
+  `(
+    "should convert $value token amount to T",
+    ({
+      value,
+      expectedValue,
+      formattedValue,
+    }: {
+      value: string | number
+      expectedValue: string
+      formattedValue: string
+    }) => {
+      const { result } = renderHook(() => useTConvertedAmount(token, value))
 
-    expect(useVendingMachineRatio).toHaveBeenCalledWith(token)
-    expect(result.current.amount).toEqual(expectedTAmount)
-    expect(result.current.formattedAmount).toEqual("4.50")
-  })
+      expect(useVendingMachineRatio).toHaveBeenCalledWith(token)
+      expect(result.current.amount).toEqual(expectedValue)
+      expect(result.current.formattedAmount).toEqual(formattedValue)
+    }
+  )
 
   test.each`
     value
