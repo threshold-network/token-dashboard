5b08bd8c761a80415b729d5fb9fb9d10272983a5
George Weiler <georgejweiler@gmail.com>
2022-01-11 22:33:20 -0700

    Fix start up script

--- scripts/start_dashboard.sh
+++ scripts/start_dashboard.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
 
 LOG_START='\n\e[1;36m' # new line + bold + color
 LOG_END='\n\e[0m' # new line + reset color
 
+help()
+{
+   echo -e "\nUsage: $0"\
+           "--multicall-only"
+   echo -e "\nCommand line arguments:\n"
+   echo -e "\t--multicall-only: Should execute Multicall contract part only."\
+                        "Other deployment scripts will not be executed.\n"
+   exit 1 # Exit script after printing help
+}
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--multicall-only") set -- "$@" "-m" ;;
+    "--help")           set -- "$@" "-h" ;;
+    *)                  set -- "$@" "$arg"
+  esac
+done
+
+# Parse short options
+OPTIND=1
+while getopts "mh" opt; do
+  case "$opt" in
+  m ) multicall_only=true ;;
+  h ) help ;;
+  ? ) help ;; # Print help in case parameter is non-existent
+  esac
+done
+shift $(expr $OPTIND - 1) # remove options from positional parameters
+
+MULTICALL_ONLY=${multicall_only:-false}
+
 DASHBOARD_PATH=$PWD
-#MULTICALL_PATH="$DASHBOARD_PATH/multicall"
+MULTICALL_PATH="$DASHBOARD_PATH/multicall"
 THRESHOLD_CONTRACTS_PATH="$PWD/../solidity-contracts"
+
 KEEP_CORE_PATH="$PWD/../keep-core"
 KEEP_CORE_SOL_PATH="$KEEP_CORE_PATH/solidity-v1"
 
-#KEEP_ECDSA_PATH="$PWD/../keep-ecdsa"
-#KEEP_ECDSA_SOL_PATH="$KEEP_ECDSA_PATH/solidity"
-#COV_POOLS_PATH="$PWD/../coverage-pools"
-#TBTC_PATH="$PWD/../tbtc/"
-#TBTC_SOL_PATH="$TBTC_PATH/solidity"
+KEEP_ECDSA_PATH="$PWD/../keep-ecdsa"
+KEEP_ECDSA_SOL_PATH="$KEEP_ECDSA_PATH/solidity"
+
+COV_POOLS_PATH="$PWD/../coverage-pools"
+
+TBTC_PATH="$PWD/../tbtc/"
+TBTC_SOL_PATH="$TBTC_PATH/solidity"
+
+function start_dashboard {
+    cd "$DASHBOARD_PATH"
+    printf "${LOG_START}Installing T Token Dashboard...${LOG_END}"
+    yarn
+    yarn link @threshold-network/solidity-contracts \
+        @keep-network/keep-core \
+        @keep-network/keep-ecdsa \
+        @keep-network/tbtc \
+        @keep-network/coverage-pools
+
+    printf "${LOG_START}Starting dashboard...${LOG_END}"
+    MULTICALL_ADDRESS="${MULTICALL_ADDRESS}" yarn start
+}
+
+cd "$MULTICALL_PATH"
+printf "${LOG_START}Installing multicall dependencies...${LOG_START}"
+yarn
+printf "${LOG_START}Deploying contracts for multicall...${LOG_START}"
+MULTICALL_ADDRESS_OUTPUT=$(yarn deploy:development)
+MULTICALL_ADDRESS=$(echo "$MULTICALL_ADDRESS_OUTPUT" | tail -1)
+printf "${LOG_START}Multicall address deployed at ${MULTICALL_ADDRESS}${LOG_END}"
+
+if [ "$MULTICALL_ONLY" = true ]; then
+ start_dashboard
+ exit 0
+fi
 
 printf "${LOG_START}Migrating contracts for keep-core...${LOG_END}"
 cd "$KEEP_CORE_PATH"
@@ -22,25 +85,25 @@ cd "$KEEP_CORE_PATH"
 cd "$KEEP_CORE_SOL_PATH"
 yarn link
 
-#printf "${LOG_START}Migrating contracts for keep-ecdsa...${LOG_END}"
-#cd "$KEEP_ECDSA_PATH"
-#./scripts/install.sh --network local --contracts-only
-#cd "$KEEP_ECDSA_SOL_PATH"
-#yarn link
-#
-#printf "${LOG_START}Migrating contracts for tBTC...${LOG_END}"
-#cd "$TBTC_PATH"
-#./scripts/install.sh
-#cd "$TBTC_SOL_PATH"
-#yarn link
+printf "${LOG_START}Migrating contracts for keep-ecdsa...${LOG_END}"
+cd "$KEEP_ECDSA_PATH"
+./scripts/install.sh --network local --contracts-only
+cd "$KEEP_ECDSA_SOL_PATH"
+yarn link
 
-#cd $COV_POOLS_PATH
-#printf "${LOG_START}Migrating contracts for coverage pools...${LOG_END}"
-#yarn
-#yarn link @keep-network/keep-core @keep-network/tbtc
-#yarn deploy --network development --reset
-#./scripts/prepare-artifacts.sh --network development
-#yarn link
+printf "${LOG_START}Migrating contracts for tBTC...${LOG_END}"
+cd "$TBTC_PATH"
+./scripts/install.sh
+cd "$TBTC_SOL_PATH"
+yarn link
+
+cd $COV_POOLS_PATH
+printf "${LOG_START}Migrating contracts for coverage pools...${LOG_END}"
+yarn
+yarn link @keep-network/keep-core @keep-network/tbtc
+yarn deploy --network development --reset
+./scripts/prepare-artifacts.sh --network development
+yarn link
 
 cd "$THRESHOLD_CONTRACTS_PATH"
 printf "${LOG_START}Installing Threshold network solidity contracts dependencies...${LOG_START}"
@@ -52,24 +115,4 @@ yarn deploy --network development --reset
 ./scripts/prepare-artifacts.sh --network development
 yarn link
 
-#cd "$MULTICALL_PATH"
-#printf "${LOG_START}Installing multicall dependencies...${LOG_START}"
-#yarn
-#printf "${LOG_START}Deploying contracts for multicall...${LOG_START}"
-#MULTICALL_ADDRESS_OUTPUT=$(yarn deploy:development)
-#MULTICALL_ADDRESS=$(echo "$MULTICALL_ADDRESS_OUTPUT" | tail -1)
-#printf "${LOG_START}Multicall address deployed at ${MULTICALL_ADDRESS}${LOG_END}"
-
-cd "$DASHBOARD_PATH"
-printf "${LOG_START}Installing T Token Dashboard...${LOG_END}"
-yarn
-yarn link @threshold-network/solidity-contracts \
-    @keep-network/keep-core
-#    @keep-network/keep-ecdsa \
-#    @keep-network/tbtc \
-#    @keep-network/coverage-pools
-
-printf "${LOG_START}Starting dashboard...${LOG_END}"
-
-yarn start
-#MULTICALL_ADDRESS="${MULTICALL_ADDRESS}" yarn start
+start_dashboard
