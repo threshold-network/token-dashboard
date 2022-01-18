a61b56e48b563b0f939c402185eeb9d78bc83009
George Weiler <georgejweiler@gmail.com>
2022-01-18 11:21:05 -0700

    Break the tx success modal out into a reusable component

--- src/components/Modal/UpgradeToTModal/UpgradeSuccess.tsx
+++ src/components/Modal/UpgradeToTModal/UpgradeSuccess.tsx
@@ -1,26 +1,9 @@
 import { FC } from "react"
-import {
-  Alert,
-  AlertIcon,
-  Box,
-  HStack,
-  Icon,
-  ModalBody,
-  ModalCloseButton,
-  ModalHeader,
-  ModalFooter,
-  Button,
-} from "@chakra-ui/react"
-import Confetti from "react-confetti"
-import Threshold from "../../../static/icons/Ttoken"
-import { Body3, H5 } from "../../Typography"
-import { Divider } from "../../Divider"
 import UpgradeStats from "./UpgradeStats"
-import ViewInBlockExplorer from "../../ViewInBlockExplorer"
 import { useTExchangeRate } from "../../../hooks/useTExchangeRate"
-import { ExplorerDataType } from "../../../utils/createEtherscanLink"
 import withBaseModal from "../withBaseModal"
 import { BaseModalProps, UpgredableToken } from "../../../types"
+import TransactionSuccessModal from "../TransactionSuccessModal"
 
 interface UpgradeSuccessProps extends BaseModalProps {
   upgradedAmount: string
@@ -34,62 +17,22 @@ const UpgradeSuccess: FC<UpgradeSuccessProps> = ({
   receivedAmount,
   transactionHash,
   token,
-  closeModal,
 }) => {
   const { formattedAmount: exchangeRate } = useTExchangeRate(token)
 
   return (
-    <>
-      <ModalHeader>
-        <H5>Success</H5>
-      </ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <Alert status="success" mb={4}>
-          <AlertIcon />
-          Your upgrade was successful
-        </Alert>
-        <HStack
-          borderRadius="md"
-          bg="gray.50"
-          justify="center"
-          py={12}
-          position="relative"
-          mb={8}
-        >
-          <Box position="absolute" left="-20px" top="-25px" zindex="-1">
-            {/* TODO: create a component wrapper for `Confetti` */}
-            <Confetti
-              width={440}
-              height={250}
-              confettiSource={{ x: 200, y: 40, h: 100, w: 100 }}
-              numberOfPieces={50}
-            />
-          </Box>
-          <Icon zIndex={999} height="105px" w="105px" as={Threshold} />
-        </HStack>
-
+    <TransactionSuccessModal
+      subTitle="Your upgrade was successful!"
+      transactionHash={transactionHash}
+      body={
         <UpgradeStats
           token={token}
           exchangeRate={exchangeRate}
           receivedAmount={receivedAmount}
           upgradedAmount={upgradedAmount}
         />
-
-        <Body3 mt="4rem" align="center">
-          <ViewInBlockExplorer
-            text="View"
-            id={transactionHash}
-            type={ExplorerDataType.TRANSACTION}
-          />{" "}
-          transaction on Etherscan
-        </Body3>
-        <Divider />
-      </ModalBody>
-      <ModalFooter>
-        <Button onClick={closeModal}>Dismiss</Button>
-      </ModalFooter>
-    </>
+      }
+    />
   )
 }
 
--- src/pages/Staking/StakesTable/StakeActionsCell.tsx
+++ src/pages/Staking/StakesTable/StakeActionsCell.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react"
 import { Button, HStack, Td } from "@chakra-ui/react"
 import { StakeCellProps } from "../../../types/staking"
+import useUnstakeTransaction from "../../../web3/hooks/useUnstakeTransaction"
+import { useModal } from "../../../hooks/useModal"
 
 const StakeActionsCell: FC<StakeCellProps> = ({ stake }) => {
   const pre = undefined
+
+  const { openModal } = useModal()
 
   return (
     <Td>
