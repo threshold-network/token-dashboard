d30ce169cfe001a8c7aa60079d7e0a6649a16134
George Weiler <georgejweiler@gmail.com>
2022-01-18 10:24:32 -0700

    Update small items from code review

--- src/utils/getUsdBalance.tsx
+++ src/utils/getUsdBalance.tsx
@@ -1,16 +1,16 @@
-import numeral from "numeral"
 import { FixedNumber } from "@ethersproject/bignumber"
 import { formatUnits } from "@ethersproject/units"
+import { formatFiatCurrencyAmount } from "./formatAmount"
 
 const getUsdBalance = (
   balance: string | number,
   usdConversion: number
 ): string => {
-  return numeral(
+  return formatFiatCurrencyAmount(
     FixedNumber.fromString(usdConversion.toString())
       .mulUnsafe(FixedNumber.fromString(formatUnits(balance)))
       .toString()
-  ).format("$0,0.00")
+  )
 }
 
 export default getUsdBalance
