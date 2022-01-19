3b0c4faa28de8aafb66202d77199a93d7bed8557
George Weiler <georgejweiler@gmail.com>
2022-01-19 12:44:13 -0700

    Add validation, clean up staking redux state, other small improvements from code review

--- src/components/Modal/UpgradeToTModal/UpgradeStats.tsx
+++ src/components/Modal/UpgradeToTModal/UpgradeStats.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react"
-import { HStack, Stack } from "@chakra-ui/react"
 import TokenBalance from "../../TokenBalance"
 import { Body3 } from "../../Typography"
 import { Token } from "../../../enums"
+import TransactionInfoTable, {
+  TransactionInfo,
+} from "../../TransactionInfoTable"
 
 interface TransactionStatsProps {
   upgradedAmount: string
@@ -17,7 +19,7 @@ const UpgradeStats: FC<TransactionStatsProps> = ({
   receivedAmount,
   exchangeRate,
 }) => {
-  const transactionInfo = [
+  const transactionInfo: TransactionInfo[] = [
     {
       text: "Upgrade Amount",
       // todo: Token might not be a string, so this should be updated once we decide on the interface
@@ -51,16 +53,7 @@ const UpgradeStats: FC<TransactionStatsProps> = ({
     },
   ]
 
-  return (
-    <Stack spacing="0.5rem">
-      {transactionInfo.map((info) => (
-        <HStack justify="space-between" key={info.text}>
-          <Body3 color="gray.500">{info.text}</Body3>
-          {info.value}
-        </HStack>
-      ))}
-    </Stack>
-  )
+  return <TransactionInfoTable transactionInfo={transactionInfo} />
 }
 
 export default UpgradeStats
