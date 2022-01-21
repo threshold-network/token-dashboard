8cb0d6972f6864153c0fef3868c9630bc85479af
George Weiler <georgejweiler@gmail.com>
2022-01-21 12:44:49 -0700

    Adjust typography colors and add branded wordmark

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,40 +1,109 @@
 import { FC } from "react"
-import { Heading, HeadingProps, Text, TextProps } from "@chakra-ui/react"
+import {
+  Heading,
+  HeadingProps,
+  Text,
+  TextProps,
+  useColorModeValue,
+} from "@chakra-ui/react"
 
 export const Headline: FC<HeadingProps> = (props) => {
-  return <Heading fontSize="7xl" {...props} />
+  return (
+    <Heading
+      fontSize="7xl"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const H1: FC<TextProps> = (props) => {
-  return <Text as="h1" fontSize="6xl" {...props} />
+  return (
+    <Text
+      as="h1"
+      fontSize="6xl"
+      fontWeight="bold"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const H2: FC<TextProps> = (props) => {
-  return <Text as="h2" fontSize="5xl" {...props} />
+  return (
+    <Text
+      as="h2"
+      fontSize="5xl"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const H3: FC<TextProps> = (props) => {
-  return <Text as="h3" fontSize="4xl" {...props} />
+  return (
+    <Text
+      as="h3"
+      fontSize="4xl"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const H4: FC<TextProps> = (props) => {
-  return <Text as="h4" fontSize="3xl" {...props} />
+  return (
+    <Text
+      as="h4"
+      fontSize="3xl"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const H5: FC<TextProps> = (props) => {
-  return <Text as="h5" fontSize="2xl" {...props} />
+  return (
+    <Text
+      as="h5"
+      fontSize="2xl"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const Body1: FC<TextProps> = (props) => {
-  return <Text as="p" fontSize="lg" {...props} />
+  return (
+    <Text
+      as="p"
+      fontSize="lg"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const Body2: FC<TextProps> = (props) => {
-  return <Text as="p" fontSize="md" {...props} />
+  return (
+    <Text
+      as="p"
+      fontSize="md"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const Body3: FC<TextProps> = (props) => {
-  return <Text as="p" fontSize="sm" {...props} />
+  return (
+    <Text
+      as="p"
+      fontSize="sm"
+      color={useColorModeValue("gray.700", "gray.300")}
+      {...props}
+    />
+  )
 }
 
 export const Label1: FC<TextProps> = (props) => {
@@ -44,6 +113,7 @@ export const Label1: FC<TextProps> = (props) => {
       fontSize="20px"
       lineHeight="28px"
       letterSpacing="0.1em"
+      color={useColorModeValue("gray.700", "gray.300")}
       {...props}
     />
   )
@@ -56,6 +126,7 @@ export const Label2: FC<TextProps> = (props) => {
       fontSize="16px"
       lineHeight="24px"
       letterSpacing="0.075em"
+      color={useColorModeValue("gray.700", "gray.300")}
       {...props}
     />
   )
@@ -68,6 +139,7 @@ export const Label3: FC<TextProps> = (props) => {
       fontSize="14px"
       lineHeight="20px"
       letterSpacing="0.05em"
+      color={useColorModeValue("gray.700", "gray.300")}
       {...props}
     />
   )
