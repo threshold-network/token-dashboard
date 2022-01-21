ab43c590e1cd97e0d0e0592f544ae429ea3416ef
George Weiler <georgejweiler@gmail.com>
2022-01-21 13:41:24 -0700

    Update token balance component and fix styling issues in wallet balance card

--- src/components/TokenBalance.tsx
+++ src/components/TokenBalance.tsx
@@ -1,8 +1,10 @@
 import { FC, useMemo } from "react"
-import { Box, TextProps } from "@chakra-ui/react"
+import { Box, Stack, TextProps } from "@chakra-ui/react"
 import { formatUnits } from "@ethersproject/units"
 import numeral from "numeral"
+import { useWeb3React } from "@web3-react/core"
 import { Body3, H5 } from "./Typography"
+import Icon from "./Icon"
 
 interface TokenBalanceProps {
   tokenAmount: string | number
@@ -11,6 +13,8 @@ interface TokenBalanceProps {
   withUSDBalance?: boolean
   withSymbol?: boolean
   tokenDecimals?: number
+  icon?: any
+  iconSize?: string
 }
 
 const TokenBalance: FC<TokenBalanceProps & TextProps> = ({
@@ -20,8 +24,16 @@ const TokenBalance: FC<TokenBalanceProps & TextProps> = ({
   withUSDBalance = false,
   withSymbol = false,
   tokenDecimals,
+  icon,
+  iconSize = "32px",
   ...restProps
 }) => {
+  const { account, active } = useWeb3React()
+  const shouldRenderTokenAmount = useMemo(
+    () => tokenAmount && account && active,
+    [tokenAmount, account, active]
+  )
+
   const _tokenAmount = useMemo(() => {
     return numeral(formatUnits(tokenAmount, tokenDecimals)).format("0,0.00")
   }, [tokenAmount])
@@ -29,11 +41,13 @@ const TokenBalance: FC<TokenBalanceProps & TextProps> = ({
   // TODO: more flexible approach to style wrapper, token balance and USD balance.
   return (
     <Box>
-      <H5 {...restProps}>
-        {`${tokenAmount ? _tokenAmount : "--"}${
-          withSymbol && tokenSymbol ? ` ${tokenSymbol}` : ""
-        }`}
-      </H5>
+      <Stack direction="row">
+        {icon && <Icon as={icon} boxSize={iconSize} alignSelf="center" />}
+        <H5 {...restProps}>{shouldRenderTokenAmount ? _tokenAmount : "--"}</H5>
+        {withSymbol && tokenSymbol && (
+          <Body3 alignSelf="center">{tokenSymbol}</Body3>
+        )}
+      </Stack>
       {withUSDBalance && usdBalance && (
         <Body3 color="gray.500">{usdBalance}</Body3>
       )}
