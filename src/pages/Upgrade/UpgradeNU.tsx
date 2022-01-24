41d3cced6bdb00ee482ea7a7b7199f18ac3d3654
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-01-24 13:20:14 +0100

    Update `Upgrade` page component
    Convert this component into the custom `PageComponent` that must define
    the routing props in `route` field.

---
+++ src/pages/Upgrade/UpgradeNU.tsx
@@ -0,0 +1,15 @@
+import UpgradeToken from "./UpgradeToken"
+import { Token } from "../../enums"
+import { PageComponent } from "../../types"
+
+const UpgradeNU: PageComponent = (props) => (
+  <UpgradeToken {...props} token={Token.Nu} />
+)
+
+UpgradeNU.route = {
+  path: "nu",
+  index: true,
+  title: "NU to T",
+}
+
+export default UpgradeNU
