6a0c4a465e6756dd1f269814251c52dbd42599bf
George Weiler <georgejweiler@gmail.com>
2022-01-24 13:11:05 -0700

    Update unstaking UI

--- src/components/Modal/UnstakeSuccessModal/index.tsx
+++ src/components/Modal/UnstakeSuccessModal/index.tsx
@@ -2,18 +2,31 @@ import { FC } from "react"
 import withBaseModal from "../withBaseModal"
 import { BaseModalProps } from "../../../types"
 import TransactionSuccessModal from "../TransactionSuccessModal"
+import StakingStats from "../../StakingStats"
+import { StakeData } from "../../../types/staking"
 
 interface UnstakeSuccessProps extends BaseModalProps {
   transactionHash: string
+  stake: StakeData
+  unstakeAmount: string | number
 }
 
 const UnstakingSuccessModal: FC<UnstakeSuccessProps> = ({
   transactionHash,
+  stake,
+  unstakeAmount,
 }) => {
+  const { beneficiary, operator, authorizer } = stake
+
   return (
     <TransactionSuccessModal
-      subTitle="You have successfully unstaked"
+      subTitle="Your unstake was successful!"
       transactionHash={transactionHash}
+      body={
+        <StakingStats
+          {...{ stakeAmount: unstakeAmount, beneficiary, operator, authorizer }}
+        />
+      }
     />
   )
 }
