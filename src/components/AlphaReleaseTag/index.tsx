94407403fd8ebab4799153da7e21fa4bf9da8c43
George Weiler <georgejweiler@gmail.com>
2022-01-24 18:08:39 -0700

    Adjust tag margins

--- src/components/AlphaReleaseTag/index.tsx
+++ src/components/AlphaReleaseTag/index.tsx
@@ -5,7 +5,7 @@ import { useSidebar } from "../../hooks/useSidebar"
 const AlphaReleaseTag: FC = () => {
   const { isOpen } = useSidebar()
   return (
-    <Box display="flex" justifyContent="center">
+    <Box display="flex" justifyContent="center" mt={6}>
       <Badge
         colorScheme="purple"
         variant="subtle"
