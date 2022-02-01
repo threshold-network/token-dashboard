994476649696b773f751b21c63f2946b99d797b2
George Weiler <georgejweiler@gmail.com>
2022-02-01 13:57:39 -0700

    Revert "Bump-up versions of keep-network and t-network dependencies"

--- src/utils/getContract.ts
+++ src/utils/getContract.ts
@@ -36,20 +36,3 @@ export function getContract(
     getProviderOrSigner(library, account) as any
   )
 }
-
-export function getContractAddressFromTruffleArtifact(
-  truffleArtifact: { networks: { [chainID: string]: { address: string } } },
-  chainID: string | undefined = undefined
-) {
-  const networks = Object.keys(truffleArtifact.networks) as Array<
-    keyof typeof truffleArtifact.networks
-  >
-
-  return networks && networks.length > 0
-    ? (
-        truffleArtifact.networks[chainID ? chainID : networks[0]] as {
-          address: string
-        }
-      ).address
-    : AddressZero
-}
--- src/web3/hooks/useKeep.ts
+++ src/web3/hooks/useKeep.ts
@@ -3,7 +3,17 @@ import { useErc20TokenContract } from "./useERC20"
 import { Token } from "../../enums"
 import { TransactionType } from "../../enums/transactionType"
 import { Contract } from "@ethersproject/contracts"
-import { getContractAddressFromTruffleArtifact } from "../../utils/getContract"
+
+// The artifacts from `@keep-network/keep-core` for a given build only support a
+// single network id.
+const networks = Object.keys(KeepToken.networks) as Array<
+  keyof typeof KeepToken.networks
+>
+
+const KEEP_TOKEN_ADDRESS =
+  networks && networks.length > 0
+    ? (KeepToken.networks[networks[0]] as { address: string }).address
+    : null
 
 export interface UseKeep {
   (): {
@@ -15,7 +25,7 @@ export interface UseKeep {
 
 export const useKeep: UseKeep = () => {
   const { balanceOf, approve, contract } = useErc20TokenContract(
-    getContractAddressFromTruffleArtifact(KeepToken),
+    KEEP_TOKEN_ADDRESS as string,
     undefined,
     KeepToken.abi
   )
