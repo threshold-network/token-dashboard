a61c1ec463d0e4b6bbd19b74f3f829c8c91c8285
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-02-09 11:03:03 +0100

    Simplify the `useTokenBalance` hook
    In `1f4a1dc` we unnecessary updated the `useTokenBalance` hook because
    we thought this hook was causing the balance update problem after
    catching the `Transfer` event. There was actually a bug in
    `useSubscribeToContractEvent` hook which was fixed in `ad0bccd`.

--- src/hooks/useTokenBalance.ts
+++ src/hooks/useTokenBalance.ts
@@ -1,19 +1,9 @@
 import { useMemo } from "react"
 import { Token } from "../enums/token"
-import { useTokenState } from "./useTokenState"
+import { useToken } from "./useToken"
 
 export const useTokenBalance = (token: Token) => {
-  const tokenMap = {
-    [Token.T]: "t",
-    [Token.Nu]: "nu",
-    [Token.Keep]: "keep",
-    [Token.TBTC]: "tbtc",
-  }
-
-  const tokenState = useTokenState()
-
-  // @ts-ignore
-  const _token = tokenState[tokenMap[token]]
+  const _token = useToken(token)
 
   return useMemo(() => _token.balance, [_token.balance])
 }
