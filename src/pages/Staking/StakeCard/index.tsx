6f6d84b1b9599ddb4ea71fe13ef898c868c11534
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-03-03 17:13:50 +0100

    Create StakeCard component
    Display stakes in cards instead of table.

---
+++ src/pages/Staking/StakeCard/index.tsx
@@ -0,0 +1,137 @@
+import { FC, useState } from "react"
+import {
+  Flex,
+  Badge,
+  ButtonGroup,
+  Button,
+  useColorModeValue,
+  useBoolean,
+  Box,
+} from "@chakra-ui/react"
+import { StakeData } from "../../../types/staking"
+import Card from "../../../components/Card"
+import { Body2, Label3 } from "../../../components/Typography"
+import NotificationPill from "../../../components/NotificationPill"
+import TokenBalance from "../../../components/TokenBalance"
+import InfoBox from "../../../components/InfoBox"
+import BoxLabel from "../../../components/BoxLabel"
+import { CopyAddressToClipboard } from "../../../components/CopyToClipboard"
+import TokenBalanceInput from "../../../components/TokenBalanceInput"
+import { useTokenBalance } from "../../../hooks/useTokenBalance"
+import { ModalType, StakeType, Token } from "../../../enums"
+import ThresholdCircleBrand from "../../../static/icons/ThresholdCircleBrand"
+import { useModal } from "../../../hooks/useModal"
+import { Divider } from "../../../components/Divider"
+
+const StakeCard: FC<{ stake: StakeData }> = ({ stake }) => {
+  const [isStakeAction, setFlag] = useBoolean(true)
+  const tBalance = useTokenBalance(Token.T)
+  const [amount, setAmount] = useState<string | number | undefined>(undefined)
+  const { openModal } = useModal()
+
+  const submitButtonText = isStakeAction ? "Top-up" : "Unstake"
+  const stakeType =
+    stake.stakeType === StakeType.NU || stake.stakeType === StakeType.KEEP
+      ? `legacy ${StakeType[stake.stakeType]}`
+      : "native"
+
+  const onSubmitForm = (event: any) => {
+    event.preventDefault()
+    if (isStakeAction) {
+      openModal(ModalType.TopupT, { stake, initialTopupAmount: amount })
+    } else {
+      openModal(ModalType.UnstakeT, { stake, initialTopupAmount: amount })
+    }
+  }
+
+  return (
+    <Card>
+      <Flex as="header" alignItems="center">
+        <Badge colorScheme="green" variant="subtle" size="small" mr="2">
+          active
+        </Badge>
+        <NotificationPill colorScheme="brand" mr="2" variant="gradient" />
+        <Label3 textTransform="uppercase" mr="auto">
+          stake - {stakeType}
+        </Label3>
+        <Switcher onClick={setFlag.toggle} isActive={isStakeAction} />
+      </Flex>
+      <Body2 mt="12">Total Rewards</Body2>
+      <Flex mt="4" alignItems="center">
+        <TokenBalance
+          tokenAmount="2000340000000000000000"
+          tokenSymbol="T"
+          withSymbol
+          isLarge
+        />
+        <Button size="sm" ml="auto" variant="outline" w="28">
+          Claim
+        </Button>
+      </Flex>
+      <Divider />
+      <Body2 mt="6">Staked Balance</Body2>
+      <InfoBox mt="3">
+        <TokenBalance
+          tokenAmount={stake.totalInTStake}
+          withSymbol
+          tokenSymbol="T"
+        />
+      </InfoBox>
+      <Flex mt="6" alignItems="center">
+        <BoxLabel bg="brand.50" color="brand.700" mr="auto">
+          Provider address
+        </BoxLabel>
+        <CopyAddressToClipboard address={stake.stakingProvider} />
+      </Flex>
+      <Box as="form" mt="7" mb="8" onSubmit={onSubmitForm}>
+        <TokenBalanceInput
+          label="Stake Amount"
+          placeholder="T amount"
+          amount={amount}
+          setAmount={setAmount}
+          max={tBalance}
+          icon={ThresholdCircleBrand}
+          mb={2}
+        />
+        <Button type="submit" w="100%" mt="6">
+          {submitButtonText}
+        </Button>
+      </Box>
+    </Card>
+  )
+}
+
+const Switcher: FC<{ onClick: () => void; isActive: boolean }> = ({
+  onClick,
+  isActive,
+}) => {
+  const bgColor = useColorModeValue("gray.50", "gray.700")
+  const activeButtonColor = useColorModeValue("white", "gray.700")
+
+  return (
+    <ButtonGroup
+      backgroundColor={bgColor}
+      borderRadius="6px"
+      p={1}
+      spacing="3"
+      size="xs"
+    >
+      <Button
+        variant={isActive ? "outline" : "ghost"}
+        bg={isActive ? activeButtonColor : undefined}
+        onClick={onClick}
+      >
+        Stake
+      </Button>
+      <Button
+        variant={!isActive ? "outline" : "ghost"}
+        bg={!isActive ? activeButtonColor : undefined}
+        onClick={onClick}
+      >
+        Unstake
+      </Button>
+    </ButtonGroup>
+  )
+}
+
+export default StakeCard
