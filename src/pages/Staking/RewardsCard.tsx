c7933d5c2f226d68b90a5dcd92a8d1b8573c5964
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-03-10 14:01:59 +0100

    Update staking page
    Add `staking tvl` card and `rewards` card. Clean up some styling issues.

--- src/pages/Staking/RewardsCard.tsx
+++ src/pages/Staking/RewardsCard.tsx
@@ -1,25 +1,30 @@
 import { FC } from "react"
+import { Button, useColorModeValue } from "@chakra-ui/react"
 import Card from "../../components/Card"
 import { Body2, Body3, Label3 } from "../../components/Typography"
-import { Box, Stack, useColorModeValue } from "@chakra-ui/react"
 import InfoBox from "../../components/InfoBox"
-import TokenBalance from "../../components/TokenBalance"
 
 const RewardsCard: FC = () => {
   return (
     <Card>
-      <Stack spacing={4}>
-        <Box>
-          <Label3 textDecoration="uppercase">Rewards</Label3>
-          <Body2 mb={2}>Total Rewards</Body2>
-          <InfoBox>
-            <TokenBalance tokenAmount={0} withSymbol tokenSymbol="T" isLarge />
-          </InfoBox>
-          <Body3 color={useColorModeValue("gray.500", "gray.300")}>
-            Rewards are released at the end of each month
-          </Body3>
-        </Box>
-      </Stack>
+      <Label3 textTransform="uppercase">Rewards</Label3>
+      <Body2 mt="6">Total Rewards</Body2>
+      <InfoBox mt="2">
+        <Body2>
+          Rewards are released at the end of each month and can be claimed
+          retroactively for March and April.
+        </Body2>
+      </InfoBox>
+      <Button mt="4" colorScheme="gray" disabled={true} isFullWidth>
+        See all Rewards
+      </Button>
+      <Body3
+        mt="2"
+        textAlign="center"
+        color={useColorModeValue("gray.500", "gray.300")}
+      >
+        Rewards are released at the end of each month
+      </Body3>
     </Card>
   )
 }
