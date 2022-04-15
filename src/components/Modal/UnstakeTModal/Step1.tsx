2c141d61472c792d0cc99dd9bf05688b0a6f259e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-04-15 16:09:45 +0200

    Update the first step of the unstaking flow
    Display the `unstake all` option only if a user has at least one legacy
    stake (KEPP/NU or KEEP + NU).

--- src/components/Modal/UnstakeTModal/Step1.tsx
+++ src/components/Modal/UnstakeTModal/Step1.tsx
@@ -51,6 +51,7 @@ const UnstakeTModal: FC<BaseModalProps & { stake: StakeData }> = ({
 
   const hasNuStake = stake.nuInTStake !== "0"
   const hasKeepStake = stake.keepInTStake !== "0"
+  const hasLegacyStake = hasNuStake || hasKeepStake
 
   const getLegacyTabTitle = (token: UpgredableToken) => {
     return `Unstake legacy ${hasKeepStake && hasNuStake ? token : ""} stake`
@@ -68,23 +69,35 @@ const UnstakeTModal: FC<BaseModalProps & { stake: StakeData }> = ({
       <ModalCloseButton />
       <ModalBody>
         <InfoBox variant="modal" mb="6">
-          <H5 mb={4}>This is a Legacy Stake</H5>
-          <Body3>Unstaking a Legacy Stake can be done:</Body3>
-          <UnorderedList>
-            <ListItem mt="4">
-              <Body3>
-                Partial by unstaking your legacy tokens and your T tokens in
-                separate transactions. Legacy tokens staked can be unstaked only
-                in full amount.
-              </Body3>
-            </ListItem>
-            <ListItem mt="4">
-              <Body3>
-                Total by unstaking the whole amount of T and legacy tokens
-                staked, in one transaction.
-              </Body3>
-            </ListItem>
-          </UnorderedList>
+          <H5 mb={4}>
+            {hasLegacyStake
+              ? "This is a Legacy Stake"
+              : "You are about unstake your tokens"}
+          </H5>
+          {hasLegacyStake ? (
+            <>
+              <Body3>Unstaking a Legacy Stake can be done:</Body3>
+              <UnorderedList>
+                <ListItem mt="4">
+                  <Body3>
+                    Partial by unstaking your legacy tokens and your T tokens in
+                    separate transactions. Legacy tokens staked can be unstaked
+                    only in full amount.
+                  </Body3>
+                </ListItem>
+                <ListItem mt="4">
+                  <Body3>
+                    Total by unstaking the whole amount of T and legacy tokens
+                    staked, in one transaction.
+                  </Body3>
+                </ListItem>
+              </UnorderedList>
+            </>
+          ) : (
+            <Body3>
+              You can partially or totally unstake depending on your needs.
+            </Body3>
+          )}
         </InfoBox>
         <Tabs isFitted>
           <TabList mb="8">
@@ -138,15 +151,19 @@ const UnstakeTModal: FC<BaseModalProps & { stake: StakeData }> = ({
             )}
           </TabPanels>
         </Tabs>
-        <Divider>
-          <DividerCenterElement>
-            <Label3>OR</Label3>
-          </DividerCenterElement>
-        </Divider>
-        <Button variant="outline" isFullWidth onClick={onUnstakeAllBtn}>
-          Unstake all
-        </Button>
-        <Body3 mt="2">{getUnstakeAllBtnHelperText()}</Body3>
+        {hasLegacyStake && (
+          <>
+            <Divider>
+              <DividerCenterElement>
+                <Label3>OR</Label3>
+              </DividerCenterElement>
+            </Divider>
+            <Button variant="outline" isFullWidth onClick={onUnstakeAllBtn}>
+              Unstake all
+            </Button>
+            <Body3 mt="2">{getUnstakeAllBtnHelperText()}</Body3>
+          </>
+        )}
         <StakingContractLearnMore mt="7" />
         <Divider mt="4" />
       </ModalBody>
