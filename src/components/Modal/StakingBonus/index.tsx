74f8a5bec85d46dcc8666e5315cdf1842026fac8
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-05-10 15:27:26 +0200

    Remove unnecessary components
    The flow has changed and we will display all owner stakes w/o typing an
    exact provider address.

--- src/components/Modal/StakingBonus/index.tsx
+++ src/components/Modal/StakingBonus/index.tsx
@@ -1,32 +1,20 @@
-import { FC, useState } from "react"
+import { FC } from "react"
 import { useWeb3React } from "@web3-react/core"
 import { ConnectWallet } from "./ConnectWallet"
 import withBaseModal from "../withBaseModal"
 import { BaseModalProps } from "../../../types"
-import { CheckEligibility } from "./CheckEligibility"
 import { EligibilityConfirmation } from "./EligibilityConfirmation"
-import { BonusEligibility } from "../../../types/staking"
+import { useStakingState } from "../../../hooks/useStakingState"
 
 const StakingBonusModal: FC<BaseModalProps> = ({ closeModal }) => {
   const { active } = useWeb3React()
-  const [isFetching, setIsFetching] = useState(false)
-  const [bonusEligibility, setBonusEligibility] =
-    useState<BonusEligibility | null>(null)
+  const { stakes } = useStakingState()
 
   return (
     <>
       {!active && <ConnectWallet closeModal={closeModal} />}
       {active && (
-        <CheckEligibility
-          closeModal={closeModal}
-          onSubmit={(values) => console.log("form values", values)}
-        />
-      )}
-      {bonusEligibility && (
-        <EligibilityConfirmation
-          closeModal={closeModal}
-          bonusEligibility={bonusEligibility}
-        />
+        <EligibilityConfirmation closeModal={closeModal} stakes={stakes} />
       )}
     </>
   )
