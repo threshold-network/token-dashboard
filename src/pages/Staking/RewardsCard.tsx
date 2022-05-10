3a0f5839f42615440d67470c14c211fb4903d8ab
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-05-10 16:35:11 +0200

    Update copy
    In StakingBonus banner component and Rewards card on staking page.

--- src/pages/Staking/RewardsCard.tsx
+++ src/pages/Staking/RewardsCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 import { Badge, Box, Button, Flex, useColorModeValue } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
 import Card from "../../components/Card"
-import { Body2, Body3, Label3 } from "../../components/Typography"
+import { Body1, Body2, Body3, Label3 } from "../../components/Typography"
 import InfoBox from "../../components/InfoBox"
 import TokenBalance from "../../components/TokenBalance"
 
@@ -15,55 +15,23 @@ const RewardsCard: FC<{
   return (
     <Card>
       <Label3 textTransform="uppercase">Rewards</Label3>
-      {!active && (
-        <>
-          <Body2 mt="6">Total Rewards</Body2>
+      <Body2 mt="6">Total Staking Bonus</Body2>
+      <InfoBox mt="2" bg={active ? "brand.50" : undefined}>
+        {active ? (
+          <TokenBalance
+            tokenAmount={totalBonusBalance}
+            withSymbol
+            tokenSymbol="T"
+            isLarge
+          />
+        ) : (
+          <Body2>
+            Rewards are released at the end of each month and can be claimed
+            retroactively for March and April.
+          </Body2>
+        )}
+      </InfoBox>
 
-          <InfoBox mt="2">
-            <Body2>
-              Rewards are released at the end of each month and can be claimed
-              retroactively for March and April.
-            </Body2>
-          </InfoBox>
-        </>
-      )}
-      {active && (
-        <Flex
-          minWidth="max-content"
-          gap={{ base: 0, md: 4 }}
-          direction={{ base: "column", md: "row" }}
-        >
-          <Box mt={{ base: "0", md: "6" }} flex="1">
-            <Body2>Total Rewards</Body2>
-            <InfoBox mt="2" p="4" textAlign="right">
-              <TokenBalance
-                tokenAmount={totalRewardsBalance}
-                withSymbol
-                tokenSymbol="T"
-              />
-            </InfoBox>
-          </Box>
-          <Box
-            mt={{ base: "0", md: "6" }}
-            textAlign="right"
-            width={{ base: "100%", md: "35%" }}
-          >
-            <Badge variant="subtle" colorScheme="brand">
-              Staking Bonus
-            </Badge>
-            <InfoBox mt="2" p="4" display="flex" alignItems="end" bg="brand.50">
-              <TokenBalance
-                fontWeight="400"
-                fontSize="18px"
-                lineHeight="28px"
-                tokenAmount={totalBonusBalance}
-                withSymbol
-                tokenSymbol="T"
-              />
-            </InfoBox>
-          </Box>
-        </Flex>
-      )}
       <Button mt="4" colorScheme="gray" disabled={true} isFullWidth>
         See all Rewards
       </Button>
