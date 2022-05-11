f1cd2d8b8fed27a3136eda7318509ad0299f4959
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-05-11 14:41:07 +0200

    Hardcode the PRE contract address and ABI
    Use hardcoded addresses for supported networks. You need to set the
    correct addresss of the `SimplePREApplication` contract for your local
    chain- see threshold-network/solidity-contracts#104. If you deployed the
    `@threshold-network/solidity-contracts` to your local chain and linked
    package using `yarn link @threshold-network/solidity-contracts` you can
    find the contract address at
    `node_modules/@threshold-network/solidity-contracts/artifacts/SimplePREApplication.json`.
    This is probably a temporary solution - eg. we could set this contract
    address using `start_dashboard` script.

--- src/web3/hooks/usePREContract.ts
+++ src/web3/hooks/usePREContract.ts
@@ -1,13 +1,26 @@
-// TODO: how to get this contract on mainnet- check if we could add this
-// artifact to the mainnet version of `@threshold-network/solidity-contracts`.
-// To test it locallt switch to `nu-contracts` branch of
-// `https://github.com/threshold-network/solidity-contracts` repo.
-import SimplePREApplication from "@threshold-network/solidity-contracts/artifacts/SimplePREApplication.json"
-import { supportedChainId } from "../../utils/getEnvVariable"
+import SimplePREApplicationABI from "../abi/SimplePreApplication.json"
 import { useContract } from "./useContract"
+import { supportedChainId } from "../../utils/getEnvVariable"
+import { ChainID } from "../../enums"
+import { AddressZero } from "../utils"
 
 export const PRE_DEPLOYMENT_BLOCK = supportedChainId === "1" ? 14141140 : 0
 
+const PRE_ADDRESSESS = {
+  // https://etherscan.io/address/0x7E01c9c03FD3737294dbD7630a34845B0F70E5Dd
+  [ChainID.Ethereum.valueOf().toString()]:
+    "0x7E01c9c03FD3737294dbD7630a34845B0F70E5Dd",
+  // https://ropsten.etherscan.io/address/0xb6f98dA65174CE8F50eA0ea4350D96B2d3eFde9a
+  [ChainID.Ropsten.valueOf().toString()]:
+    "0xb6f98dA65174CE8F50eA0ea4350D96B2d3eFde9a",
+  // Set the correct `SimplePREApplication` contract address. If you deployed
+  // the `@threshold-network/solidity-contracts` to your local chain and linked
+  // package using `yarn link @threshold-network/solidity-contracts` you can
+  // find the contract address at
+  // `node_modules/@threshold-network/solidity-contracts/artifacts/SimplePREApplication.json`.
+  [ChainID.Localhost.valueOf().toString()]: AddressZero,
+} as Record<string, string>
+
 export const usePREContract = () => {
-  return useContract(SimplePREApplication.address, SimplePREApplication.abi)
+  return useContract(PRE_ADDRESSESS[supportedChainId], SimplePREApplicationABI)
 }
