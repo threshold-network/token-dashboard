9f97f5407f33b93e6d67ceb59191927801149b51
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-05-12 14:30:08 +0200

    Set the correct link for PRE node setup
    See:
    - https://github.com/threshold-network/token-dashboard/issues/97
    - https://github.com/threshold-network/token-dashboard/pull/95

--- src/enums/externalHref.ts
+++ src/enums/externalHref.ts
@@ -3,7 +3,7 @@ export enum ExternalHref {
   thresholdDiscord = "https://discord.gg/WXK9PC6SRF",
   metamaskHomePage = "https://metamask.io/",
   stakingContractLeanMore = "https://github.com/threshold-network/solidity-contracts/issues/53",
-  preNodeSetup = "https://docs.nucypher.com/en/latest/pre_application/running_a_node.html#configure-and-run-a-pre-node",
+  preNodeSetup = "https://docs.nucypher.com/en/latest/pre_application/running_a_node.html",
   preStakingProvidersList = "https://docs.nucypher.com/en/latest/pre_application/node_providers.html",
   exchangeRateLearnMore = "https://blog.threshold.network/threshold-launch/",
   keepDapp = "https://dashboard.keep.network/",
