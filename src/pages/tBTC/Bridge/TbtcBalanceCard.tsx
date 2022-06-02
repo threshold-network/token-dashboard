2452fbedfaee6413f492646a94a9ee06e5023633
michalsmiarowski <michal.smiarowski@thesis.co>
2022-06-02 23:08:52 +0200

    Add proper labels to tBTCBalanceCard

--- src/pages/tBTC/Bridge/TbtcBalanceCard.tsx
+++ src/pages/tBTC/Bridge/TbtcBalanceCard.tsx
@@ -1,13 +1,19 @@
 import { FC, ComponentProps } from "react"
 import Card from "../../../components/Card"
-import { Label3 } from "../../../components/Typography"
+import { Label2, Body3, H5 } from "../../../components/Typography"
+import { HStack } from "@chakra-ui/react"
 
 export const TbtcBalanceCard: FC<ComponentProps<typeof Card>> = ({
   ...props
 }) => {
   return (
     <Card {...props}>
-      <Label3 mb="5">tBTC Balance</Label3>
+      <Label2 mb="5">tBTC Balance</Label2>
+      <HStack alignItems={"baseline"}>
+        <H5>0.00</H5>
+        <Body3>tBTC</Body3>
+      </HStack>
+      <Body3>$0 USD</Body3>
     </Card>
   )
 }
