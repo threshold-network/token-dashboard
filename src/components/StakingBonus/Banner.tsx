3acd6bb2b036f1f3219496de4638663c522a8922
George Weiler <georgejweiler@gmail.com>
2022-06-13 10:13:54 -0600

    Upate button casing

--- src/components/StakingBonus/Banner.tsx
+++ src/components/StakingBonus/Banner.tsx
@@ -46,7 +46,7 @@ export const StakingBonusBanner = () => {
           marginInlineStart={{ base: "8", xl: "auto !important" }}
           px={{ base: 4, md: 12 }}
         >
-          Check eligibility
+          Check Eligibility
         </Button>
       </Stack>
     </Card>
