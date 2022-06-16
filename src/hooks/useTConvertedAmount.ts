52af86d8753a3cd3f9dae4fa964b4c652221f81e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-06-16 14:50:42 +0200

    Update `useTConvertedAmount` hook
    Add a fn that converts a given amount of KEEP/NU token into T and return
    it from hook so we can use this function in case we can't pass the token
    amount into hook.

--- src/hooks/useTConvertedAmount.ts
+++ src/hooks/useTConvertedAmount.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useCallback } from "react"
 import { BigNumber } from "@ethersproject/bignumber"
 import { formatUnits } from "@ethersproject/units"
 import numeral from "numeral"
@@ -14,27 +14,36 @@ export const useTConvertedAmount = (
 ) => {
   const ratio = useVendingMachineRatio(token)
 
+  const convertToT = useCallback(
+    (_amountToConvert = 0) => {
+      const wrappedRemainder = BigNumber.from(_amountToConvert).mod(
+        FLOATING_POINT_DIVISOR
+      )
+      const convertibleAmount =
+        BigNumber.from(_amountToConvert).sub(wrappedRemainder)
+      return convertibleAmount
+        .mul(BigNumber.from(ratio))
+        .div(FLOATING_POINT_DIVISOR)
+        .toString()
+    },
+    [ratio]
+  )
+
   return useMemo(() => {
     if (!amountToConvert || !ratio || ratio === "0") {
       return {
         amount: "0",
         formattedAmount: "--",
+        convertToT,
       }
     }
 
-    const wrappedRemainder = BigNumber.from(amountToConvert).mod(
-      FLOATING_POINT_DIVISOR
-    )
-    const convertibleAmount =
-      BigNumber.from(amountToConvert).sub(wrappedRemainder)
-    const amount = convertibleAmount
-      .mul(BigNumber.from(ratio))
-      .div(FLOATING_POINT_DIVISOR)
-      .toString()
+    const amount = convertToT(amountToConvert)
 
     return {
       amount,
       formattedAmount: numeral(formatUnits(amount)).format("0,0.00"),
+      convertToT,
     }
-  }, [amountToConvert, ratio])
+  }, [amountToConvert, ratio, convertToT])
 }
