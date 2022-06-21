025e6285818efdcae909b1615c7d192b13000412
George Weiler <georgejweiler@gmail.com>
2022-06-21 15:43:57 -0600

    Add filter comppnent from t-components repo

--- src/pages/tBTC/Bridge/MintUnmintNav.tsx
+++ src/pages/tBTC/Bridge/MintUnmintNav.tsx
@@ -1,6 +1,6 @@
 import { ComponentProps, FC } from "react"
-import { Button, HStack } from "@chakra-ui/react"
-import { Card } from "@threshold-network/components"
+import { Box } from "@chakra-ui/react"
+import { Card, FilterTabs } from "@threshold-network/components"
 import { TbtcMintingType } from "../../../types/tbtc"
 import { useTbtcState } from "../../../hooks/useTbtcState"
 
@@ -8,24 +8,23 @@ export const MintUnmintNav: FC<ComponentProps<typeof Card>> = ({
   ...props
 }) => {
   const { mintingType, updateState } = useTbtcState()
+
   return (
-    <Card {...props}>
-      <HStack>
-        <Button
-          isFullWidth
-          variant={mintingType === TbtcMintingType.mint ? "outline" : "ghost"}
-          onClick={() => updateState("mintingType", TbtcMintingType.mint)}
-        >
-          Mint
-        </Button>
-        <Button
-          isFullWidth
-          variant={mintingType === TbtcMintingType.unmint ? "outline" : "ghost"}
-          onClick={() => updateState("mintingType", TbtcMintingType.unmint)}
-        >
-          Unmint
-        </Button>
-      </HStack>
-    </Card>
+    <Box {...props}>
+      <FilterTabs
+        selectedTabId={mintingType}
+        setSelectedTabId={(tabId) => updateState("mintingType", tabId)}
+        tabs={[
+          {
+            title: "Mint",
+            tabId: TbtcMintingType.mint,
+          },
+          {
+            title: "Unmint",
+            tabId: TbtcMintingType.unmint,
+          },
+        ]}
+      />
+    </Box>
   )
 }
