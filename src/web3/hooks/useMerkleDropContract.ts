1dfafcaa0704f397d174ceb84401781be85ee2b5
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-06-27 13:18:00 +0200

    Add `CumulativeMerkleDrop` contract

---
+++ src/web3/hooks/useMerkleDropContract.ts
@@ -0,0 +1,23 @@
+import CumulativeMerkleDropABI from "../abi/CumulativeMerkleDrop.json"
+import { useContract } from "./useContract"
+import { supportedChainId } from "../../utils/getEnvVariable"
+import { ChainID } from "../../enums"
+import { AddressZero } from "../utils"
+
+export const DEPLOYMENT_BLOCK = supportedChainId === "1" ? 0 : 0
+
+const CONTRACT_ADDRESSESS = {
+  // TODO: Set mainnet address
+  [ChainID.Ethereum.valueOf().toString()]: AddressZero,
+  // TODO: Set ropsten address
+  [ChainID.Ropsten.valueOf().toString()]: AddressZero,
+  // TODO: Set local address- how to resolve it in local network?
+  [ChainID.Localhost.valueOf().toString()]: AddressZero,
+} as Record<string, string>
+
+export const useMerkleDropContract = () => {
+  return useContract(
+    CONTRACT_ADDRESSESS[supportedChainId],
+    CumulativeMerkleDropABI
+  )
+}
