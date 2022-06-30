7bc6dad32151c41517b6c9d28b8ae3a965b3a119
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-06-30 10:20:41 +0200

    Update the `RewardsJSONData` type
    The rewards will be keyed by staking provider not beneficiary.

--- src/types/rewards.ts
+++ src/types/rewards.ts
@@ -1,7 +1,7 @@
 export interface RewardsJSONData {
   tokenTotal: string
   merkleRoot: string
-  claims: { [beneficiary: string]: { amount: string; proof: string[] } }
+  claims: { [stakingProvider: string]: { amount: string; proof: string[] } }
 }
 
 export interface BonusEligibility {
