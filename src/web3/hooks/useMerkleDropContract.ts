aa7143e3fea8a94e162940d2f16a7ebe0bca3cf6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-07-15 09:43:50 +0200

    Set the `CumulativeMerkleDrop` contract address
    Set the contract address for Ropsten network. We deployed the mocked
    contract to the Ropsten network.
    
    https://ropsten.etherscan.io/address/0x835d2F36450c7d20cCE3b3F31aBa6BF2Ac31c6f3

--- src/web3/hooks/useMerkleDropContract.ts
+++ src/web3/hooks/useMerkleDropContract.ts
@@ -10,8 +10,9 @@ const CONTRACT_ADDRESSESS = {
   // TODO: Set mainnet address
   [ChainID.Ethereum.valueOf().toString()]:
     "0xcE14F142f16cb0f360b8EB021A780ed3EC04516c",
-  // TODO: Set ropsten address
-  [ChainID.Ropsten.valueOf().toString()]: AddressZero,
+  // https://ropsten.etherscan.io/address/0x835d2F36450c7d20cCE3b3F31aBa6BF2Ac31c6f3
+  [ChainID.Ropsten.valueOf().toString()]:
+    "0x835d2F36450c7d20cCE3b3F31aBa6BF2Ac31c6f3",
   // TODO: Set local address- how to resolve it in local network?
   [ChainID.Localhost.valueOf().toString()]: AddressZero,
 } as Record<string, string>
