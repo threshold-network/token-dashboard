e2a4fff1a739d98f0cd3437a4aea70b9beef495e
michalsmiarowski <michal.smiarowski@thesis.co>
2022-07-21 16:51:00 +0200

    Remove unnecessary args from `install` script

--- scripts/start_dashboard.sh
+++ scripts/start_dashboard.sh
@@ -83,7 +83,7 @@ fi
 
 printf "${LOG_START}Migrating contracts for keep-core...${LOG_END}"
 cd "$KEEP_CORE_PATH"
-./scripts/install_keep_core_v1.sh --network local --contracts-only
+./scripts/install-v1.sh --network local
 cd "$KEEP_CORE_SOL_PATH"
 yarn link
 
