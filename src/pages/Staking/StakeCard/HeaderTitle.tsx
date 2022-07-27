cf8bb41a3b68074153c0a02c1d4b4557f4525399
George Weiler <georgejweiler@gmail.com>
2022-07-27 12:49:34 -0600

    Extracts the stake type to a util for reuse in other components

--- src/pages/Staking/StakeCard/HeaderTitle.tsx
+++ src/pages/Staking/StakeCard/HeaderTitle.tsx
@@ -1,22 +1,17 @@
 import { FC } from "react"
 import { StakeData } from "../../../types/staking"
-import { StakeType } from "../../../enums"
 import NotificationPill from "../../../components/NotificationPill"
 import { LabelSm } from "@threshold-network/components"
+import { getStakeType } from "../../../utils/getStakeType"
 
 export const StakeCardHeaderTitle: FC<{ stake: StakeData | null }> = ({
   stake,
 }) => {
-  const stakeType = !stake
-    ? ""
-    : stake.stakeType === StakeType.NU || stake.stakeType === StakeType.KEEP
-    ? ` - legacy ${StakeType[stake.stakeType]}`
-    : " - native"
   return (
     <>
       <NotificationPill colorScheme="brand" mr="2" variant="gradient" />
       <LabelSm textTransform="uppercase" mr="auto">
-        stake{stakeType}
+        stake{getStakeType(stake)}
       </LabelSm>
     </>
   )
