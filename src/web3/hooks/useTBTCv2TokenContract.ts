bbc8c0df8cf3d41cfecc16dfa4795006427432f1
michalsmiarowski <michal.smiarowski@thesis.co>
2022-08-02 18:02:17 +0200

    Add tbtc-v2 packages to package.json
    Adds tbtc-v2 packages to package.json and remove the dynamic imports

--- src/web3/hooks/useTBTCv2TokenContract.ts
+++ src/web3/hooks/useTBTCv2TokenContract.ts
@@ -3,35 +3,29 @@ import { Token, TransactionType } from "../../enums"
 import { useContext } from "react"
 import { FeatureFlagsContext } from "../../contexts/FeatureFlagContext"
 import { FeatureFlag } from "../../feature-flags/featureFlags"
+import TBTC from "@keep-network/tbtc-v2/artifacts/TBTC.json"
 
-const tbtcv2TokenPath = "@keep-network/tbtc-v2/artifacts/TBTC.json"
 export const useTBTCv2TokenContract: any = () => {
   const featureFlagsContext = useContext(FeatureFlagsContext)
   if (featureFlagsContext[FeatureFlag.TBTCV2].isActive) {
-    import(/* webpackIgnore: true */ tbtcv2TokenPath)
-      .then((TBTC) => {
-        const { balanceOf, approve, contract } = useErc20TokenContract(
-          TBTC.address,
-          undefined,
-          TBTC.abi
-        )
+    const { balanceOf, approve, contract } = useErc20TokenContract(
+      TBTC.address,
+      undefined,
+      TBTC.abi
+    )
 
-        // TODO:
-        const approveTBTCV2 = () => {}
+    // TODO:
+    const approveTBTCV2 = () => {}
 
-        const fetchTBTCV2Balance = () => {
-          balanceOf(Token.TBTCV2)
-        }
+    const fetchTBTCV2Balance = () => {
+      balanceOf(Token.TBTCV2)
+    }
 
-        return {
-          fetchTBTCV2Balance,
-          approveTBTCV2,
-          contract,
-        }
-      })
-      .catch((err) => {
-        return undefined
-      })
+    return {
+      fetchTBTCV2Balance,
+      approveTBTCV2,
+      contract,
+    }
   }
 
   return undefined
