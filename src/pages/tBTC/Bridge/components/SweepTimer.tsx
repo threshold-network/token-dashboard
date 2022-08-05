1d493aa043a849cdf27d4f5f700949da39ecf0d3
michalsmiarowski <michal.smiarowski@thesis.co>
2022-08-05 20:53:43 +0200

    Use Countdown component from components lib
    Use Countdown component from `components` lib and remove `useCountdown` hook.

--- src/pages/tBTC/Bridge/components/SweepTimer.tsx
+++ src/pages/tBTC/Bridge/components/SweepTimer.tsx
@@ -1,7 +1,12 @@
 import { FC, useEffect, useState } from "react"
-import { Box, H4, LabelSm, Skeleton } from "@threshold-network/components"
+import {
+  Box,
+  Countdown,
+  H4,
+  LabelSm,
+  Skeleton,
+} from "@threshold-network/components"
 import { useTbtcState } from "../../../../hooks/useTbtcState"
-import { useCountdown } from "../../../../hooks/useCountdown"
 
 export const SweepTimer: FC = () => {
   const { nextBridgeCrossingInUnix, updateState } = useTbtcState()
@@ -10,11 +15,6 @@ export const SweepTimer: FC = () => {
     const currentTimeInSeconds = Math.floor(Date.now() / 1000)
     updateState("nextBridgeCrossingInUnix", currentTimeInSeconds + 10)
   }
-
-  const { days, hours, minutes, seconds } = useCountdown(
-    nextBridgeCrossingInUnix ? nextBridgeCrossingInUnix : 0,
-    onComplete
-  )
 
   // TODO: this is a hack to make the timer load after 3 seconds. We need to
   // pull this from the contract
@@ -31,9 +31,21 @@ export const SweepTimer: FC = () => {
       <LabelSm>Next Bridge Crossing</LabelSm>
 
       <Skeleton isLoaded={!!nextBridgeCrossingInUnix}>
-        <H4 color="brand.500" fontWeight={800}>
-          {hours} : {minutes} : {seconds}
-        </H4>
+        <Countdown
+          targetDateInUnix={
+            nextBridgeCrossingInUnix ? nextBridgeCrossingInUnix : 0
+          }
+          onComplete={onComplete}
+          withLeadingZeroes={false}
+        >
+          {(days, hours, minutes, seconds) => {
+            return (
+              <H4 color="brand.500" fontWeight={800}>
+                {hours} : {minutes} : {seconds}
+              </H4>
+            )
+          }}
+        </Countdown>
       </Skeleton>
     </Box>
   )
