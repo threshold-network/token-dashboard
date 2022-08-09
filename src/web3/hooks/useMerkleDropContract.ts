7f2597010889ed18979ca0ff38e857f0be717d18
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-08-09 16:26:45 +0200

    Set merkle drop contrarct deployment block
    The merkle drop contract has been deployed at
    `0x902eae448671b8f0eeb0d59bf796c55dac5ae4988fe8b71da1645d9d014583d6` tx
    at `15146501` block. See
    https://etherscan.io/tx/0x902eae448671b8f0eeb0d59bf796c55dac5ae4988fe8b71da1645d9d014583d6

--- src/web3/hooks/useMerkleDropContract.ts
+++ src/web3/hooks/useMerkleDropContract.ts
@@ -4,7 +4,7 @@ import { supportedChainId } from "../../utils/getEnvVariable"
 import { ChainID } from "../../enums"
 import { AddressZero } from "../utils"
 
-export const DEPLOYMENT_BLOCK = supportedChainId === "1" ? 0 : 0
+export const DEPLOYMENT_BLOCK = supportedChainId === "1" ? 15146501 : 0
 
 const CONTRACT_ADDRESSESS = {
   // https://etherscan.io/address/0xea7ca290c7811d1cc2e79f8d706bd05d8280bd37
