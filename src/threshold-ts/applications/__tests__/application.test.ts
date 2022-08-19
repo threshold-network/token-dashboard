09be38ef92d3a3a58fa205faa7b7b9c859c1eb02
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-08-19 12:09:38 +0200

    Add unit tests for application implementation

---
+++ src/threshold-ts/applications/__tests__/application.test.ts
@@ -0,0 +1,145 @@
+import { ContractInterface, BigNumber } from "ethers"
+import { AddressZero, getContract } from "../../utils"
+import { Application, IApplication } from ".."
+import { IStaking } from "../../staking"
+
+jest.mock("../../utils", () => ({
+  ...(jest.requireActual("../../utils") as {}),
+  getContract: jest.fn(),
+}))
+
+describe("Application test", () => {
+  let application: IApplication
+  let staking: IStaking
+
+  const amount = BigNumber.from("123")
+  const time = BigNumber.from("456")
+  const address = AddressZero
+  const abi: ContractInterface = []
+  const stakingProvider = "0x486b0ee2eed761f069f327034eb2ae5e07580bf3"
+  const mockAppContract = {
+    address,
+    minimumAuthorization: jest.fn(),
+    pendingAuthorizationDecrease: jest.fn(),
+    remainingAuthorizationDecreaseDelay: jest.fn(),
+    authorizationParameters: jest.fn(),
+    stakingProviderToOperator: jest.fn(),
+    isOperatorInPool: jest.fn(),
+  }
+
+  beforeEach(() => {
+    staking = {
+      authorizedStake: jest.fn(),
+    } as unknown as IStaking
+    ;(getContract as jest.Mock).mockImplementation(() => mockAppContract)
+    application = new Application(staking, { address, abi })
+  })
+
+  test("should create the Application instance correctly", () => {
+    expect(getContract).toHaveBeenCalledWith(address, abi)
+    expect(application.address).toEqual(address)
+    expect(application.contract).toEqual(mockAppContract)
+  })
+
+  test("should return the auhorized stake for a given staking provider", async () => {
+    const stakingSpy = jest
+      .spyOn(staking, "authorizedStake")
+      .mockResolvedValue(amount)
+
+    const result = await application.authorizedStake(stakingProvider)
+
+    expect(stakingSpy).toHaveBeenCalledWith(
+      stakingProvider,
+      application.address
+    )
+    expect(result).toEqual(amount)
+  })
+
+  test("should return the minimum authorization amount", async () => {
+    const minAuthSpy = jest
+      .spyOn(mockAppContract, "minimumAuthorization")
+      .mockResolvedValue(amount)
+
+    const result = await application.minimumAuthorization()
+
+    expect(minAuthSpy).toHaveBeenCalled()
+    expect(result).toEqual(amount)
+  })
+
+  test("should return the amount being deauthorized for the staking provider.", async () => {
+    const spy = jest
+      .spyOn(mockAppContract, "pendingAuthorizationDecrease")
+      .mockResolvedValue(amount)
+
+    const result = await application.pendingAuthorizationDecrease(
+      stakingProvider
+    )
+
+    expect(spy).toHaveBeenCalledWith(stakingProvider)
+    expect(result).toEqual(amount)
+  })
+
+  test("should return the the time until the deauthorization can be completed.", async () => {
+    const spy = jest
+      .spyOn(mockAppContract, "remainingAuthorizationDecreaseDelay")
+      .mockResolvedValue(time)
+
+    const result = await application.remainingAuthorizationDecreaseDelay(
+      stakingProvider
+    )
+
+    expect(spy).toHaveBeenCalledWith(stakingProvider)
+    expect(result).toEqual(time)
+  })
+
+  test("should return authorization decrease delay in seconds between the time authorization decrease is requested and the time the authorization decrease can be approved.", async () => {
+    const mockResult = {
+      authorizationDecreaseDelay: time,
+      minimumAuthorization: amount,
+      authorizationDecreaseChangePeriod: time,
+    }
+    const spy = jest
+      .spyOn(mockAppContract, "authorizationParameters")
+      .mockResolvedValue(mockResult)
+
+    const result = await application.authorizationDecreaseDelay()
+
+    expect(spy).toHaveBeenCalled()
+    expect(result).toEqual(mockResult.authorizationDecreaseDelay)
+  })
+
+  describe("rewards eligibility", () => {
+    test("should return true if the operator exists for a given staking provider and operator is in the pool.", async () => {
+      const operator = "0xeCbEb9B6Aa18dD3B57ECD061490ae5029c05EC91"
+      const isInPool = true
+      const spyOnOperatorMap = jest
+        .spyOn(mockAppContract, "stakingProviderToOperator")
+        .mockResolvedValue(operator)
+
+      const spyOnPool = jest
+        .spyOn(mockAppContract, "isOperatorInPool")
+        .mockResolvedValue(isInPool)
+
+      const result = await application.isEligibleForRewards(stakingProvider)
+
+      expect(spyOnOperatorMap).toHaveBeenCalledWith(stakingProvider)
+      expect(spyOnPool).toHaveBeenCalledWith(operator)
+      expect(result).toBeTruthy()
+    })
+
+    test("should return false if the operator doesn't exist for a given staking provider.", async () => {
+      const operator = AddressZero
+      const spyOnOperatorMap = jest
+        .spyOn(mockAppContract, "stakingProviderToOperator")
+        .mockResolvedValue(operator)
+
+      const spyOnPool = jest.spyOn(mockAppContract, "isOperatorInPool")
+
+      const result = await application.isEligibleForRewards(stakingProvider)
+
+      expect(spyOnOperatorMap).toHaveBeenCalledWith(stakingProvider)
+      expect(spyOnPool).not.toHaveBeenCalled()
+      expect(result).toBeFalsy()
+    })
+  })
+})
