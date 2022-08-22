0e6284c0cb672c9528cee050abfa97684fe1acae
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-08-22 16:37:13 +0200

    Create the Threshold lib
    Create a class that creates all services based on the config.

--- src/threshold-ts/index.ts
+++ src/threshold-ts/index.ts
@@ -1,2 +1,16 @@
-// TODO: Implement Threshold wrapper that includes all services
-export const Threhosld = {}
+import { MultiAppStaking } from "./mas"
+import { IMulticall, Multicall } from "./multicall"
+import { IStaking, Staking } from "./staking"
+import { ThresholdConfig } from "./types"
+
+export class Threshold {
+  multicall: IMulticall
+  staking: IStaking
+  multiAppStaking: MultiAppStaking
+
+  constructor(config: ThresholdConfig) {
+    this.multicall = new Multicall(config.ethereum)
+    this.staking = new Staking(config.ethereum)
+    this.multiAppStaking = new MultiAppStaking(this.staking, this.multicall)
+  }
+}
