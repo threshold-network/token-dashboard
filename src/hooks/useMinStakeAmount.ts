462143b0ce103e8f5acf42af0a64e12a05228508
George Weiler <georgejweiler@gmail.com>
2022-08-22 13:30:36 -0600

    Update validation for min stake so that 0 stake amounts for Gorli are still valid

--- src/hooks/useMinStakeAmount.ts
+++ src/hooks/useMinStakeAmount.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { setMinStake } from "../store/staking"
 import { useTStakingContract } from "../web3/hooks"
@@ -9,17 +9,26 @@ export const useMinStakeAmount = () => {
   const { minStakeAmount } = useStakingState()
   const dispatch = useDispatch()
 
+  const [isLoading, setLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
+
   useEffect(() => {
     const fetchMinStakeAmount = async () => {
+      setLoading(true)
       try {
         const minStakeAmount = await tStakingContract?.minTStakeAmount()
+        console.log("min loaded: " + minStakeAmount)
         dispatch(setMinStake({ amount: minStakeAmount.toString() }))
+        setLoading(false)
+        setHasError(false)
       } catch (error) {
+        setLoading(false)
+        setHasError(true)
         console.error("Could not fetch the min stake amount: ", error)
       }
     }
     if (minStakeAmount === "0" && tStakingContract) fetchMinStakeAmount()
   }, [tStakingContract, dispatch, minStakeAmount])
 
-  return minStakeAmount
+  return { minStakeAmount, isLoading, hasError }
 }
