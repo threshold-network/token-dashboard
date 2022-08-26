231b7aba38a01a7b03d44af9338c8eb88c879084
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-08-26 08:08:45 +0200

    Update the `MultiAppStaking` impl
    Add function that returns authorization parameters for all supported
    apps in a single call. Also, remove the functions that aggregate and
    return authorized stakes and authorization decrease requests for
    multiple staking providers because statistics show that only 15 owners
    have more than one stake so more effective is aggregating the 3
    important parameters for a one staking provider(get 3 values for a
    staking provider in one call) instead of creating 3 aggregated calls for
    multiple staking providers(3 calls to get 3 values if there is only 1
    staking providerv).

--- src/threshold-ts/mas/index.ts
+++ src/threshold-ts/mas/index.ts
@@ -1,20 +1,18 @@
 import { BigNumber } from "ethers"
 import RandomBeacon from "@keep-network/random-beacon/artifacts/RandomBeacon.json"
 import WalletRegistry from "@keep-network/ecdsa/artifacts/WalletRegistry.json"
-import { ContractCall } from "../../web3/utils"
-import { Application, IApplication } from "../applications"
-import { IMulticall } from "../multicall"
+import {
+  Application,
+  AuthorizationParameters,
+  IApplication,
+} from "../applications"
+import { IMulticall, ContractCall } from "../multicall"
 import { IStaking } from "../staking"
+import { EthereumConfig } from "../types"
 
-export interface AuthorizedStakes {
-  [stakingProvider: string]: BigNumber
-}
-
-export interface PendingAuthorizationDecreaseRequests {
-  [stakingProvider: string]: {
-    amount: BigNumber
-    remainingDelay: BigNumber
-  }
+export interface SupportedAppAuthorizationParameters {
+  tbtc: AuthorizationParameters
+  randomBeacon: AuthorizationParameters
 }
 
 export class MultiAppStaking {
@@ -23,139 +21,48 @@ export class MultiAppStaking {
   public readonly randomBeacon: IApplication
   public readonly ecdsa: IApplication
 
-  constructor(staking: IStaking, multicall: IMulticall) {
+  constructor(
+    staking: IStaking,
+    multicall: IMulticall,
+    config: EthereumConfig
+  ) {
     this._staking = staking
     this._multicall = multicall
-    this.randomBeacon = new Application(this._staking, {
+    this.randomBeacon = new Application(this._staking, this._multicall, {
       address: RandomBeacon.address,
       abi: RandomBeacon.abi,
+      ...config,
     })
-    this.ecdsa = new Application(this._staking, {
+    this.ecdsa = new Application(this._staking, this._multicall, {
       address: WalletRegistry.address,
       abi: WalletRegistry.abi,
+      ...config,
     })
   }
 
-  private async getAuthorizedStakes(
-    stakingProviders: string[],
-    application: string
-  ): Promise<AuthorizedStakes> {
-    const multicalls: ContractCall[] = stakingProviders.map(
-      (stakingProvider) => ({
-        contract: this._staking.stakingContract,
-        method: "authorizedStake",
-        args: [stakingProvider, application],
-      })
-    )
-    const result: BigNumber[] = await this._multicall.aggregate(multicalls)
-
-    return result.reduce(
-      (reducer, authorizedStake, index): AuthorizedStakes => {
-        reducer[stakingProviders[index]] = authorizedStake
-
-        return reducer
-      },
-      {} as AuthorizedStakes
-    )
-  }
-
-  async getTBTCAuthorizedStakes(
-    stakingProviders: string[]
-  ): Promise<AuthorizedStakes> {
-    return await this.getAuthorizedStakes(stakingProviders, this.ecdsa.address)
-  }
-
-  async getRandomBeaconAuthorizedStakes(
-    stakingProviders: string[]
-  ): Promise<AuthorizedStakes> {
-    return await this.getAuthorizedStakes(
-      stakingProviders,
-      this.randomBeacon.address
-    )
-  }
-
-  async getMinimumAppAuthorizations(): Promise<{
+  async getSupportedAppsAuthParameters(): Promise<{
     tbtc: BigNumber
     randomBeacon: BigNumber
   }> {
     const calls: ContractCall[] = [
       {
         contract: this.ecdsa.contract,
-        method: "minimumAuthorization",
+        method: "authorizationParameters",
         args: [],
       },
       {
         contract: this.randomBeacon.contract,
-        method: "minimumAuthorization",
+        method: "authorizationParameters",
         args: [],
       },
     ]
 
-    const [tbtcMinAuthorization, randomBeaconMinAuthorization] =
+    const [tbtcMinAuthorizationParams, randomBeaconMinAuthorizationParams] =
       await this._multicall.aggregate(calls)
 
     return {
-      tbtc: tbtcMinAuthorization,
-      randomBeacon: randomBeaconMinAuthorization,
+      tbtc: tbtcMinAuthorizationParams,
+      randomBeacon: randomBeaconMinAuthorizationParams,
     }
-  }
-
-  private async getPendingAuthorizationDecreseRequests(
-    stakingProviders: string[],
-    application: IApplication
-  ): Promise<PendingAuthorizationDecreaseRequests> {
-    if (stakingProviders.length === 0) return {}
-
-    const decreaseRequestsCalls: ContractCall[] = stakingProviders.map(
-      (stakingProvider) => ({
-        contract: application.contract,
-        method: "pendingAuthorizationDecrease",
-        args: [stakingProvider],
-      })
-    )
-
-    const remainingAuthorizationDecreaseDelayCalls: ContractCall[] =
-      stakingProviders.map((stakingProvider) => ({
-        contract: application.contract,
-        method: "remainingAuthorizationDecreaseDelay",
-        args: [stakingProvider],
-      }))
-
-    const decreaseRequests = await this._multicall.aggregate(
-      decreaseRequestsCalls
-    )
-    const delays = await this._multicall.aggregate(
-      remainingAuthorizationDecreaseDelayCalls
-    )
-
-    return stakingProviders.reduce(
-      (
-        reducer,
-        stakingProvider,
-        index
-      ): PendingAuthorizationDecreaseRequests => {
-        const amount = decreaseRequests[index]
-        const remainingDelay = delays[index]
-        reducer[stakingProvider] = { amount, remainingDelay }
-        return reducer
-      },
-      {} as PendingAuthorizationDecreaseRequests
-    )
-  }
-
-  async getRandomBeaconPendingAuthorizationDecreseRequests(
-    stakingProviders: string[]
-  ) {
-    return await this.getPendingAuthorizationDecreseRequests(
-      stakingProviders,
-      this.randomBeacon
-    )
-  }
-
-  async getTBTCPendingAuthorizationDecreseRequests(stakingProviders: string[]) {
-    return await this.getPendingAuthorizationDecreseRequests(
-      stakingProviders,
-      this.ecdsa
-    )
   }
 }
