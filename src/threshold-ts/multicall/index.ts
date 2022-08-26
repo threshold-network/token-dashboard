a153cd239d0b6730db4479ca9f06c09903f1bfbe
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-08-26 11:49:59 +0200

    Add docs and unit tests for multicall

--- src/threshold-ts/multicall/index.ts
+++ src/threshold-ts/multicall/index.ts
@@ -8,15 +8,27 @@ export interface ContractCall {
   args?: any[]
 }
 
+/**
+ * Represents a multicall contract wrapper. @see
+ * {@link https://github.com/makerdao/multicall#multicall}
+ */
 export interface IMulticall {
-  aggregate(calls: ContractCall[]): Promise<any>
+  /**
+   * Allows multiple smart contract constant function calls to be grouped into a
+   * single call and the results aggregated into a single result. This reduces
+   * the number of separate JSON RPC requests that need to be sent.
+   * @param calls An array of contract calls represented by @see
+   * {@link ContractCall}.
+   * @returns Contract calls results aggregated into a single result.
+   */
+  aggregate(calls: ContractCall[]): Promise<any[]>
 }
 
-const MULTICALL_ABI = [
+export const MULTICALL_ABI = [
   "function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)",
   "function getEthBalance(address addr) view returns (uint256 balance)",
 ]
-const MULTICALL_ADDRESSESS = {
+export const MULTICALL_ADDRESSESS = {
   1: "0xeefba1e63905ef1d7acba5a8513c70307c1ce441",
   5: "0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e",
   1337: process.env.REACT_APP_MULTICALL_ADDRESS || AddressZero,
