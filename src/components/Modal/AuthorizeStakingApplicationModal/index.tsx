2586ce30edd08538b4a075225560f8af8961c5a0
George Weiler <georgejweiler@gmail.com>
2022-09-01 08:37:26 -0600

    Copy authorization card component from staking page into authorization modal

--- src/components/Modal/AuthorizeStakingApplicationModal/index.tsx
+++ src/components/Modal/AuthorizeStakingApplicationModal/index.tsx
@@ -1,16 +1,21 @@
 import { FC, useState, useMemo } from "react"
 import {
   Button,
+  HStack,
   ModalBody,
   ModalCloseButton,
   ModalFooter,
   ModalHeader,
-} from "@chakra-ui/react"
-import { BodyLg, H5 } from "@threshold-network/components"
+  BodyLg,
+  H5,
+  BodyMd,
+} from "@threshold-network/components"
 import InfoBox from "../../InfoBox"
 import { BaseModalProps } from "../../../types"
 import { StakeData } from "../../../types/staking"
 import withBaseModal from "../withBaseModal"
+import AuthorizationCard from "./AuthorizationCard"
+import TokenBalance from "../../TokenBalance"
 
 const AuthorizeStakingApplicationModal: FC<
   BaseModalProps & { stake: StakeData }
@@ -33,6 +38,40 @@ const AuthorizeStakingApplicationModal: FC<
             at any time.
           </BodyLg>
         </InfoBox>
+
+        <HStack>
+          <BodyMd>Total Staked Balance</BodyMd>
+          <TokenBalance
+            tokenAmount={stake?.totalInTStake}
+            tokenSymbol="T"
+            withSymbol
+          />
+        </HStack>
+
+        <AuthorizationCard
+          appAuthData={{
+            label: "idk",
+            isAuthorized: false,
+            percentage: 100,
+            isAuthRequired: true,
+          }}
+          isSelected
+          onCheckboxClick={() => {}}
+          maxAuthAmount={stake?.totalInTStake}
+          minAuthAmount={"1"}
+        />
+        <AuthorizationCard
+          appAuthData={{
+            label: "idk",
+            isAuthorized: false,
+            percentage: 100,
+            isAuthRequired: true,
+          }}
+          isSelected
+          onCheckboxClick={() => {}}
+          maxAuthAmount={stake?.totalInTStake}
+          minAuthAmount={"1"}
+        />
       </ModalBody>
       <ModalFooter>
         <Button onClick={closeModal} variant="outline" mr={2}>
