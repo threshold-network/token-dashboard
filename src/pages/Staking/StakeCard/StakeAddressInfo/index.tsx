11151f0e3dc4f5c83cbf6c50d6e0830048d713d6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-09-13 14:23:40 +0200

    Update `StakeAddressInfo` component
    Add `FlexProps` so we can change the styles for the main wrapper.

--- src/pages/Staking/StakeCard/StakeAddressInfo/index.tsx
+++ src/pages/Staking/StakeCard/StakeAddressInfo/index.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react"
-import { BoxLabel, Flex } from "@threshold-network/components"
+import { BoxLabel, Flex, FlexProps } from "@threshold-network/components"
 import { CopyAddressToClipboard } from "../../../../components/CopyToClipboard"
 import { StakeData } from "../../../../types"
 
-const StakeAddressInfo: FC<{ stake: StakeData }> = ({ stake }) => {
+const StakeAddressInfo: FC<{ stake: StakeData } & FlexProps> = ({
+  stake,
+  ...restProps
+}) => {
   return (
-    <Flex mt="6" mb="8" alignItems="center">
+    <Flex mt="6" mb="8" alignItems="center" {...restProps}>
       <BoxLabel bg="brand.50" color="brand.700" mr="auto">
         Provider address
       </BoxLabel>
