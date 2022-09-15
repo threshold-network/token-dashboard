1c822323b3bc62ed52588bb1d678486179409535
George Weiler <georgejweiler@gmail.com>
2022-09-14 18:50:05 -0600

    Simplifies the helper text logic

--- src/components/Forms/HelperErrorText.tsx
+++ src/components/Forms/HelperErrorText.tsx
@@ -7,20 +7,14 @@ const HelperErrorText: FC<{
   helperText?: string | JSX.Element
 }> = ({ errorMsgText, helperText, hasError }) => {
   if (hasError) {
-    return typeof errorMsgText === "string" ? (
-      <FormErrorMessage>{errorMsgText}</FormErrorMessage>
-    ) : (
-      errorMsgText || (
-        <FormErrorMessage>Please enter a valid value</FormErrorMessage>
-      )
-    )
-  } else {
-    return typeof helperText === "string" ? (
-      <FormHelperText>{helperText}</FormHelperText>
-    ) : (
-      helperText || null
+    return (
+      <FormErrorMessage>
+        {errorMsgText || "Please enter a valid value"}
+      </FormErrorMessage>
     )
   }
+
+  return helperText ? <FormHelperText>{helperText}</FormHelperText> : null
 }
 
 export default HelperErrorText
