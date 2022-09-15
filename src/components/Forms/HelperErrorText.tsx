430a309d92159d34a113ab12bd0f4e1f8b226cde
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-09-15 10:52:41 +0200

    Fix import
    We should import components from `@threshold-network/components`.

--- src/components/Forms/HelperErrorText.tsx
+++ src/components/Forms/HelperErrorText.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
-import { FormErrorMessage, FormHelperText } from "@chakra-ui/react"
+import { FormErrorMessage, FormHelperText } from "@threshold-network/components"
 
 const HelperErrorText: FC<{
   errorMsgText?: string | JSX.Element
