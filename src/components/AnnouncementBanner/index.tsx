e162e44bfa92261fc06128ddd90877038f9e25f7
George Weiler <georgejweiler@gmail.com>
2022-09-19 14:08:11 -0400

    Adds secondary variant to Announcement banner

--- src/components/AnnouncementBanner/index.tsx
+++ src/components/AnnouncementBanner/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { Link as RouterLink } from "react-router-dom"
 import {
   Card,
@@ -8,6 +8,8 @@ import {
   Stack,
   useDisclosure,
   Image,
+  BoxProps,
+  useColorMode,
 } from "@threshold-network/components"
 
 interface AnnouncementBannerProps {
@@ -15,15 +17,32 @@ interface AnnouncementBannerProps {
   title: string
   buttonText: string
   href: string
+  variant?: "primary" | "secondary"
+  size?: "sm" | "lg"
+  hideCloseBtn?: boolean
 }
 
-const AnnouncementBanner: FC<AnnouncementBannerProps> = ({
+const AnnouncementBanner: FC<AnnouncementBannerProps & BoxProps> = ({
   title,
   imgSrc,
   buttonText,
   href,
+  variant = "primary",
+  size = "sm",
+  hideCloseBtn = false,
+  ...props
 }) => {
   const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true })
+
+  const { colorMode } = useColorMode()
+  const darkMode = colorMode === "dark"
+
+  const bg = useMemo(() => {
+    if (variant === "secondary") {
+      return darkMode ? "gray.800" : "brand.50"
+    }
+    return "white"
+  }, [colorMode, variant])
 
   return (
     <Card
@@ -32,21 +51,29 @@ const AnnouncementBanner: FC<AnnouncementBannerProps> = ({
       position="relative"
       px="16"
       mb={4}
+      bg={bg}
+      {...props}
     >
-      <CloseButton
-        position="absolute"
-        right="14px"
-        top="12px"
-        onClick={onClose}
-      />
+      {!hideCloseBtn && (
+        <CloseButton
+          position="absolute"
+          right="14px"
+          top="12px"
+          onClick={onClose}
+        />
+      )}
       <Stack
         alignItems="center"
         spacing={{ base: "8", xl: "16" }}
         direction={{ base: "column", xl: "row" }}
         bg="inherit"
       >
-        <Image maxW="146px" src={imgSrc} />
-        <H4 textAlign={{ base: "center", xl: "unset" }} maxW="460px">
+        <Image maxW={size == "sm" ? "146px" : "280px"} src={imgSrc} />
+        <H4
+          textAlign={{ base: "center", xl: "unset" }}
+          maxW="460px"
+          color={darkMode ? "white" : "gray.700"}
+        >
           {title}
         </H4>
         <Button
