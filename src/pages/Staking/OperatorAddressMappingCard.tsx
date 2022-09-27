bcbbf001620875d2437a5c27e3c0e6574c18de7c
michalsmiarowski <michal.smiarowski@thesis.co>
2022-09-27 13:22:22 +0200

    Add additional styles for operator mapping card
    Add additional styles for operator mapping card when only one app needs mapping.

--- src/pages/Staking/OperatorAddressMappingCard.tsx
+++ src/pages/Staking/OperatorAddressMappingCard.tsx
@@ -9,29 +9,38 @@ import {
 } from "@threshold-network/components"
 import { LabelSm } from "@threshold-network/components"
 import { ModalType } from "../../enums"
+import { useOperatorMappedtoStakingProviderHelpers } from "../../hooks/staking-applications/useOperatorMappedToStakingProviderHelpers"
 import { useModal } from "../../hooks/useModal"
 
 const OperatorAddressMappingCard = () => {
   const { openModal } = useModal()
+  const { isOperatorMappedOnlyInRandomBeacon, isOperatorMappedOnlyInTbtc } =
+    useOperatorMappedtoStakingProviderHelpers()
+  const isOneOfTheAppsNotMapped =
+    isOperatorMappedOnlyInRandomBeacon || isOperatorMappedOnlyInTbtc
 
   const onStartMappingClick = () => {
     openModal(ModalType.MapOperatorToStakingProvider)
   }
 
   return (
-    <Card>
+    <Card borderColor={isOneOfTheAppsNotMapped ? "red.500" : "gray.100"}>
       <HStack justifyContent={"space-between"}>
         <LabelSm>Operator Address Mapping</LabelSm>
         <Badge variant={"solid"} size={"sm"}>
           Node operators only
         </Badge>
       </HStack>
-      <Alert status={"warning"} alignItems={"center"} mt={5}>
+      <Alert
+        status={isOneOfTheAppsNotMapped ? "error" : "warning"}
+        alignItems={"center"}
+        mt={5}
+      >
         <AlertIcon alignSelf="auto" />
         <BodyXs>
-          This section is for Staking Providers and self-operating stakers only.
-          Map your Operator Address to your Provider Address for a better
-          support for your hardware wallet in the node setup.
+          {isOneOfTheAppsNotMapped
+            ? "One application from the tBTC + Random Beacon Rewards Bundle Suite requires the Operator Address mapped."
+            : "This section is for Staking Providers and self-operating stakers only. Map your Operator Address to your Provider Address for a better support for your hardware wallet in the node setup."}
         </BodyXs>
       </Alert>
       <Button size="lg" w="100%" mt="5" onClick={onStartMappingClick}>
