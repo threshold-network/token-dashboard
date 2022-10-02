3dc60ea86d5900e844fddbf28147e9302835efc8
michalsmiarowski <michal.smiarowski@thesis.co>
2022-10-02 21:52:28 +0200

    Fix display operator mapping effect
    There was a bug where when you close the login modal too quick, before
    `setStakes` action was dispatched, then the effect got blocked on `await
    listenerApi.condition` and was waiting for another action to dispatch and there
    were not any at that particular moment.
    
    To fix that I've added a condition to check if login modal was closed, if not,
    then we wait with the `condition()` function since the
    `displayMapOperatorToStakingProviderModalEffect` effect is run on a `setStakes`
    action, so we have all needed data fetcher already.

--- src/store/staking-applications/effects.ts
+++ src/store/staking-applications/effects.ts
@@ -171,9 +171,12 @@ export const displayMapOperatorToStakingProviderModalEffect = async (
   action: AnyAction,
   listenerApi: AppListenerEffectAPI
 ) => {
-  const result = await listenerApi.condition((action, currentState: any) => {
-    return currentState.modalQueue.isSuccessfullLoginModalClosed
-  })
+  const { modalQueue } = listenerApi.getState()
+  if (!modalQueue.isSuccessfullLoginModalClosed) {
+    await listenerApi.condition((action, currentState: any) => {
+      return currentState.modalQueue.isSuccessfullLoginModalClosed
+    })
+  }
   const { connectedAccount, staking } = listenerApi.getState()
   const { address } = connectedAccount
   if (address) {
