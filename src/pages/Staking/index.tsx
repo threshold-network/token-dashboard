eb19f50d303e7733efd4fd508ab49821243c78fd
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-10-17 13:13:33 +0200

    Update breadcrumb copy on staking page
    Also use the `Breadcrumb` component.

--- src/pages/Staking/index.tsx
+++ src/pages/Staking/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo } from "react"
-import { HStack, Stack, VStack } from "@chakra-ui/react"
 import StakingTVLCard from "./StakingTVLCard"
 import StakedPortfolioCard from "./StakedPortfolioCard"
 import PageLayout from "../PageLayout"
@@ -18,11 +17,20 @@ import AuthorizeStakingAppsPage from "./AuthorizeStakingApps"
 import {
   FilterTabs,
   FilterTab,
-  BodyLg,
   H4,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  HStack,
+  VStack,
+  useColorModeValue,
 } from "@threshold-network/components"
-import { Link, Outlet, useLocation, useParams } from "react-router-dom"
-import { Link as RouterLink } from "react-router-dom"
+import {
+  Link as RouterLink,
+  Outlet,
+  useLocation,
+  useParams,
+} from "react-router-dom"
 import { stakingApplicationsSlice } from "../../store/staking-applications/slice"
 import StakeDetailsPage from "./StakeDetailsPage"
 import NewStakeCard from "./NewStakeCard"
@@ -137,16 +145,29 @@ const StakingProviderDetails: PageComponent = (props) => {
     }
   }, [pathname])
 
+  const breadcrumbColor = useColorModeValue("gray.700", "gray.300")
+
   return (
     <>
-      <BodyLg mb={5}>
-        <Link to={"/staking"}>Staking</Link>
-        {" > "}
-        <Link to={pathname}>
-          Stake{" "}
-          {lastElementOfTheUrl === "authorize" ? "applications" : "details"}
-        </Link>
-      </BodyLg>
+      <Breadcrumb
+        separator=">"
+        textStyle="bodyLg"
+        color={breadcrumbColor}
+        mb="10"
+      >
+        <BreadcrumbItem>
+          <BreadcrumbLink as={RouterLink} to="/staking">
+            Staking
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={RouterLink} to={pathname}>
+            {lastElementOfTheUrl === "authorize"
+              ? "Authorize Applications"
+              : "Stake Details"}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
       <FilterTabs selectedTabId={selectedTabId} mb="5" size="lg">
         <FilterTab
           tabId={"1"}
@@ -160,7 +181,7 @@ const StakingProviderDetails: PageComponent = (props) => {
           as={RouterLink}
           to={`/staking/${stakingProviderAddress}/authorize`}
         >
-          Authorize Application
+          Authorize Applications
         </FilterTab>
       </FilterTabs>
       <Outlet />
