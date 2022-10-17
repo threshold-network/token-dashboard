0e055e9384bd7686f11f0038c4ad808372085693
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-10-17 14:06:18 +0200

    Update the info tooltip component
    To match the TDS

--- src/components/TooltipIcon.tsx
+++ src/components/TooltipIcon.tsx
@@ -1,22 +1,31 @@
-import { IoInformationCircleSharp } from "react-icons/all"
-import { Icon, IconProps, Tooltip } from "@chakra-ui/react"
+import { IoInformationCircleOutline } from "react-icons/all"
+import { Icon, IconProps, Tooltip, useColorModeValue } from "@chakra-ui/react"
 import { FC } from "react"
+import { ONE_SEC_IN_MILISECONDS } from "../utils/date"
 
-const TooltipIcon: FC<{ label: string } & IconProps> = ({
+const TooltipIcon: FC<{ label: string | JSX.Element } & IconProps> = ({
   label,
   css,
   ...iconProps
 }) => {
+  const bgColor = useColorModeValue("white", "gray.300")
+  const textColor = useColorModeValue("gray.700", "gray.900")
+  const iconColor = useColorModeValue("gray.500", "gray.300")
+
   return (
-    <Tooltip label={label} fontSize="md">
-      <span>
-        <Icon
-          color="gray.500"
-          boxSize="16px"
-          as={IoInformationCircleSharp}
-          {...iconProps}
-        />
-      </span>
+    <Tooltip
+      label={label}
+      fontSize="md"
+      placement="top"
+      hasArrow
+      bg={bgColor}
+      color={textColor}
+      p="2"
+      closeDelay={ONE_SEC_IN_MILISECONDS}
+      borderRadius="1"
+      shouldWrapChildren
+    >
+      <Icon color={iconColor} as={IoInformationCircleOutline} {...iconProps} />
     </Tooltip>
   )
 }
