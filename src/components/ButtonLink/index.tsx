8d3dcb84108b2b560d9e32537761ea783d80e9c7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-10-18 14:30:22 +0200

    Simplify the ButtonLink component
    Use `Link` component so there is no need to duplicate the same logic for
    external and internal links.

--- src/components/ButtonLink/index.tsx
+++ src/components/ButtonLink/index.tsx
@@ -1,36 +1,14 @@
 import { FC } from "react"
-import { Link as RouterLink } from "react-router-dom"
-import {
-  Button,
-  ButtonProps,
-  forwardRef,
-  Icon,
-} from "@threshold-network/components"
-import { FiArrowUpRight } from "react-icons/all"
-import { LinkProps } from "../Link"
+import { Button, ButtonProps, forwardRef } from "@threshold-network/components"
+import Link, { LinkProps } from "../Link"
+
+const style = {
+  textDecoration: "none",
+}
 
 const ButtonLink: FC<ButtonProps & LinkProps> = forwardRef(
-  ({ isExternal, href, to, icon, children, ...props }, ref) => {
-    return (
-      <Button
-        as={isExternal ? "a" : RouterLink}
-        ref={isExternal ? ref : undefined}
-        href={isExternal ? href : undefined}
-        to={isExternal ? undefined : to}
-        target={isExternal ? "_blank" : undefined}
-        rel={isExternal ? "noopener noreferrer" : undefined}
-        rightIcon={
-          icon ? (
-            icon
-          ) : isExternal ? (
-            <Icon boxSize="12px" ml="1" as={FiArrowUpRight} />
-          ) : undefined
-        }
-        {...props}
-      >
-        {children}
-      </Button>
-    )
+  ({ ...props }, ref) => {
+    return <Button as={Link} ref={ref} style={style} {...props} />
   }
 )
 
