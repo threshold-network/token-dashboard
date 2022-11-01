18df46d0a57bb4f6ea60fb3212c22ce7d36fdea1
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-11-01 13:16:57 +0100

    Move contract related fns to the threshold-ts lib

--- src/utils/getContract.ts
+++ src/utils/getContract.ts
@@ -1,19 +1,5 @@
-import { AddressZero } from "../web3/utils/address"
-export { getSigner, getContract } from "../threshold-ts/utils/contract"
-
-export function getContractAddressFromTruffleArtifact(
-  truffleArtifact: { networks: { [chainID: string]: { address: string } } },
-  chainID: string | undefined = undefined
-) {
-  const networks = Object.keys(truffleArtifact.networks) as Array<
-    keyof typeof truffleArtifact.networks
-  >
-
-  return networks && networks.length > 0
-    ? (
-        truffleArtifact.networks[chainID ? chainID : networks[0]] as {
-          address: string
-        }
-      ).address
-    : AddressZero
-}
+export {
+  getSigner,
+  getContract,
+  getContractAddressFromTruffleArtifact,
+} from "../threshold-ts/utils/contract"
--- src/web3/utils/events.ts
+++ src/web3/utils/events.ts
@@ -1,18 +1,1 @@
-import { Contract, Event } from "@ethersproject/contracts"
-import { BlockTag } from "@ethersproject/abstract-provider"
-
-interface Options {
-  fromBlock?: BlockTag
-  toBlock?: BlockTag
-  filterParams: any[]
-  eventName: string
-}
-
-export const getContractPastEvents = async (
-  contract: Contract,
-  options: Options
-): Promise<Array<Event>> => {
-  const filter = contract.filters[options.eventName](...options.filterParams)
-
-  return await contract.queryFilter(filter, options.fromBlock, options.toBlock)
-}
+export { getContractPastEvents } from "../../threshold-ts/utils"
