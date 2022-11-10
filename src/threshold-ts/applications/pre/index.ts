527d535e37c32edda6586125e848d87e4caf44c1
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-11-10 14:53:31 +0100

    Move fetching the PRE app data to ts-lib and redux
    The PRE is a seprate app so should be stored in the `staking-apps` slice
    not as a part of the stake data.

---
+++ src/threshold-ts/applications/pre/index.ts
@@ -0,0 +1,101 @@
+import { BigNumber, Contract } from "ethers"
+import SimplePREApplicationABI from "./abi.json"
+import { AddressZero, isAddressZero, getContract } from "../../utils"
+import { EthereumConfig } from "../../types"
+
+const PRE_ADDRESSESS = {
+  // https://etherscan.io/address/0x7E01c9c03FD3737294dbD7630a34845B0F70E5Dd
+  1: "0x7E01c9c03FD3737294dbD7630a34845B0F70E5Dd",
+  // https://goerli.etherscan.io/address/0x829fdCDf6Be747FEA37518fBd83dF70EE371fCf2
+  // As NuCypher hasn't depoyed the `SimplePreApplication` contract on Goerli,
+  // we're using a stub contract.
+  5: "0x829fdCDf6Be747FEA37518fBd83dF70EE371fCf2",
+  // Set the correct `SimplePREApplication` contract address. If you deployed
+  // the `@threshold-network/solidity-contracts` to your local chain and linked
+  // package using `yarn link @threshold-network/solidity-contracts` you can
+  // find the contract address at
+  // `node_modules/@threshold-network/solidity-contracts/artifacts/SimplePREApplication.json`.
+  1337: AddressZero,
+} as Record<string, string>
+
+export interface StakingProviderInfo {
+  /**
+   * Operator address mapped to a given staking provider/
+   */
+  operator: string
+  /**
+   * Determines if the operator is confirmed.
+   */
+  isOperatorConfirmed: boolean
+  /**
+   * Timestamp where operator were bonded.
+   */
+  operatorStartTimestamp: string
+  /**
+   * Determines if the operator for the given staking provider is
+   * mapped.
+   */
+  isOperatorMapped: boolean
+}
+
+// NOTE: The simple PRE application contract doesn't implement the application
+// interface so we can't use the same interface as for the Keep staking apps.
+export interface IPRE {
+  /**
+   * Application address.
+   */
+  address: string
+
+  /**
+   * Application contract.
+   */
+  contract: Contract
+
+  getStakingProviderAppInfo: (
+    stakingProvider: string
+  ) => Promise<StakingProviderInfo>
+}
+
+export class PRE implements IPRE {
+  private _application: Contract
+
+  constructor(config: EthereumConfig) {
+    const address = PRE_ADDRESSESS[config.chainId]
+    if (!address) {
+      throw new Error("Unsupported chain id")
+    }
+
+    this._application = getContract(
+      address,
+      SimplePREApplicationABI,
+      config.providerOrSigner,
+      config.account
+    )
+  }
+  getStakingProviderAppInfo = async (
+    stakingProvider: string
+  ): Promise<StakingProviderInfo> => {
+    const operatorInfo = (await this._application.stakingProviderInfo(
+      stakingProvider
+    )) as {
+      operator: string
+      operatorConfirmed: boolean
+      operatorStartTimestamp: BigNumber
+    }
+
+    return {
+      operator: operatorInfo.operator,
+      isOperatorConfirmed: operatorInfo.operatorConfirmed,
+      operatorStartTimestamp: operatorInfo.operatorStartTimestamp.toString(),
+      isOperatorMapped:
+        !isAddressZero(operatorInfo.operator) && operatorInfo.operatorConfirmed,
+    }
+  }
+
+  get address() {
+    return this._application.address
+  }
+  get contract() {
+    return this._application
+  }
+}
