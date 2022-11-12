ba3a8b66a0551040a0b87e2cceb8ee9ec3aea122
George Weiler <georgejweiler@gmail.com>
2022-11-12 16:35:03 -0700

    Set up initial structure for the feedback module page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import Overview from "./Overview"
 import UpgradePage from "./Upgrade"
 import Staking from "./Staking"
 import TBTC from "./tBTC"
+import Feedback from "./Feedback"
 
-export const pages = [Overview, UpgradePage, Staking, TBTC]
+export const pages = [Overview, UpgradePage, Staking, TBTC, Feedback]
