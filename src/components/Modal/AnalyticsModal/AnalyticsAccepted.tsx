bce9eb1c7d80fc02a8402b4e60d4bcee4d7ae220
George Weiler <georgejweiler@gmail.com>
2022-11-15 13:52:52 -0700

    Builds the success and rejected analytics modals

--- src/components/Modal/AnalyticsModal/AnalyticsAccepted.tsx
+++ src/components/Modal/AnalyticsModal/AnalyticsAccepted.tsx
@@ -1,12 +1,39 @@
 import { FC } from "react"
-import { Button, ModalBody, ModalFooter } from "@threshold-network/components"
+import {
+  Alert,
+  AlertIcon,
+  BodyMd,
+  BodyLg,
+  Button,
+  ModalBody,
+  ModalFooter,
+  H5,
+} from "@threshold-network/components"
+import InfoBox from "../../InfoBox"
+import Link from "../../Link"
+import { useModal } from "../../../hooks/useModal"
 
 const AnalyticsAccepted: FC = () => {
+  const { closeModal } = useModal()
+
   return (
     <>
-      <ModalBody>Accept</ModalBody>
+      <ModalBody>
+        <Alert status="success">
+          <AlertIcon />
+          <BodyMd>Analytics are turned on</BodyMd>
+        </Alert>
+        <InfoBox p={6}>
+          <H5 mb={4}>Thanks for opting in!</H5>
+          <BodyLg>
+            This will help us to make the product even better. You can change
+            the analytics setting at any time in{" "}
+            <Link to="/feedback/settings">Feedback Settings</Link>.
+          </BodyLg>
+        </InfoBox>
+      </ModalBody>
       <ModalFooter>
-        <Button>Accept</Button>
+        <Button onClick={closeModal}>Dismiss</Button>
       </ModalFooter>
     </>
   )
--- src/components/Modal/AnalyticsModal/AnalyticsRejected.tsx
+++ src/components/Modal/AnalyticsModal/AnalyticsRejected.tsx
@@ -1,12 +1,38 @@
 import { FC } from "react"
-import { Button, ModalBody, ModalFooter } from "@threshold-network/components"
+import {
+  Alert,
+  AlertIcon,
+  BodyMd,
+  BodyLg,
+  Button,
+  ModalBody,
+  ModalFooter,
+  H5,
+} from "@threshold-network/components"
+import InfoBox from "../../InfoBox"
+import Link from "../../Link"
+import { useModal } from "../../../hooks/useModal"
 
 const AnalyticsRejected: FC = () => {
+  const { closeModal } = useModal()
+
   return (
     <>
-      <ModalBody>Reject</ModalBody>
+      <ModalBody>
+        <Alert status="success">
+          <AlertIcon />
+          <BodyMd>Analytics are turned off</BodyMd>
+        </Alert>
+        <InfoBox p={6}>
+          <H5 mb={4}>You have opted out of analytics</H5>
+          <BodyLg>
+            If you change your mind, you can turn analytics on at any time in{" "}
+            <Link to="/feedback/settings">Feedback Settings</Link>.
+          </BodyLg>
+        </InfoBox>
+      </ModalBody>
       <ModalFooter>
-        <Button>Reject</Button>
+        <Button onClick={closeModal}>Dismiss</Button>
       </ModalFooter>
     </>
   )
