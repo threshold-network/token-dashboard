2986d8e4a5406c7699dd65f3dfdeb9a7e7fa0b80
George Weiler <georgejweiler@gmail.com>
2022-12-02 17:50:12 -0700

    Adds the analtyics consent to localstorage

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -8,15 +8,21 @@ import { PageComponent } from "../../types"
 import { AuthorizeApplicationsBanner } from "./AuthorizeApplicationsBanner"
 import { featureFlags } from "../../constants"
 import AnalyticsBanner from "./AnalyticsBanner"
+import { useAnalytics } from "../../hooks/useAnalytics"
 
 const Overview: PageComponent = () => {
   useDocumentTitle("Threshold - Overview")
+
+  const { isAnalyticsEnabled, hasUserResponded } = useAnalytics()
+  console.log("hasUserResponded", hasUserResponded)
 
   return (
     <Container maxW={{ base: "2xl", xl: "6xl" }} my={16}>
       <Image src={thresholdWordMark} mb={4} />
       <H1 mb={12}>Overview</H1>
-      {featureFlags.FEEDBACK_MODULE && <AnalyticsBanner />}
+      {featureFlags.FEEDBACK_MODULE &&
+        !isAnalyticsEnabled &&
+        !hasUserResponded && <AnalyticsBanner />}
       {featureFlags.MULTI_APP_STAKING && <AuthorizeApplicationsBanner />}
       <Outlet />
     </Container>
