6dff6ec09088db8e091aaec44b5685ebdab8e1ff
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-05 07:27:14 +0100

    Update `useInitiateDeauthorization` hook
    There was a bug- if a user declines `requestAuthorizationDecrease` tx
    the dapp triggers `updateOperatorStatus`. In that case we should display
    an error modal and end the flow. There is no need to call
    `updateOperatorStatus` w/o `requestAuthorizationDecrease`. This commit
    will fix it.

--- src/hooks/staking-applications/useInitiateDeauthorization.ts
+++ src/hooks/staking-applications/useInitiateDeauthorization.ts
@@ -1,9 +1,11 @@
 import { ContractTransaction } from "ethers"
 import { useCallback } from "react"
 import { useThreshold } from "../../contexts/ThresholdContext"
+import { ModalType } from "../../enums"
 import { StakingAppName } from "../../store/staking-applications"
 import { useSendTransactionFromFn } from "../../web3/hooks"
 import { isAddressZero } from "../../web3/utils"
+import { useModal } from "../useModal"
 import { stakingAppNameToThresholdAppService } from "./useStakingAppContract"
 import { useUpdateOperatorStatus } from "./useUpdateOperatorStatus"
 
@@ -13,11 +15,25 @@ export const useInitiateDeauthorization = (
   onSuccess?: (tx: ContractTransaction) => void | Promise<void>,
   onError?: (error: any) => void | Promise<void>
 ) => {
+  const { openModal } = useModal()
   const threshold = useThreshold()
   const {
     sendTransaction: updateOperatorStatus,
     status: updateOperatorTxStatus,
   } = useUpdateOperatorStatus(appName)
+
+  const onErrorRequestAuthorizationDecrease = useCallback(
+    (error) => {
+      onError?.(error)
+      openModal(ModalType.TransactionFailed, {
+        transactionHash: error?.transaction?.hash,
+        error,
+        isExpandableError: true,
+      })
+      throw error
+    },
+    [onError, openModal]
+  )
 
   const {
     sendTransaction: requestAuthorizationDecrease,
@@ -26,12 +42,16 @@ export const useInitiateDeauthorization = (
     threshold.multiAppStaking[stakingAppNameToThresholdAppService[appName]]
       .requestAuthorizationDecrease,
     onSuccess,
-    onError
+    onErrorRequestAuthorizationDecrease
   )
 
   const sendTransaction = useCallback(
     async (stakingProvider: string, amount: string, operator?: string) => {
-      await requestAuthorizationDecrease(stakingProvider, amount)
+      try {
+        await requestAuthorizationDecrease(stakingProvider, amount)
+      } catch (error) {
+        return
+      }
 
       if (!shouldUpdateOperatorStatusAfterInitiation) return
       if (!operator || isAddressZero(operator))
