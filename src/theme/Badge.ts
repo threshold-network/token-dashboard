92ed7d2a4c0dfb1a6c74f4c4160ee05515073244
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-09 14:38:03 +0100

    Update `Badge` theme
    Add `sm` size and update the `subtle` variant to match TDS.

--- src/theme/Badge.ts
+++ src/theme/Badge.ts
@@ -1,4 +1,8 @@
-import { SystemStyleFunction } from "@chakra-ui/theme-tools"
+import {
+  mode,
+  SystemStyleFunction,
+  SystemStyleObject,
+} from "@chakra-ui/theme-tools"
 import { defaultTheme } from "@threshold-network/components"
 
 // TODO: probably we should add a new color schema: `magic` and use
@@ -15,9 +19,32 @@ const variantMagic: SystemStyleFunction = (props) => {
 const variants = {
   ...defaultTheme.components.Badge.variants,
   magic: variantMagic,
+  subtle: (props: any) => {
+    const { colorScheme: c, theme } = props
+    const whiteAlpha200 = "rgba(255, 255, 255, 0.08);"
+
+    return {
+      bg: mode(`${c}.50`, whiteAlpha200)(props),
+      color: mode(`${c}.500`, `${c}.200`)(props),
+    }
+  },
+}
+
+const sizes: Record<string, SystemStyleObject> = {
+  sm: {
+    px: "2",
+    py: "1",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "10px",
+    lineHeight: "12px",
+    letterSpacing: "0.075em",
+    textTransform: "uppercase",
+  },
 }
 
 export const Badge = {
   ...defaultTheme.components.Badge,
   variants,
+  sizes,
 }
