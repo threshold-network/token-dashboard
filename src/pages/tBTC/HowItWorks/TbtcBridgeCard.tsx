36945c2bb3aaffdcc1b7bf0067ae60e21e351f0d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-19 14:38:40 +0100

    Update the bridge card on the how it works page
    Accordgin to Figma views.

--- src/pages/tBTC/HowItWorks/TbtcBridgeCard.tsx
+++ src/pages/tBTC/HowItWorks/TbtcBridgeCard.tsx
@@ -1,17 +1,36 @@
 import { FC, ComponentProps } from "react"
-import { Stack } from "@chakra-ui/react"
-import { BodyMd, LabelSm, Card } from "@threshold-network/components"
+import {
+  BodyMd,
+  LabelSm,
+  Card,
+  Wrap,
+  WrapItem,
+  Image,
+  useColorModeValue,
+  Box,
+} from "@threshold-network/components"
+import tbtcAppIllustrationLight from "../../../static/images/tbtcAppIllustrationLight.svg"
+import tbtcAppIllustrationDark from "../../../static/images/tbtcAppIllustrationDark.svg"
 
 export const TbtcBridgeCard: FC<ComponentProps<typeof Card>> = ({
   ...props
 }) => {
+  const tbtcAppImg = useColorModeValue(
+    tbtcAppIllustrationLight,
+    tbtcAppIllustrationDark
+  )
   return (
-    <Card {...props}>
-      <LabelSm mb={5} textTransform="none">
-        tBTC BRIDGE
-      </LabelSm>
-      <Stack spacing={5}>
-        <BodyMd>
+    <Card
+      {...props}
+      display="flex"
+      flexDirection={{ base: "column", xl: "row" }}
+      p="8"
+    >
+      <Box mr={{ base: "none", lg: "20" }}>
+        <LabelSm mb={5} textTransform="none">
+          tBTC BRIDGE
+        </LabelSm>
+        <BodyMd mb="5">
           The second generation of tBTC is a truly decentralized bridge between
           Bitcoin and Ethereum.
         </BodyMd>
@@ -19,10 +38,9 @@ export const TbtcBridgeCard: FC<ComponentProps<typeof Card>> = ({
           By providing Bitcoin holders permissionless access to DeFi and the
           expanding web3 universe.
         </BodyMd>
-        <BodyMd>
-          tBTC v2 is permissionless, making it open and accessible to all.
-        </BodyMd>
-      </Stack>
+      </Box>
+
+      <Image src={tbtcAppImg} />
     </Card>
   )
 }
