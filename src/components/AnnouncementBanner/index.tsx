ddd47f66f6b9c10f6ff31c41956242c86733c27f
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-20 10:51:52 +0100

    Refactor `AnnouncementBanner` component
    Extract the common parts to separate components to make this component
    more flexible in terms of the layout inside the banner.

--- src/components/AnnouncementBanner/index.tsx
+++ src/components/AnnouncementBanner/index.tsx
@@ -9,60 +9,109 @@ import {
   Image,
   BoxProps,
   useMultiStyleConfig,
+  StylesProvider,
+  useStyles,
 } from "@threshold-network/components"
 import ButtonLink from "../ButtonLink"
 
-interface AnnouncementBannerProps {
-  imgSrc: any
-  preTitle?: string
-  title: string
-  buttonText: string
-  href: string
+type AnnouncementBannerContainerProps = {
   variant?: "primary" | "secondary"
   size?: "sm" | "lg"
   hideCloseBtn?: boolean
+} & BoxProps
+
+type AnnouncementBannerTitleProps = {
+  preTitle?: string
+  title: string
 }
 
-const AnnouncementBanner: FC<AnnouncementBannerProps & BoxProps> = ({
+type AnnouncementBannerBodyProps = {
+  imgSrc: any
+  buttonText: string
+  href: string
+} & AnnouncementBannerTitleProps
+
+type AnnouncementBannerProps = AnnouncementBannerContainerProps &
+  AnnouncementBannerBodyProps
+
+export const AnnouncementBannerContainer: FC<
+  AnnouncementBannerContainerProps
+> = ({ size, variant, hideCloseBtn, children, ...props }) => {
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true })
+
+  const styles = useMultiStyleConfig("AnnouncementBanner", {
+    size,
+    variant,
+  })
+
+  return isOpen ? (
+    <Card sx={styles.container} {...props}>
+      {!hideCloseBtn && (
+        <CloseButton sx={styles.closeButton} onClick={onClose} />
+      )}
+      <StylesProvider value={styles}>{children}</StylesProvider>
+    </Card>
+  ) : null
+}
+
+export const AnnouncementBannerTitle: FC<AnnouncementBannerTitleProps> = ({
+  preTitle,
+  title,
+}) => {
+  const styles = useStyles()
+
+  return (
+    <>
+      {preTitle && <LabelMd sx={styles.preTitle}>{preTitle}</LabelMd>}
+      <H4 sx={styles.title}>{title}</H4>
+    </>
+  )
+}
+
+const AnnouncementBannerBody: FC<AnnouncementBannerBodyProps> = ({
+  preTitle,
+  imgSrc,
+  title,
+  href,
+  buttonText,
+}) => {
+  const styles = useStyles()
+
+  return (
+    <Stack
+      direction={{ base: "column", xl: "row" }}
+      spacing={{ base: "8", xl: "16" }}
+      sx={styles.subContainer}
+    >
+      <Image src={imgSrc} sx={styles.image} />
+      <Stack>
+        <AnnouncementBannerTitle title={title} preTitle={preTitle} />
+      </Stack>
+      <ButtonLink to={href} sx={styles.ctaButton}>
+        {buttonText}
+      </ButtonLink>
+    </Stack>
+  )
+}
+
+const AnnouncementBanner: FC<AnnouncementBannerProps> = ({
   preTitle,
   title,
   imgSrc,
   buttonText,
   href,
-  variant = "primary",
-  size = "sm",
-  hideCloseBtn = false,
   ...props
 }) => {
-  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true })
-
-  const styles = useMultiStyleConfig("AnnouncementBanner", {
-    isOpen,
-    size,
-    variant,
-    ...props,
-  })
-
   return (
-    <Card sx={styles.container} {...props}>
-      {!hideCloseBtn && (
-        <CloseButton sx={styles.closeButton} onClick={onClose} />
-      )}
-      <Stack
-        direction={{ base: "column", xl: "row" }}
-        spacing={{ base: "8", xl: "16" }}
-        sx={styles.subContainer}
-      >
-        <Image maxW={size == "sm" ? "146px" : "280px"} src={imgSrc} />
-        <Stack>
-          {preTitle && <LabelMd sx={styles.preTitle}>{preTitle}</LabelMd>}
-          <H4 sx={styles.title}>{title}</H4>
-        </Stack>
-        <ButtonLink to={href} sx={styles.ctaButton}>
-          {buttonText}
-        </ButtonLink>
-      </Stack>
-    </Card>
+    <AnnouncementBannerContainer {...props}>
+      <AnnouncementBannerBody
+        title={title}
+        preTitle={preTitle}
+        imgSrc={imgSrc}
+        href={href}
+        buttonText={buttonText}
+      />
+    </AnnouncementBannerContainer>
   )
 }
 
