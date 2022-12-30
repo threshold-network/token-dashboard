e93d10c88dd22ab95d018df55ce15ce3fdea0603
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-30 14:09:30 +0100

    Add the new function to the tBTC wrapper
    Add a function that returns the bridge transacion history by depositor
    address. The deposit(in optimistic minting flow) can have 3 statuses:
    - `PENDING`- deposit revealed and waiting for finalizing minting,
    - `MINTED`- the minter minted a deposit and a depositor received tBTC
    token,
    - `ERROR`- the guardian cancelled the optimistic minting request for
    some reason.

--- src/threshold-ts/tbtc/tbtc.ts
+++ src/threshold-ts/tbtc/tbtc.ts
@@ -10,21 +10,38 @@ import {
   Network,
   validate as isValidBtcAddress,
 } from "bitcoin-address-validation"
-import { getProviderOrSigner, unprefixedAndUncheckedAddress } from "../utils"
+import { Contract } from "ethers"
+import {
+  getContract,
+  getProviderOrSigner,
+  unprefixedAndUncheckedAddress,
+  getContractPastEvents,
+} from "../utils"
 import {
   computeHash160,
   decodeBitcoinAddress,
   UnspentTransactionOutput,
 } from "@keep-network/tbtc-v2.ts/dist/bitcoin"
-import { ITBTC } from "./tbtc.interface"
+import { BridgeHistoryStatus, BridgeTxHistory, ITBTC } from "./tbtc.interface"
 import { EthereumBridge } from "@keep-network/tbtc-v2.ts"
 import BridgeArtifact from "@keep-network/tbtc-v2/artifacts/Bridge.json"
+import TBTCVault from "@keep-network/tbtc-v2/artifacts/TBTCVault.json"
 import { MockBitcoinClient } from "../../tbtc/mock-bitcoin-client"
 import { BitcoinConfig, EthereumConfig } from "../types"
+
+export interface RevealedDeposit {
+  amount: string
+  walletPublicKeyHash: string
+  fundingTxHash: string
+  fundingOutputIndex: string
+  depositKey: string
+  txHash: string
+}
 
 export class TBTC implements ITBTC {
   private _bridge: EthereumBridge
   private _bitcoinClient: MockBitcoinClient
+  private _vault: Contract
 
   constructor(ethereumConfig: EthereumConfig, bitcoinConfig: BitcoinConfig) {
     this._bridge = new EthereumBridge({
@@ -35,6 +52,12 @@ export class TBTC implements ITBTC {
       ) as any,
     })
     this._bitcoinClient = new MockBitcoinClient()
+    this._vault = getContract(
+      TBTCVault.address,
+      TBTCVault.abi,
+      ethereumConfig.providerOrSigner,
+      ethereumConfig.account
+    )
   }
 
   suggestDepositWallet = async (): Promise<string | undefined> => {
@@ -117,5 +140,70 @@ export class TBTC implements ITBTC {
       this._bitcoinClient,
       this._bridge
     )
+  }
+
+  bridgeTxHistory = async (depositor: string): Promise<BridgeTxHistory[]> => {
+    // We can assume that all revealed deposits has `PENDING` status.
+    const revealedDeposits = await this._findAllRevealedDeposits(depositor)
+    const depositKeys = revealedDeposits.map((_) => _.depositKey)
+
+    const mintedDeposits = new Map(
+      (await this._findAllMintedDeposits(depositor, depositKeys)).map((_) => [
+        _.args?.depositKey,
+        { txHash: _.transactionHash },
+      ])
+    )
+
+    const cancelledDeposits = new Map(
+      (await this._findAllCancelledDeposits(depositKeys)).map((_) => [
+        _.args?.depositKey,
+        { txHash: _.transactionHash },
+      ])
+    )
+
+    return revealedDeposits.map((deposit) => {
+      const { depositKey, amount, txHash: depositTxHash } = deposit
+      let status = BridgeHistoryStatus.PENDING
+      let txHash = depositTxHash
+      if (mintedDeposits.has(depositKey)) {
+        status = BridgeHistoryStatus.MINTED
+        txHash = mintedDeposits.get(depositKey)?.txHash!
+      } else if (cancelledDeposits.has(depositKey)) {
+        status = BridgeHistoryStatus.ERROR
+        txHash = cancelledDeposits.get(depositKey)?.txHash!
+      }
+
+      return { amount, txHash, status }
+    })
+  }
+
+  private _findAllRevealedDeposits = async (
+    depositor: string
+  ): Promise<RevealedDeposit[]> => {
+    // TODO: probably add a new function to the Ethereum Bridge implementation
+    // to find all revealed deposits because we don't have access to the
+    // `Contract`(from ethers lib) instance.
+    return []
+  }
+
+  private _findAllMintedDeposits = async (
+    depositor: string,
+    depositKeys: string[] = []
+  ): Promise<ReturnType<typeof getContractPastEvents>> => {
+    return await getContractPastEvents(this._vault, {
+      fromBlock: TBTCVault.receipt.blockNumber,
+      filterParams: [null, depositKeys, depositor],
+      eventName: "OptimisticMintingFinalized",
+    })
+  }
+
+  private _findAllCancelledDeposits = async (
+    depositKeys: string[]
+  ): Promise<ReturnType<typeof getContractPastEvents>> => {
+    return getContractPastEvents(this._vault, {
+      fromBlock: TBTCVault.receipt.blockNumber,
+      filterParams: [null, depositKeys],
+      eventName: "OptimisticMintingCancelled",
+    })
   }
 }
