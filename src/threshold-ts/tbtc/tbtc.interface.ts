256ba5a24c20dd3806cb54cdf090e3cec120ae25
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-30 14:36:51 +0100

    Add a new function to the TBTC interface
    see e93d10c.

--- src/threshold-ts/tbtc/tbtc.interface.ts
+++ src/threshold-ts/tbtc/tbtc.interface.ts
@@ -3,6 +3,18 @@ import { UnspentTransactionOutput } from "@keep-network/tbtc-v2.ts/dist/bitcoin"
 import { DepositScriptParameters } from "@keep-network/tbtc-v2.ts/dist/deposit"
 import { MockBitcoinClient } from "../../tbtc/mock-bitcoin-client"
 
+export enum BridgeHistoryStatus {
+  PENDING,
+  MINTED,
+  ERROR,
+}
+
+export interface BridgeTxHistory {
+  status: BridgeHistoryStatus
+  txHash: string
+  amount: string
+}
+
 export interface ITBTC {
   suggestDepositWallet(): Promise<string | undefined>
 
@@ -24,4 +36,7 @@ export interface ITBTC {
     utxo: UnspentTransactionOutput,
     deposit: DepositScriptParameters
   ): Promise<string>
+
+  // TODO: figure out a better name?
+  bridgeTxHistory(depositor: string): Promise<BridgeTxHistory[]>
 }
