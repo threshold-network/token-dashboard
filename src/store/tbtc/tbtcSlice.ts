6cc21fe356c9467836e47a8499fc1f5f5ba83ff2
Rafal Czajkowski <rafal.czajkowski@keep.network>
2022-12-30 14:23:00 +0100

    Fetch and store bridge tx history on redux side

--- src/store/tbtc/tbtcSlice.ts
+++ src/store/tbtc/tbtcSlice.ts
@@ -8,6 +8,11 @@ import {
   UnmintingStep,
 } from "../../types/tbtc"
 import { UpdateStateActionPayload } from "../../types/state"
+import { FetchingState } from "../../types"
+import { BridgeTxHistory } from "../../threshold-ts/tbtc"
+import { featureFlags } from "../../constants"
+import { startAppListening } from "../listener"
+import { fetchBridgeTxHitoryEffect } from "./effects"
 
 interface TbtcState {
   mintingType: TbtcMintingType
@@ -33,6 +38,8 @@ interface TbtcState {
   thresholdNetworkFee: number
   bitcoinMinerFee: number
   isLoadingBitcoinMinerFee: boolean
+
+  transactionsHistory: FetchingState<BridgeTxHistory[]>
 }
 
 export const tbtcSlice = createSlice({
@@ -40,6 +47,11 @@ export const tbtcSlice = createSlice({
   initialState: {
     mintingType: TbtcMintingType.mint,
     mintingStep: MintingSteps[0],
+    transactionsHistory: {
+      isFetching: false,
+      error: "",
+      data: [] as BridgeTxHistory[],
+    },
   } as TbtcState,
   reducers: {
     updateState: (
@@ -49,7 +61,42 @@ export const tbtcSlice = createSlice({
       // @ts-ignore
       state[action.payload.key] = action.payload.value
     },
+    requestBridgeTransactionHistory: (
+      state,
+      action: PayloadAction<{ depositor: string }>
+    ) => {},
+    fetchingBridgeTransactionHistory: (state) => {
+      state.transactionsHistory.isFetching = true
+    },
+    bridgeTransactionHistoryFetched: (
+      state,
+      action: PayloadAction<BridgeTxHistory[]>
+    ) => {
+      state.transactionsHistory.isFetching = false
+      state.transactionsHistory.error = ""
+      state.transactionsHistory.data = action.payload
+    },
+    bridgeTransactionHistoryFailed: (
+      state,
+      action: PayloadAction<{ error: string }>
+    ) => {
+      state.transactionsHistory.isFetching = false
+      state.transactionsHistory.error = action.payload.error
+    },
   },
 })
 
 export const { updateState } = tbtcSlice.actions
+
+const registerTBTCListeners = () => {
+  if (!featureFlags.TBTC_V2) return
+
+  startAppListening({
+    actionCreator: tbtcSlice.actions.requestBridgeTransactionHistory,
+    effect: fetchBridgeTxHitoryEffect,
+  })
+}
+
+// TODO: Move to the `../listener` file once we merge
+// https://github.com/threshold-network/token-dashboard/pull/302.
+registerTBTCListeners()
