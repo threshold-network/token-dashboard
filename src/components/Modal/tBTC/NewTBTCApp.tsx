5cd86bc5f49217559ca29ddb31c17a63aec0d771
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-01-16 08:45:16 +0100

    Remove the close btn from the new tBTC dapp modal
    According to the Figma views.

--- src/components/Modal/tBTC/NewTBTCApp.tsx
+++ src/components/Modal/tBTC/NewTBTCApp.tsx
@@ -24,7 +24,6 @@ const NewTBTCAppBase: FC<BaseModalProps> = ({ closeModal }) => {
   return (
     <>
       <ModalHeader>Take note</ModalHeader>
-      <ModalCloseButton />
       <ModalBody>
         <Image
           src={tbtcAppBannerIllustration}
