eeef9d4601430991f273a26953547ffb7d8fbedf
michalsmiarowski <michal.smiarowski@thesis.co>
2023-01-17 11:33:24 +0100

    Simplify utxos reveal check
    Simplify the code that checks if any of the utxos is revealed.

--- src/pages/tBTC/Bridge/MintingCard/MakeDeposit.tsx
+++ src/pages/tBTC/Bridge/MintingCard/MakeDeposit.tsx
@@ -68,15 +68,15 @@ const MakeDepositComponent: FC<{
 
   useEffect(() => {
     const checkIfAnyUtxosAreNotRevealed = async () => {
-      if (utxos && utxos.length > 0) {
-        for (const utxo of utxos) {
-          const revealedDeposit = await threshold.tbtc.getRevealedDeposit(utxo)
-          if (revealedDeposit.revealedAt === 0) {
-            setHasAnyUnrevealedDeposits(true)
-            break
-          }
-        }
-      }
+      if (!utxos) return
+
+      const deposits = await Promise.all(
+        utxos.map(threshold.tbtc.getRevealedDeposit)
+      )
+
+      setHasAnyUnrevealedDeposits(
+        deposits.some((deposit) => deposit.revealedAt === 0)
+      )
     }
     checkIfAnyUtxosAreNotRevealed()
   }, [utxos?.length])
