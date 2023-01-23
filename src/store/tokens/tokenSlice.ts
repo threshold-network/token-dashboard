9699e226afed621d2dddd3546a274776725d7e43
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-01-23 13:02:07 +0100

    Update the tokenSlice
    Set the decimals for the tBTC token to `8` so we can display correctly
    the tBTC balance in readable format. It also fixes calculating the usd
    balance by taking into account token decimals places.

--- src/store/tokens/tokenSlice.ts
+++ src/store/tokens/tokenSlice.ts
@@ -1,9 +1,6 @@
 import numeral from "numeral"
-import axios from "axios"
-import { FixedNumber } from "@ethersproject/bignumber"
-import { formatUnits } from "@ethersproject/units"
-import { PayloadAction } from "@reduxjs/toolkit/dist/createAction"
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { BigNumber } from "ethers"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { CoingeckoID, Token } from "../../enums/token"
 import {
   TokenState,
@@ -23,6 +20,15 @@ export const fetchTokenPriceUSD = createAsyncThunk(
     return { usd, token }
   }
 )
+
+const toUsdBalance = (token: TokenState) => {
+  const amount = BigNumber.from(10)
+    .pow(18 - (token.decimals ?? 18)) // cast all to 18 decimals
+    .mul(BigNumber.from(token.balance))
+    .toString()
+
+  return getUsdBalance(amount, token.usdConversion)
+}
 
 export const tokenSlice = createSlice({
   name: "tokens",
@@ -65,6 +71,7 @@ export const tokenSlice = createSlice({
       balance: 0,
       usdConversion: 0,
       usdBalance: "0",
+      decimals: 8,
     },
   } as Record<Token, TokenState>,
   reducers: {
@@ -80,10 +87,7 @@ export const tokenSlice = createSlice({
     ) => {
       const { token, balance } = action.payload
       state[token].balance = balance
-      state[token].usdBalance = getUsdBalance(
-        state[token].balance,
-        state[token].usdConversion
-      )
+      state[token].usdBalance = toUsdBalance(state[token])
     },
     setTokenConversionRate: (
       state,
@@ -103,10 +107,7 @@ export const tokenSlice = createSlice({
       const { token, usd } = action.payload
 
       state[token].usdConversion = usd
-      state[token].usdBalance = getUsdBalance(
-        state[token].balance,
-        state[token].usdConversion
-      )
+      state[token].usdBalance = toUsdBalance(state[token])
     })
   },
 })
