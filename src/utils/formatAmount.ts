4776114be1b270b5f88d77b7232c6d8acb0064d5
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-01-25 12:16:02 +0100

    Improve `formatTokenAmount` amount function
    We do not want to display the amount with trailing zeros- to fix that
    here we set the default format to `0,00.[0]0`. Also the default rounding
    function of the `numeral` rounds away from zero- this rounding mode
    could be missleading when we display BTC amount eg `1670000 sats` ->
    `0.02` BTC. To fix that we pass `Math.floor` as rounding function.

--- src/utils/formatAmount.ts
+++ src/utils/formatAmount.ts
@@ -2,12 +2,12 @@ import { formatUnits } from "@ethersproject/units"
 import numeral from "numeral"
 
 export const formatNumeral = (number: string | number, format = "0,00.00") => {
-  return numeral(number).format(format)
+  return numeral(number).format(format, Math.floor)
 }
 
 export const formatTokenAmount = (
   rawAmount: number | string,
-  format = "0,00.00",
+  format = "0,00.[0]0",
   decimals = 18
 ) => {
   return formatNumeral(formatUnits(rawAmount, decimals), format)
