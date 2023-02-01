71b85af1014b6fa2488b40551b1f56081a7c33ea
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-02-01 15:22:07 +0100

    Detect if window.ethereum is a Tally Ho
    MM and other extension wallets injects the provider to `window.ethereum`
    so we need to make sure if a selected wallet option is actually a Tally
    Ho.

--- src/web3/connectors/tallyHo.ts
+++ src/web3/connectors/tallyHo.ts
@@ -1,18 +1,52 @@
 import { InjectedConnector } from "@web3-react/injected-connector"
 import { ConnectorUpdate } from "@web3-react/types"
 
-class TallyHo extends InjectedConnector {
-  async isTallyHo(): Promise<boolean> {
-    const provider = await this.getProvider()
+declare global {
+  interface Window {
+    tally: TallyProvider
+  }
+}
 
-    return provider?.isTally as boolean
+interface TallyProvider {
+  isTally: boolean
+}
+
+export class TallyHoNotInstalledError extends Error {
+  constructor() {
+    super("TallyHo not installed.")
+  }
+}
+
+export class TallyHoIsNotDefaultWalletError extends Error {
+  constructor() {
+    super("TallyHo is not the default wallet.")
+  }
+}
+
+class TallyHo extends InjectedConnector {
+  private isTally(provider: unknown): boolean {
+    return (
+      typeof provider === "object" &&
+      provider !== null &&
+      "request" in provider &&
+      "isTally" in provider &&
+      (provider as TallyProvider).isTally === true
+    )
   }
 
   async activate(): Promise<ConnectorUpdate<string | number>> {
+    if (!this.isTally(window.tally)) {
+      throw new TallyHoNotInstalledError()
+    }
+
+    const provider = await this.getProvider()
+
+    if (!this.isTally(provider)) {
+      throw new TallyHoIsNotDefaultWalletError()
+    }
+
     return await super.activate()
   }
 }
 
-const tallyHo = new TallyHo({})
-
-export default tallyHo
+export const tallyHo = new TallyHo({})
