1bb2b1b219518a8a11df50d16dd1816cbd88a009
michalsmiarowski <michal.smiarowski@thesis.co>
2023-03-02 14:20:53 +0100

    Wrap `ExternalPool` with `Card` component

--- src/pages/tBTC/Bridge/MintingCard/ExternalPool.tsx
+++ src/pages/tBTC/Bridge/MintingCard/ExternalPool.tsx
@@ -1,7 +1,7 @@
 import {
   BodySm,
   BodyXs,
-  Button,
+  Card,
   HStack,
   Image,
   Skeleton,
@@ -14,7 +14,7 @@ import {
   Thead,
   Tr,
 } from "@threshold-network/components"
-import { FC } from "react"
+import { ComponentProps, FC } from "react"
 import ButtonLink from "../../../../components/ButtonLink"
 import { useTbtcState } from "../../../../hooks/useTbtcState"
 import TBTCCurvePool from "../../../../static/images/TBTCCurvePool.svg"
@@ -23,75 +23,77 @@ import { formatNumeral } from "../../../../utils/formatAmount"
 import { formatPercentage } from "../../../../utils/percentage"
 import shortenAddress from "../../../../utils/shortenAddress"
 
-export const ExternalPool: FC = () => {
+export const ExternalPool: FC<ComponentProps<typeof Card>> = ({ ...props }) => {
   const { curveTBTCPool } = useTbtcState()
 
   return (
-    <Stack>
-      <HStack justifyContent={"space-between"} mb={4}>
-        <HStack>
-          <Image src={TBTCCurvePool} />
-          <BodySm>TBTC Curve Pool</BodySm>
+    <Card {...props}>
+      <Stack>
+        <HStack justifyContent={"space-between"} mb={4}>
+          <HStack>
+            <Image src={TBTCCurvePool} />
+            <BodySm>TBTC Curve Pool</BodySm>
+          </HStack>
+          <ButtonLink
+            isLoading={!curveTBTCPool}
+            isExternal
+            href={curveTBTCPool ? curveTBTCPool.url : ""}
+          >
+            Deposit in pool
+          </ButtonLink>
         </HStack>
-        <ButtonLink
-          isLoading={!curveTBTCPool}
-          isExternal
-          href={curveTBTCPool ? curveTBTCPool.url : ""}
-        >
-          Deposit in pool
-        </ButtonLink>
-      </HStack>
-      <Skeleton isLoaded={!!curveTBTCPool && !!curveTBTCPool.url}>
-        <TableContainer>
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th>Pool</Th>
-                <Th>APY</Th>
-                <Th>TVL</Th>
-                <Th textAlign={"end"}>Contract</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>
-                  <HStack>
-                    <Image src={TBTCWBTCSBTCPool} />{" "}
-                    <BodyXs>tBTC/WBTC/sBTC</BodyXs>
-                  </HStack>
-                </Td>
-                <Td>
-                  {!!curveTBTCPool &&
-                    !!curveTBTCPool.apy &&
-                    curveTBTCPool.apy.length == 2 && (
-                      <BodyXs>{`${formatPercentage(
-                        curveTBTCPool.apy[0],
-                        2,
-                        false,
-                        true
-                      )} -> ${formatPercentage(
-                        curveTBTCPool.apy[1],
-                        2,
-                        false,
-                        true
-                      )} CRV`}</BodyXs>
+        <Skeleton isLoaded={!!curveTBTCPool && !!curveTBTCPool.url}>
+          <TableContainer>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Pool</Th>
+                  <Th>APY</Th>
+                  <Th>TVL</Th>
+                  <Th textAlign={"end"}>Contract</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>
+                    <HStack>
+                      <Image src={TBTCWBTCSBTCPool} />{" "}
+                      <BodyXs>tBTC/WBTC/sBTC</BodyXs>
+                    </HStack>
+                  </Td>
+                  <Td>
+                    {!!curveTBTCPool &&
+                      !!curveTBTCPool.apy &&
+                      curveTBTCPool.apy.length == 2 && (
+                        <BodyXs>{`${formatPercentage(
+                          curveTBTCPool.apy[0],
+                          2,
+                          false,
+                          true
+                        )} -> ${formatPercentage(
+                          curveTBTCPool.apy[1],
+                          2,
+                          false,
+                          true
+                        )} CRV`}</BodyXs>
+                      )}
+                  </Td>
+                  <Td>
+                    {!!curveTBTCPool && !!curveTBTCPool.tvl && (
+                      <BodyXs>{formatNumeral(curveTBTCPool.tvl)}</BodyXs>
                     )}
-                </Td>
-                <Td>
-                  {!!curveTBTCPool && !!curveTBTCPool.tvl && (
-                    <BodyXs>{formatNumeral(curveTBTCPool.tvl)}</BodyXs>
-                  )}
-                </Td>
-                <Td textAlign={"end"}>
-                  {!!curveTBTCPool && !!curveTBTCPool.address && (
-                    <BodyXs>{shortenAddress(curveTBTCPool.address)}</BodyXs>
-                  )}
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Skeleton>
-    </Stack>
+                  </Td>
+                  <Td textAlign={"end"}>
+                    {!!curveTBTCPool && !!curveTBTCPool.address && (
+                      <BodyXs>{shortenAddress(curveTBTCPool.address)}</BodyXs>
+                    )}
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Skeleton>
+      </Stack>
+    </Card>
   )
 }
