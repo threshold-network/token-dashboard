f9ede2911b10c469b850db6fd74657f41f354f73
michalsmiarowski <michal.smiarowski@thesis.co>
2023-03-09 22:47:18 +0100

    Add beforeBreadcrumb callback to sentry config
    Adds beforeBreadcrumb callback to the sentry config passed to the `init()`
    method. This will improve the messages for `ui.click` and show the button name,
    uri on which the button was clicked and the button class. We need that
    improvement to make the sentry stack trace more readable, because before it only
    displayed the button class which was hard to debug since chakra-ui generate the
    class names for the elements automatically.

--- src/sentry/index.ts
+++ src/sentry/index.ts
@@ -11,6 +11,41 @@ export const init = () => {
     dsn: dsn,
     integrations: [new BrowserTracing()],
     tracesSampleRate: 0.5,
+    beforeBreadcrumb(breadcrumb, hint) {
+      if (breadcrumb.category === "ui.click") {
+        console.log("BREADCRUMB: ", breadcrumb)
+        console.log("HINT: ", hint)
+        const { target } = hint?.event
+        let buttonName: string
+        let buttonURI: string
+        let buttonClass: string
+        if (
+          target?.firstChild?.data &&
+          typeof target?.firstChild?.data === "string"
+        ) {
+          buttonName = target.firstChild.data
+        } else if (target?.ariaLabel && typeof target?.ariaLabel === "string") {
+          buttonName = target.ariaLabel
+        }
+        if (target?.baseURI && typeof target?.baseURI === "string") {
+          buttonURI = target.baseURI
+        }
+        if (target?.className) {
+          buttonClass = target.classList.toString()
+        }
+        const button = {
+          name: buttonName!,
+          url: buttonURI!,
+          class: buttonClass!,
+        }
+        console.log("Clicked button: ")
+        console.log("name: ", button.name)
+        console.log("url: ", button.url)
+        console.log("buttonClass: ", button.class)
+        breadcrumb.message = `Button name: ${button.name}; Url: ${button.url}; Class: ${button.class}`
+      }
+      return breadcrumb
+    },
   })
 }
 
