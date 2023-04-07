1adb87fcdaaee1e5f7a443dd720bb9e2a2aff598
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-04-07 10:12:15 +0200

    Add tBTC user stats card on the overview page
    Display user's tBTC balance and their latest 2 bridge activities. Also
    user can navigate to a tBTC page from this card.

--- src/pages/Overview/Network/index.tsx
+++ src/pages/Overview/Network/index.tsx
@@ -1,15 +1,35 @@
 import { useEffect } from "react"
 import { SimpleGrid, Stack } from "@threshold-network/components"
+import { useWeb3React } from "@web3-react/core"
 import TotalValueLocked from "./TotalValueLocked"
 import StakingOverview from "./StakingOverview"
 import { useFetchTvl } from "../../../hooks/useFetchTvl"
 import { PageComponent } from "../../../types"
 import { TBTCBrdigeStats } from "./tBTCBridgeStats"
 import { useFetchRecentDeposits } from "../../../hooks/tbtc"
+import { TBTCUserStats } from "./tBTCUserStats"
+import { useAppDispatch, useAppSelector } from "../../../hooks/store"
+import { selectBridgeActivity, tbtcSlice } from "../../../store/tbtc"
 
 const Network: PageComponent = () => {
   const [tvlInUSD, fetchtTvlData, tvlInTokenUnits] = useFetchTvl()
   const [deposits, isFetching, error] = useFetchRecentDeposits()
+  const { account } = useWeb3React()
+  const dispatch = useAppDispatch()
+  const bridgeActivity = useAppSelector(selectBridgeActivity)
+  const isBridgeActivityFetching = useAppSelector(
+    (state) => state.tbtc.bridgeActivity.isFetching
+  )
+
+  useEffect(() => {
+    if (!account) return
+
+    dispatch(
+      tbtcSlice.actions.requestBridgeActivity({
+        depositor: account,
+      })
+    )
+  }, [dispatch, account])
 
   useEffect(() => {
     fetchtTvlData()
@@ -23,6 +43,10 @@ const Network: PageComponent = () => {
         deposits={deposits}
       />
       <Stack spacing={4}>
+        <TBTCUserStats
+          bridgeActivity={bridgeActivity}
+          isBridgeActivityFetching={isBridgeActivityFetching}
+        />
         <StakingOverview />
         <TotalValueLocked totalValueLocked={tvlInUSD.total} />
       </Stack>
