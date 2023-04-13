16a87a05806d3438548de5702a0791f34a8851e7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-04-13 15:57:15 +0200

    Wrap resume deposit form with formik
    Add validation for uploaded file. If validation passed we save the data
    in local storage and then redirect user to the `tBTC/mint` page. Then
    the background task redirects a user to a correct step based on the data
    saved in local storage.

--- src/pages/tBTC/Bridge/ResumeDeposit.tsx
+++ src/pages/tBTC/Bridge/ResumeDeposit.tsx
@@ -1,3 +1,4 @@
+import { FC, useEffect } from "react"
 import {
   BodyLg,
   BodyMd,
@@ -5,11 +6,46 @@ import {
   Button,
   FileUploader,
 } from "@threshold-network/components"
+import { useWeb3React } from "@web3-react/core"
+import { FormikErrors, FormikProps, withFormik } from "formik"
+import { DepositScriptParameters } from "@keep-network/tbtc-v2.ts/dist/src/deposit"
+import { useNavigate } from "react-router-dom"
 import { PageComponent } from "../../../types"
 import { TbtcMintingCardTitle } from "./components/TbtcMintingCardTitle"
 import { BridgeContractLink } from "../../../components/tBTC"
+import { useTbtcState } from "../../../hooks/useTbtcState"
+import { MintingStep } from "../../../types/tbtc"
+import { Form } from "../../../components/Forms"
+import { isSameETHAddress, isAddress, parseJsonFile } from "../../../web3/utils"
+import { getErrorsObj } from "../../../utils/forms"
+import { useTBTCDepositDataFromLocalStorage } from "../../../hooks/tbtc"
 
 export const ResumeDepositPage: PageComponent = () => {
+  const { updateState } = useTbtcState()
+  const { account } = useWeb3React()
+  const navigate = useNavigate()
+  const { setDepositDataInLocalStorage } = useTBTCDepositDataFromLocalStorage()
+
+  useEffect(() => {
+    updateState("mintingStep", MintingStep.InitiateMinting)
+  }, [updateState])
+
+  const onSubmit = (values: FormValues) => {
+    const { depositParameters } = values
+
+    setDepositDataInLocalStorage({
+      ethAddress: depositParameters?.depositor.identifierHex!,
+      blindingFactor: depositParameters?.blindingFactor!,
+      btcRecoveryAddress: depositParameters?.btcRecoveryAddress!,
+      walletPublicKeyHash: depositParameters?.walletPublicKeyHash!,
+      refundLocktime: depositParameters?.refundLocktime!,
+      btcDepositAddress: depositParameters?.btcRecoveryAddress!,
+    })
+
+    updateState("mintingStep", MintingStep.ProvideData)
+    navigate("/tBTC/mint")
+  }
+
   return (
     <>
       <TbtcMintingCardTitle />
@@ -23,29 +59,105 @@ export const ResumeDepositPage: PageComponent = () => {
         To resume your minting you need to upload your .JSON file and sign the
         Minting Initiation transaction triggered in the dApp.
       </BodyMd>
-      <FileUploader
-        onFileUpload={() => console.log("file uploaded")}
-        headerHelperText="Require"
-        accept="application/JSON"
-      />
-      <Button
-        size="lg"
-        isFullWidth
-        onClick={() => {
-          console.log(
-            "set data from json file to localstorage and redirect to tBTC mint page"
-          )
-        }}
-        mt="6"
-      >
-        Upload and Resume
-      </Button>
+      <ResumeDepositFormik address={account!} onSubmitForm={onSubmit} />
       <Box as="p" textAlign="center" mt="10">
         <BridgeContractLink />
       </Box>
     </>
   )
 }
+
+const ResumeDepositForm: FC<FormikProps<FormValues>> = (props) => {
+  const { setValues, getFieldMeta, setFieldError, values } = props
+
+  const { error } = getFieldMeta("depositParameters")
+
+  const isError = Boolean(error)
+
+  const onFileUpload = async (file: File | null) => {
+    if (!file) {
+      setValues({ depositParameters: null })
+      return
+    }
+
+    try {
+      const result = (await parseJsonFile(file)) as DepositDetails
+      setValues({ depositParameters: result })
+    } catch (error) {
+      console.error("Unexpected error: ", (error as Error).toString())
+      setFieldError(
+        "depositParameters",
+        "Unexpected error while reading file, try again..."
+      )
+    }
+  }
+
+  return (
+    <Form>
+      <FileUploader
+        onFileUpload={onFileUpload}
+        headerHelperText="Require"
+        accept="text"
+      />
+      {isError && error}
+      <Button
+        size="lg"
+        isFullWidth
+        mt="6"
+        disabled={!values.depositParameters || isError}
+        type="submit"
+      >
+        Upload and Resume
+      </Button>
+    </Form>
+  )
+}
+
+type DepositDetails = DepositScriptParameters & { btcRecoveryAddress: string }
+
+type FormValues = {
+  depositParameters: DepositDetails | null
+}
+
+type ResumeDepositFormikProps = {
+  onSubmitForm: (values: FormValues) => void
+  address: string
+}
+
+const ResumeDepositFormik = withFormik<ResumeDepositFormikProps, FormValues>({
+  mapPropsToValues: () => ({ depositParameters: null }),
+  validate: (values, { address }) => {
+    const errors: FormikErrors<FormValues> = {}
+
+    if (!values.depositParameters) {
+      errors.depositParameters = "Required."
+    } else if (
+      !values.depositParameters?.depositor ||
+      !values.depositParameters?.depositor?.identifierHex ||
+      !isAddress(values.depositParameters?.depositor?.identifierHex) ||
+      !values.depositParameters?.refundLocktime ||
+      !values.depositParameters?.refundPublicKeyHash ||
+      !values.depositParameters?.blindingFactor ||
+      !values.depositParameters?.walletPublicKeyHash ||
+      !values.depositParameters?.btcRecoveryAddress
+    ) {
+      errors.depositParameters = "Invalid JSON. file."
+    } else if (
+      !isSameETHAddress(
+        values.depositParameters?.depositor?.identifierHex,
+        address
+      )
+    ) {
+      errors.depositParameters = "You are not a depositor."
+    }
+
+    return getErrorsObj(errors)
+  },
+  handleSubmit: (values, { props }) => {
+    props.onSubmitForm(values)
+  },
+  displayName: "ResumeDepositForm",
+})(ResumeDepositForm)
 
 ResumeDepositPage.route = {
   path: "continue",
--- src/web3/utils/files.ts
+++ src/web3/utils/files.ts
@@ -18,3 +18,13 @@ export const downloadFile = (
   a.dispatchEvent(clickEvt)
   a.remove()
 }
+
+export async function parseJsonFile(file: File) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader()
+    fileReader.onload = (event) =>
+      resolve(JSON.parse(event?.target?.result?.toString() ?? ""))
+    fileReader.onerror = (error) => reject(error)
+    fileReader.readAsText(file)
+  })
+}
