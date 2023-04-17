b903c8c78e38b3898553b05ec59ca5b20d4b9bc7
michalsmiarowski <michal.smiarowski@thesis.co>
2023-04-17 12:19:40 +0200

    Remove analytics flag
    Remove `isAnalyticsEnabled` flag so that the posthog events are captured by
    default for all users.

--- src/hooks/posthog/usePosthog.ts
+++ src/hooks/posthog/usePosthog.ts
@@ -3,16 +3,13 @@ import { featureFlags } from "../../constants"
 import * as posthog from "../../posthog"
 import { useCapturePageview } from "./useCapturePageview"
 import { useIdentify } from "./useIdentify"
-import { useAnalytics } from "../useAnalytics"
 
 export const usePosthog = () => {
-  const { isAnalyticsEnabled } = useAnalytics()
-
   useEffect(() => {
-    if (featureFlags.POSTHOG && isAnalyticsEnabled) {
+    if (featureFlags.POSTHOG) {
       posthog.init()
     }
-  }, [isAnalyticsEnabled])
+  }, [])
 
   useCapturePageview()
   useIdentify()
