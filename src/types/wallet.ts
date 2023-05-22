c73d4f385f037f63ee5329f9acb2a7083dbf5bea
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-05-22 12:28:37 +0200

    Clean-up `WalletConnectionModalBase` props
    Remove unnecessary `WalletConnectionModalProps`- this type is used only
    to extend the `Props` interface in `WalletConnectionModalBase.tsx`. We
    can achieve the same by adding the `goBack` prop and extending
    `BaseModalProps` which has `closeModal` prop.

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -1,11 +1,6 @@
 import { FC } from "react"
 import { WalletType } from "../enums"
 
-export interface WalletConnectionModalProps {
-  goBack: () => void
-  closeModal: () => void
-}
-
 export interface WalletOption {
   id: WalletType
   icon: FC
