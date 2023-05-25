fde7d2054a71474b45f84c1d141f03de42b17ea7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-05-25 15:33:42 +0200

    Update `BridgeProcessCardSubTitle` component
    Render children so we can render custom subtitle component.

--- src/pages/tBTC/Bridge/components/BridgeProcessCardSubTitle.tsx
+++ src/pages/tBTC/Bridge/components/BridgeProcessCardSubTitle.tsx
@@ -4,15 +4,15 @@ import { BodyLg, Box } from "@threshold-network/components"
 export const BridgeProcessCardSubTitle: FC<
   {
     stepText: string
-    subTitle: string | JSX.Element
+    subTitle?: string
   } & ComponentProps<typeof BodyLg>
-> = ({ stepText, subTitle, ...restProps }) => {
+> = ({ stepText, subTitle, children, ...restProps }) => {
   return (
     <BodyLg mb={4} {...restProps}>
       <Box as="span" fontWeight="bold" color="brand.500">
         {stepText}
-      </Box>{" "}
-      - {subTitle}
+      </Box>
+      {subTitle ? ` - ${subTitle}` : children}
     </BodyLg>
   )
 }
