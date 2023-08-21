b274717d798801163ecb6f5df1a48c984cd191c5
michalsmiarowski <michal.smiarowski@thesis.co>
2023-08-21 13:17:17 +0200

    Change OutlineListItem color for dark mode
    Change OutlineListItem color for dark mode to `gray.700`.

--- src/components/OutlineListItem.tsx
+++ src/components/OutlineListItem.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react"
-import { ListItem, ListItemProps } from "@threshold-network/components"
+import {
+  ListItem,
+  ListItemProps,
+  useColorModeValue,
+} from "@threshold-network/components"
 
 export const OutlineListItem: FC<ListItemProps> = ({ ...props }) => {
   return (
@@ -7,7 +11,7 @@ export const OutlineListItem: FC<ListItemProps> = ({ ...props }) => {
       display="flex"
       justifyContent="space-between"
       alignItems="center"
-      borderColor="gray.100"
+      borderColor={useColorModeValue("gray.100", "gray.700")}
       borderWidth="1px"
       borderStyle="solid"
       borderRadius="6px"
