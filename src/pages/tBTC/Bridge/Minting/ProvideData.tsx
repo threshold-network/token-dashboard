363f759f157a1b221222a56d737a1077dcdbed0f
michalsmiarowski <michal.smiarowski@thesis.co>
2023-08-21 15:09:42 +0200

    Fix dark mode issues for step 1 minting
    - change body text to `gray.300` for dark mode
    - change placeholder text for the input to `gray.500` for dark mode

--- src/pages/tBTC/Bridge/Minting/ProvideData.tsx
+++ src/pages/tBTC/Bridge/Minting/ProvideData.tsx
@@ -1,6 +1,10 @@
 import { FC, Ref, useRef, useState } from "react"
 import { FormikErrors, FormikProps, withFormik } from "formik"
-import { Button, BodyMd } from "@threshold-network/components"
+import {
+  Button,
+  BodyMd,
+  useColorModeValue,
+} from "@threshold-network/components"
 import { useTbtcState } from "../../../../hooks/useTbtcState"
 import { BridgeProcessCardTitle } from "../components/BridgeProcessCardTitle"
 import { BridgeProcessCardSubTitle } from "../components/BridgeProcessCardSubTitle"
@@ -167,7 +171,7 @@ export const ProvideDataComponent: FC<{
         stepText="Step 1"
         subTitle="Generate a Deposit Address"
       />
-      <BodyMd color="gray.500" mb={12}>
+      <BodyMd color={useColorModeValue("gray.500", "gray.300")} mb={12}>
         Based on these two addresses, the system will generate for you a unique
         BTC deposit address. There is no minting limit.
       </BodyMd>
