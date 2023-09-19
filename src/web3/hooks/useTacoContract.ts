68a0d6df4a3c98d4ddfaee4f2b25f5f5627b1f7b
James Campbell <james.campbell@tanti.org.uk>
2023-09-19 12:44:56 +0200

    Add some todos and export the Taco contract const

--- src/web3/hooks/useTacoContract.ts
+++ src/web3/hooks/useTacoContract.ts
@@ -10,19 +10,11 @@ const TACO_ADDRESSESS = {
   // https://etherscan.io/address/0x7E01c9c03FD3737294dbD7630a34845B0F70E5Dd
   [ChainID.Ethereum.valueOf().toString()]:
     AddressZero,
-  // https://goerli.etherscan.io/address/0x829fdCDf6Be747FEA37518fBd83dF70EE371fCf2
-  // As NuCypher hasn't depoyed the `SimplePreApplication` contract on Goerli,
-  // we're using a stub contract.
   [ChainID.Goerli.valueOf().toString()]:
     "0xA7FD8E3A4731FA6C3b9Bb65C21D7082151B38c36",
-  // Set the correct `SimplePREApplication` contract address. If you deployed
-  // the `@threshold-network/solidity-contracts` to your local chain and linked
-  // package using `yarn link @threshold-network/solidity-contracts` you can
-  // find the contract address at
-  // `node_modules/@threshold-network/solidity-contracts/artifacts/SimplePREApplication.json`.
   [ChainID.Localhost.valueOf().toString()]: AddressZero,
 } as Record<string, string>
 
-export const usePREContract = () => {
+export const useTacoContract = () => {
   return useContract(TACO_ADDRESSESS[supportedChainId], TacoApplicationABI)
 }
