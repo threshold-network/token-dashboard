8cc30e512fc0d66b8058be06f659d0d1a171812f
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-09-29 17:22:07 +0200

    Update the `Sidebar` component
    We can render nav items based on the defined pages. Thanks to this we
    can filter out pages that should not be available because we
    enable/disable page by set `isPageEnabled` for each `PageComponent` and
    there is no need to push nav items based on feature flags.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,74 +1,33 @@
-import { NavItemDetail } from "./NavItem"
 import {
   IoHomeSharp,
-  IoLockClosedOutline,
-  IoLockClosedSharp,
-  IoSwapHorizontalSharp,
-  IoChatbubbleEllipsesOutline,
-  IoChatbubbleEllipsesSharp,
+  // IoLockClosedOutline,
+  // IoLockClosedSharp,
+  // IoSwapHorizontalSharp,
+  // IoChatbubbleEllipsesOutline,
+  // IoChatbubbleEllipsesSharp,
 } from "react-icons/all"
-import { useLocation } from "react-router-dom"
-import { useMemo } from "react"
 import DesktopSidebar from "./DesktopSidebar"
 import MobileSidebar from "./MobileSidebar"
 import { IoHomeOutlineSharp } from "../../static/icons/IoHomeOutlineSharp"
-import useUpgradeHref from "../../hooks/useUpgradeHref"
-import { tBTCFill } from "../../static/icons/tBTCFill"
-import { tBTCOutline } from "../../static/icons/tBTCOutline"
-import { featureFlags } from "../../constants"
+// import { tBTCFill } from "../../static/icons/tBTCFill"
+// import { tBTCOutline } from "../../static/icons/tBTCOutline"
+import { pages } from "../../pages"
+
+const sidebarItems = pages
+  .filter((page) => page.route.isPageEnabled)
+  .map((page) => ({
+    text: page.route.title!,
+    // TODO: set correct icons for a given page.
+    activeIcon: IoHomeSharp,
+    passiveIcon: IoHomeOutlineSharp,
+    href: `/${page.route.path}`,
+  }))
 
 const Sidebar = () => {
-  const { pathname } = useLocation()
-
-  const upgradeHref = useUpgradeHref()
-
-  const navItems: NavItemDetail[] = useMemo(() => {
-    const navItems = [
-      {
-        text: "Overview",
-        activeIcon: IoHomeSharp,
-        passiveIcon: IoHomeOutlineSharp,
-        href: "/overview",
-      },
-      {
-        text: "Upgrade",
-        activeIcon: IoSwapHorizontalSharp,
-        passiveIcon: IoSwapHorizontalSharp,
-        href: upgradeHref,
-      },
-      {
-        text: "Staking",
-        activeIcon: IoLockClosedSharp,
-        passiveIcon: IoLockClosedOutline,
-        href: "/staking/how-it-works",
-      },
-    ]
-
-    if (featureFlags.TBTC_V2) {
-      navItems.push({
-        text: "tBTC",
-        activeIcon: tBTCFill,
-        passiveIcon: tBTCOutline,
-        href: "/tBTC",
-      } as NavItemDetail)
-    }
-
-    if (featureFlags.FEEDBACK_MODULE) {
-      navItems.push({
-        text: "Feedback",
-        activeIcon: IoChatbubbleEllipsesSharp,
-        passiveIcon: IoChatbubbleEllipsesOutline,
-        href: "/feedback/usability-survey",
-      })
-    }
-
-    return navItems
-  }, [pathname, upgradeHref])
-
   return (
     <>
-      <DesktopSidebar navItems={navItems} />
-      <MobileSidebar navItems={navItems} />
+      <DesktopSidebar navItems={sidebarItems} />
+      <MobileSidebar navItems={sidebarItems} />
     </>
   )
 }
