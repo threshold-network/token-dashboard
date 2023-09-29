c8a8577cf041c04bb411544ba715c03977d275e0
Kamil Pyszkowski <kamilpyszkowski@gmail.com>
2023-09-29 17:26:38 +0200

    Adjust rendering condition
    Co-authored-by: Michał Śmiarowski <40306834+michalsmiarowski@users.noreply.github.com>

--- src/pages/Staking/AuthorizeStakingApps/index.tsx
+++ src/pages/Staking/AuthorizeStakingApps/index.tsx
@@ -115,8 +115,9 @@ const AuthorizeStakingAppsPage: FC = () => {
   }
 
   const isFullyAuthorized = Object.values(appsAuthData).every(
-    ({ status }) =>
-      status === "authorized" || status === "authorization-not-required"
+    ({ status, percentage }) =>
+      (status === "authorized" && percentage === 100) ||
+      status === "authorization-not-required"
   )
 
   useEffect(() => {
