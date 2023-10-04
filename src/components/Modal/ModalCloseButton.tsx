7c4f4b4b210b87ce96775d8e5956d8fc1341eed4
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-04 13:07:08 +0200

    Refactor "ModaCloseButton" component import

--- src/components/Modal/ModalCloseButton.tsx
+++ src/components/Modal/ModalCloseButton.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react"
-import { ModalCloseButton as _ModalCloseButton } from "@threshold-network/components"
-import { CloseButtonProps } from "@chakra-ui/close-button"
+import {
+  ModalCloseButton as _ModalCloseButton,
+  CloseButtonProps,
+} from "@threshold-network/components"
 
 const ModalCloseButton: FC<CloseButtonProps> = (props) => {
   return <_ModalCloseButton className="ph-no-capture" {...props} />
