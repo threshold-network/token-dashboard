36d20c74289d911ebc0964c4df8b95aae4db4c5f
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-09 14:45:24 +0200

    Refactor app name/label utils
    https://github.com/threshold-network/token-dashboard/pull/636\#discussion_r1350098230

--- src/hooks/staking-applications/useSubscribeToAuthorizationIncreasedEvent.ts
+++ src/hooks/staking-applications/useSubscribeToAuthorizationIncreasedEvent.ts
@@ -1,24 +1,10 @@
-import {
-  stakingApplicationsSlice,
-  StakingAppName,
-} from "../../store/staking-applications"
-import { threshold } from "../../utils/getThresholdLib"
+import { stakingApplicationsSlice } from "../../store/staking-applications"
+import { getStakingAppNameFromAppAddress } from "../../utils/getStakingAppLabel"
 import {
   useSubscribeToContractEvent,
   useTStakingContract,
 } from "../../web3/hooks"
 import { useAppDispatch } from "../store"
-
-const getApplicationName = (address: string) => {
-  const { multiAppStaking } = threshold
-  const namesDictionary = Object.fromEntries(
-    Object.entries(multiAppStaking).map(([name, { address }]) => [
-      address,
-      name === "ecdsa" ? "tbtc" : name,
-    ])
-  )
-  return namesDictionary[address]
-}
 
 export const useSubscribeToAuthorizationIncreasedEvent = () => {
   const contract = useTStakingContract()
@@ -29,7 +15,7 @@ export const useSubscribeToAuthorizationIncreasedEvent = () => {
     "AuthorizationIncreased",
     // @ts-ignore
     async (stakingProvider, application, fromAmount, toAmount) => {
-      const appName = getApplicationName(application)
+      const appName = getStakingAppNameFromAppAddress(application)
 
       dispatch(
         stakingApplicationsSlice.actions.authorizationIncreased({
