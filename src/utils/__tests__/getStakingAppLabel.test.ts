112e081f8296f7a7a3c1a150331c8785721edfee
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-12 11:33:07 +0200

    Rename variables
    rb -> randomBeacon mock variables: CAPS -> camelCase

--- src/utils/__tests__/getStakingAppLabel.test.ts
+++ src/utils/__tests__/getStakingAppLabel.test.ts
@@ -6,42 +6,45 @@ import {
 import { StakingAppName } from "../../store/staking-applications"
 import { getThresholdLib } from "../getThresholdLib"
 
-const MOCK_ADDRESSES: Record<StakingAppName, string> = {
+const mockAddresses: Record<StakingAppName, string> = {
   tbtc: getThresholdLib().multiAppStaking.ecdsa.address,
   randomBeacon: getThresholdLib().multiAppStaking.randomBeacon.address,
 }
-const MOCK_LABELS: Record<StakingAppName, string> = {
+const mockLabels: Record<StakingAppName, string> = {
   tbtc: "tBTC",
   randomBeacon: "Random Beacon",
 }
-const MOCK_APP_NAMES: StakingAppName[] = ["tbtc", "randomBeacon"]
+const mockAppNames: StakingAppName[] = ["tbtc", "randomBeacon"]
 
 describe("Staking app label utils tests", () => {
-  const [tbtcName, rbName] = MOCK_APP_NAMES
-  const tbtcAddress = MOCK_ADDRESSES[tbtcName]
-  const rbAddress = MOCK_ADDRESSES[rbName]
+  const [tbtcName, randomBeaconName] = mockAppNames
+  const tbtcAddress = mockAddresses[tbtcName]
+  const randomBeaconAddress = mockAddresses[randomBeaconName]
 
   it("returns correct app label if app address is given", () => {
     const resultTbtcLabel = getStakingAppLabelFromAppAddress(tbtcAddress)
-    const resultRbLabel = getStakingAppLabelFromAppAddress(rbAddress)
+    const resultRandomBeaconLabel =
+      getStakingAppLabelFromAppAddress(randomBeaconAddress)
 
-    expect(resultTbtcLabel).toBe(MOCK_LABELS[tbtcName])
-    expect(resultRbLabel).toBe(MOCK_LABELS[rbName])
+    expect(resultTbtcLabel).toBe(mockLabels[tbtcName])
+    expect(resultRandomBeaconLabel).toBe(mockLabels[randomBeaconName])
   })
 
   it("returns correct app label if app name is given", () => {
     const resultTbtcLabel = getStakingAppLabelFromAppName(tbtcName)
-    const resultRbLabel = getStakingAppLabelFromAppName(rbName)
+    const resultRandomBeaconLabel =
+      getStakingAppLabelFromAppName(randomBeaconName)
 
-    expect(resultTbtcLabel).toBe(MOCK_LABELS[tbtcName])
-    expect(resultRbLabel).toBe(MOCK_LABELS[rbName])
+    expect(resultTbtcLabel).toBe(mockLabels[tbtcName])
+    expect(resultRandomBeaconLabel).toBe(mockLabels[randomBeaconName])
   })
 
   it("returns correct app name if address is given", () => {
     const resultTbtcName = getStakingAppNameFromAppAddress(tbtcAddress)
-    const resultRbName = getStakingAppNameFromAppAddress(rbAddress)
+    const resultRandomBeaconName =
+      getStakingAppNameFromAppAddress(randomBeaconAddress)
 
     expect(resultTbtcName).toBe(tbtcName)
-    expect(resultRbName).toBe(rbName)
+    expect(resultRandomBeaconName).toBe(randomBeaconName)
   })
 })
