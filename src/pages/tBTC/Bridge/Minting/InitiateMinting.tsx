5d1aa5544cab54bedd4db565fae97289d31d4028
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-20 16:02:05 +0200

    Redesign Step 3
    Redesigned Step 3: integrated modal contents into step, disabled modal,
    removed unused imports.
    NOTE: Transaction details will be updated with Phase II. It's corelated
    with Solana integration. (?)

--- src/pages/tBTC/Bridge/Minting/InitiateMinting.tsx
+++ src/pages/tBTC/Bridge/Minting/InitiateMinting.tsx
@@ -1,20 +1,48 @@
 import { FC, useEffect } from "react"
-import { BodyMd, Button } from "@threshold-network/components"
+import { BodyLg, Button, H5 } from "@threshold-network/components"
 import { BridgeProcessCardTitle } from "../components/BridgeProcessCardTitle"
 import { MintingStep } from "../../../../types/tbtc"
 import { BridgeProcessCardSubTitle } from "../components/BridgeProcessCardSubTitle"
-import { useModal } from "../../../../hooks/useModal"
-import { ModalType } from "../../../../enums"
 import withOnlyConnectedWallet from "../../../../components/withOnlyConnectedWallet"
-import { UnspentTransactionOutput } from "@keep-network/tbtc-v2.ts/dist/src/bitcoin"
+import {
+  decodeBitcoinAddress,
+  UnspentTransactionOutput,
+} from "@keep-network/tbtc-v2.ts/dist/src/bitcoin"
 import { useToast } from "../../../../hooks/useToast"
+import InfoBox from "../../../../components/InfoBox"
+import { InlineTokenBalance } from "../../../../components/TokenBalance"
+import MintingTransactionDetails from "../components/MintingTransactionDetails"
+import { useTbtcState } from "../../../../hooks/useTbtcState"
+import { BigNumber } from "ethers"
+import { useThreshold } from "../../../../contexts/ThresholdContext"
+import { DepositScriptParameters } from "@keep-network/tbtc-v2.ts/dist/src/deposit"
+import { getChainIdentifier } from "../../../../threshold-ts/utils"
+import { useRevealDepositTransaction } from "../../../../hooks/tbtc"
 
 const InitiateMintingComponent: FC<{
   utxo: UnspentTransactionOutput
   onPreviousStepClick: (previosuStep: MintingStep) => void
 }> = ({ utxo, onPreviousStepClick }) => {
-  const { openModal } = useModal()
   const { addToast, removeToast } = useToast("tbtc-bridge-minting")
+  const {
+    tBTCMintAmount,
+    updateState,
+    ethAddress,
+    blindingFactor,
+    walletPublicKeyHash,
+    btcRecoveryAddress,
+    refundLocktime,
+  } = useTbtcState()
+  const threshold = useThreshold()
+
+  const onSuccessfulDepositReveal = () => {
+    updateState("mintingStep", MintingStep.MintingSuccess)
+  }
+
+  const { sendTransaction: revealDeposit } = useRevealDepositTransaction(
+    onSuccessfulDepositReveal
+  )
+  const { value: depositedAmount } = utxo
 
   useEffect(() => {
     removeToast()
@@ -25,8 +53,30 @@ const InitiateMintingComponent: FC<{
     })
   }, [])
 
-  const confirmDespotAndMint = async () => {
-    openModal(ModalType.TbtcMintingConfirmation, { utxo: utxo })
+  useEffect(() => {
+    const getEstimatedDepositFees = async () => {
+      const amount = BigNumber.from(depositedAmount).toString()
+      const { treasuryFee, optimisticMintFee, amountToMint } =
+        await threshold.tbtc.getEstimatedDepositFees(amount)
+
+      updateState("mintingFee", optimisticMintFee)
+      updateState("thresholdNetworkFee", treasuryFee)
+      updateState("tBTCMintAmount", amountToMint)
+    }
+
+    getEstimatedDepositFees()
+  }, [depositedAmount, updateState, threshold])
+
+  const initiateMintTransaction = async () => {
+    const depositScriptParameters: DepositScriptParameters = {
+      depositor: getChainIdentifier(ethAddress),
+      blindingFactor,
+      walletPublicKeyHash: walletPublicKeyHash,
+      refundPublicKeyHash: decodeBitcoinAddress(btcRecoveryAddress),
+      refundLocktime,
+    }
+
+    await revealDeposit(utxo, depositScriptParameters)
   }
 
   return (
@@ -39,14 +89,30 @@ const InitiateMintingComponent: FC<{
         stepText="Step 3"
         subTitle="Initiate minting"
       />
-      <BodyMd color="gray.500" mb={6}>
-        This step requires you to sign a transaction in your Ethereum wallet.
-      </BodyMd>
-      <BodyMd color="gray.500" mb={6}>
-        Your tBTC will arrive in your wallet in around ~3 hours.
-      </BodyMd>
+      <InfoBox variant="modal" mb="6">
+        <H5>
+          You deposited&nbsp;
+          <InlineTokenBalance
+            tokenAmount={depositedAmount.toString()}
+            tokenSymbol="BTC"
+            withSymbol
+          />
+          &nbsp;and will receive&nbsp;
+          <InlineTokenBalance
+            tokenAmount={tBTCMintAmount}
+            tokenSymbol="tBTC"
+            withSymbol
+          />
+        </H5>
+        <BodyLg>
+          Receiving tBTC requires a single transaction on Solana and takes
+          approximately 2 hours. The bridging can be initiated before you get
+          all your Bitcoin deposit confirmations.
+        </BodyLg>
+      </InfoBox>
+      <MintingTransactionDetails />
       <Button
-        onClick={confirmDespotAndMint}
+        onClick={initiateMintTransaction}
         isFullWidth
         data-ph-capture-attribute-button-name={
           "Confirm deposit & mint (Step 2)"
