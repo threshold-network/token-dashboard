890b3e039f8445a66c82a994600b3e938da23f13
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-24 15:47:35 +0200

    Resolve ECPair library issue
    Applied changes proposed in commit d20aee19

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ module.exports = {
     plugins: [
       "@babel/plugin-proposal-nullish-coalescing-operator",
       "@babel/plugin-proposal-optional-chaining",
+      ["@babel/plugin-transform-class-properties", { loose: true }],
     ],
   },
 }
