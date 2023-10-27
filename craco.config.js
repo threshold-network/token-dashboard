f5b5358e531c81e2e4c51ebb00f26ef4fac5de5d
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-10-27 15:54:36 +0200

    Explain babel plugin usage

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,10 @@ module.exports = {
     plugins: [
       "@babel/plugin-proposal-nullish-coalescing-operator",
       "@babel/plugin-proposal-optional-chaining",
+      /*
+       * Installed "@babel/plugin-transform-class-properties" to resolve
+       * ECPair library error used by "@keep-network/tbtc-v2.ts"
+       */
       ["@babel/plugin-transform-class-properties", { loose: true }],
     ],
   },
