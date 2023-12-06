ab30cbbad14e630870207b3650fc1cc7cb99088c
Michalina <michalina.cienciala@akena.co>
2023-12-06 15:35:16 +0100

    Execute tests in CI
    We're adding step in which we execute the tests on the built code. As the tests
    haven't been updated yet after the SDK changes, some are excluded from the
    execution.

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,47 +4,48 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
 
-jest.mock("@keep-network/tbtc-v2.ts/dist/src/deposit", () => ({
-  calculateDepositAddress: jest.fn(),
-  calculateDepositRefundLocktime: jest.fn(),
-  DepositScriptParameters: jest.fn(),
-  revealDeposit: jest.fn(),
-  getRevealedDeposit: jest.fn(),
-}))
+// Commented out because we haven't updated tests after SDK update
+// jest.mock("@keep-network/tbtc-v2.ts/dist/src/deposit", () => ({
+//   calculateDepositAddress: jest.fn(),
+//   calculateDepositRefundLocktime: jest.fn(),
+//   DepositScriptParameters: jest.fn(),
+//   revealDeposit: jest.fn(),
+//   getRevealedDeposit: jest.fn(),
+// }))
 
-jest.mock("@keep-network/tbtc-v2.ts/dist/src/bitcoin", () => ({
-  decodeBitcoinAddress: jest.fn(),
-  TransactionHash: {
-    from: jest.fn().mockReturnValue({
-      reverse: jest
-        .fn()
-        .mockReturnValue(
-          "reversed_9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
-        ),
-      toString: jest
-        .fn()
-        .mockReturnValue(
-          "9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
-        ),
-    }),
-    reverse: jest
-      .fn()
-      .mockReturnValue(
-        "reversed_9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
-      ),
-    toString: jest
-      .fn()
-      .mockReturnValue(
-        "9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
-      ),
-  },
-  computeHash160: jest.fn(),
-}))
+// jest.mock("@keep-network/tbtc-v2.ts/dist/src/bitcoin", () => ({
+//   decodeBitcoinAddress: jest.fn(),
+//   TransactionHash: {
+//     from: jest.fn().mockReturnValue({
+//       reverse: jest
+//         .fn()
+//         .mockReturnValue(
+//           "reversed_9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
+//         ),
+//       toString: jest
+//         .fn()
+//         .mockReturnValue(
+//           "9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
+//         ),
+//     }),
+//     reverse: jest
+//       .fn()
+//       .mockReturnValue(
+//         "reversed_9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
+//       ),
+//     toString: jest
+//       .fn()
+//       .mockReturnValue(
+//         "9eb901fc68f0d9bcaf575f23783b7d30ac5dd8d95f3c83dceaa13dce17de816a"
+//       ),
+//   },
+//   computeHash160: jest.fn(),
+// }))
 
-jest.mock("@keep-network/tbtc-v2.ts/dist/src", () => ({
-  EthereumBridge: jest.fn(),
-  ElectrumClient: jest.fn(),
-  EthereumTBTCToken: jest.fn(),
-}))
+// jest.mock("@keep-network/tbtc-v2.ts/dist/src", () => ({
+//   EthereumBridge: jest.fn(),
+//   ElectrumClient: jest.fn(),
+//   EthereumTBTCToken: jest.fn(),
+// }))
 
 jest.mock("crypto-js")
