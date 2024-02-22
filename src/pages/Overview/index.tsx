413140adda129061de935652bfb4a0b81f6bea84
evandrosaturnino <evandro.saturnino@gmail.com>
2024-02-21 21:59:39 -0300

    Remove no bridge fees banner

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -4,8 +4,6 @@ import thresholdWordMark from "../../static/images/thresholdWordMark.svg"
 import useDocumentTitle from "../../hooks/useDocumentTitle"
 import Network from "./Network"
 import { PageComponent } from "../../types"
-import { featureFlags } from "../../constants"
-import { NoBridgeFeesBanner } from "./NoBridgeFeesBanner"
 
 const Overview: PageComponent = () => {
   useDocumentTitle("Threshold - Overview")
@@ -14,7 +12,6 @@ const Overview: PageComponent = () => {
     <Container maxW={{ base: "2xl", xl: "6xl" }} my={16}>
       <Image src={thresholdWordMark} mb={4} />
       <H1 mb={12}>Overview</H1>
-      {featureFlags.TBTC_V2 && <NoBridgeFeesBanner mb={"4"} />}
       <Outlet />
     </Container>
   )
