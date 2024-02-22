376b04cadcab79fecd8435ea9de9348fdba07a7c
michalsmiarowski <michal.smiarowski@thesis.co>
2024-02-22 14:35:28 +0100

    Create `useCaptureWalletConnected` hook
    Extracts the code from `useSaveConnectedAddressToStore` hook to a separate hook.
    This way it will be easier to get the tbtc balance that we want to attach to
    Wallet Connected event.

---
+++ src/hooks/posthog/useCaptureWalletConnected.ts
@@ -0,0 +1,29 @@
+import { useWeb3React } from "@web3-react/core"
+import { useEffect } from "react"
+import { Token } from "../../enums"
+import { PosthogEvent } from "../../types/posthog"
+import { getWalletTypeFromConnector } from "../../web3/utils/connectors"
+import { useToken } from "../useToken"
+import { useCapture } from "./useCapture"
+
+export const useCaptureWalletConnected = () => {
+  const { account, connector } = useWeb3React()
+  const { balance, loading, isLoadedFromConnectedAccount } = useToken(
+    Token.TBTCV2
+  )
+  const captureWalletConnected = useCapture(PosthogEvent.WalletConnected)
+
+  useEffect(() => {
+    // Capture posthog wallet connected event only if account is connected AND
+    // when TBTCV2 token balance is fetched for this account.
+    if (account && connector && !loading && isLoadedFromConnectedAccount) {
+      const walletType = getWalletTypeFromConnector(connector)
+      const posthogData = {
+        walletType,
+        address: account,
+        tbtcBalance: balance,
+      }
+      captureWalletConnected(posthogData)
+    }
+  }, [account, connector, balance, loading, isLoadedFromConnectedAccount])
+}
--- src/hooks/useSaveConnectedAddressToStore.ts
+++ src/hooks/useSaveConnectedAddressToStore.ts
@@ -2,27 +2,13 @@ import { useWeb3React } from "@web3-react/core"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { walletConnected } from "../store/account"
-import { PosthogEvent } from "../types/posthog"
-import { getWalletTypeFromConnector } from "../web3/utils/connectors"
-import { useCapture } from "./posthog"
 
 export const useSaveConnectedAddressToStore = () => {
-  const { account, connector } = useWeb3React()
+  const { account } = useWeb3React()
   const dispatch = useDispatch()
-  const captureWalletConnected = useCapture(PosthogEvent.WalletConnected)
 
   useEffect(() => {
     const address = account ? account : ""
     dispatch(walletConnected(address))
-
-    // Capture posthog wallet connected event
-    if (account && connector) {
-      const walletType = getWalletTypeFromConnector(connector)
-      const posthogData = {
-        walletType,
-        account,
-      }
-      captureWalletConnected(posthogData)
-    }
-  }, [account, connector])
+  }, [account])
 }
