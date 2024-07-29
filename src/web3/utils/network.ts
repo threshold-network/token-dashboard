0a5d6dd793f43a30490304bd65c280e48f4b6bd3
evandrosaturnino <evandro.saturnino@gmail.com>
2024-07-29 16:50:05 -0300

    refactor chain id validation to make it leaner

--- src/web3/utils/network.ts
+++ src/web3/utils/network.ts
@@ -6,9 +6,4 @@ export const toHex = (value: string | number): string =>
 export const isSameChainId = (
   chainId1: string | number,
   chainId2: string | number
-): boolean => {
-  const chainId1Hex = toHex(chainId1)
-  const chainId2Hex = toHex(chainId2)
-
-  return chainId1Hex.toLowerCase() === chainId2Hex.toLowerCase()
-}
+): boolean => toHex(chainId1) === toHex(chainId2)
