a54ccb5eb9c45fbe1a1696b78a45e0914d0027da
evandrosaturnino <evandro.saturnino@gmail.com>
2024-11-12 10:20:16 -0300

    Fix ledger support

--- src/pages/tBTC/HowItWorks/ContractsCard.tsx
+++ src/pages/tBTC/HowItWorks/ContractsCard.tsx
@@ -3,39 +3,43 @@ import { List } from "@chakra-ui/react"
 import { Card, LabelSm } from "@threshold-network/components"
 import DetailedLinkListItem from "../../../components/DetailedLinkListItem"
 import { useTBTCTokenAddress } from "../../../hooks/useTBTCTokenAddress"
-import createEtherscanLink, {
-  ExplorerDataType,
-} from "../../../utils/createEtherscanLink"
-import { supportedChainId } from "../../../utils/getEnvVariable"
 import { IoLinkSharp } from "react-icons/all"
 import { useTBTCBridgeContractAddress } from "../../../hooks/useTBTCBridgeContractAddress"
+import { useIsActive } from "../../../hooks/useIsActive"
+import { ExplorerDataType } from "../../../networks/enums/networks"
+import { createExplorerLink } from "../../../networks/utils/createExplorerLink"
 
 export const ContractsCard: FC<ComponentProps<typeof Card>> = (props) => {
   const tbtcTokenContractAddress = useTBTCTokenAddress()
   const bridgeContractAddress = useTBTCBridgeContractAddress()
+  const { chainId } = useIsActive()
 
   return (
     <Card {...props}>
       <LabelSm mb={8}>Contracts</LabelSm>
       <List spacing="2">
-        <DetailedLinkListItem
-          icon={IoLinkSharp}
-          title="Token Contract"
-          href={createEtherscanLink(
-            Number(supportedChainId),
-            tbtcTokenContractAddress,
-            ExplorerDataType.ADDRESS
-          )}
-        />
-        <DetailedLinkListItem
-          icon={IoLinkSharp}
-          title="Bridge Contract"
-          href={createEtherscanLink(
-            Number(supportedChainId),
-            bridgeContractAddress,
-            ExplorerDataType.ADDRESS
-          )}
-        />
+        {tbtcTokenContractAddress && (
+          <DetailedLinkListItem
+            icon={IoLinkSharp}
+            title="Token Contract"
+            href={createExplorerLink(
+              chainId,
+              tbtcTokenContractAddress,
+              ExplorerDataType.ADDRESS
+            )}
+          />
+        )}
+        {bridgeContractAddress && (
+          <DetailedLinkListItem
+            icon={IoLinkSharp}
+            title="Bridge Contract"
+            href={createExplorerLink(
+              chainId,
+              bridgeContractAddress,
+              ExplorerDataType.ADDRESS
+            )}
+          />
+        )}
       </List>
     </Card>
   )
