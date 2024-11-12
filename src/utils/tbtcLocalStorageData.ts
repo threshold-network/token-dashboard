a07c8ff6f61bffa7611b249052b0b699e17666a6
evandrosaturnino <evandro.saturnino@gmail.com>
2024-11-12 10:46:49 -0300

    Update local storage logic handling

--- src/utils/tbtcLocalStorageData.ts
+++ src/utils/tbtcLocalStorageData.ts
@@ -1,12 +1,14 @@
 import { writeStorage } from "@rehooks/local-storage"
 
 export type TBTCDepositData = {
+  chainName: string
   ethAddress: string
   blindingFactor: string
   btcRecoveryAddress: string
   walletPublicKeyHash: string
   refundLocktime: string
   btcDepositAddress: string
+  extraData?: string
 }
 
 export type TBTCLocalStorageDepositData = {
@@ -18,7 +20,8 @@ export const key = "tBTCDepositData"
 export function write(
   account: string,
   newDepositData: TBTCDepositData,
-  prevData: TBTCLocalStorageDepositData
+  prevData: TBTCLocalStorageDepositData,
+  chainId?: number | string
 ) {
   if (!account) return
 
@@ -27,17 +30,24 @@ export function write(
     [account]: newDepositData,
   }
 
-  writeStorage<TBTCLocalStorageDepositData>(key, newLocalStorageData)
+  writeStorage<TBTCLocalStorageDepositData>(
+    `${key}-${chainId?.toString()}`,
+    newLocalStorageData
+  )
 }
 
 export function removeDataForAccount(
   account: string,
-  prevData: TBTCLocalStorageDepositData
+  prevData: TBTCLocalStorageDepositData,
+  chainId?: number | string
 ) {
   const newLocalStorageData = {
     ...prevData,
   }
   delete newLocalStorageData[`${account}`]
 
-  writeStorage<TBTCLocalStorageDepositData>(key, newLocalStorageData)
+  writeStorage<TBTCLocalStorageDepositData>(
+    `${key}-${chainId?.toString()}`,
+    newLocalStorageData
+  )
 }
