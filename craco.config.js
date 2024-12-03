8f3cb90987a35b58bf05273655a3c9f5057f2982
evandrosaturnino <evandro.saturnino@gmail.com>
2024-12-03 04:28:02 -0300

    Transition build webpack for better performance

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,41 @@
+const path = require("path")
+
 module.exports = {
-  babel: {
-    plugins: [
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "@babel/plugin-proposal-optional-chaining",
-      /*
-       * Installed "@babel/plugin-transform-class-properties" to resolve
-       * ECPair library error used by "@keep-network/tbtc-v2.ts"
-       */
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-    ],
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.cache = {
+        type: "filesystem",
+      }
+
+      // Find the rule that contains the Babel loader
+      const oneOfRule = webpackConfig.module.rules.find((rule) =>
+        Array.isArray(rule.oneOf)
+      )
+      if (oneOfRule) {
+        const babelLoader = oneOfRule.oneOf.find(
+          (rule) =>
+            rule.loader &&
+            rule.loader.includes("babel-loader") &&
+            rule.options &&
+            rule.options.presets
+        )
+
+        // Include @ledgerhq and @walletconnect packages in the Babel transpilation
+        if (babelLoader) {
+          // Ensure include is an array
+          babelLoader.include = Array.isArray(babelLoader.include)
+            ? babelLoader.include
+            : [babelLoader.include]
+
+          babelLoader.include = babelLoader.include.concat([
+            path.resolve(__dirname, "node_modules/@ledgerhq"),
+            path.resolve(__dirname, "node_modules/@walletconnect"),
+            path.resolve(__dirname, "node_modules/ecpair"),
+          ])
+        }
+      }
+
+      return webpackConfig
+    },
   },
 }
