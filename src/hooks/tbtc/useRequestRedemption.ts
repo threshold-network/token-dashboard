3088914d148ab7426ffcbc1b75f04dd03bce8046
evandrosaturnino <evandro.saturnino@gmail.com>
2025-03-30 20:45:24 -0300

    chore: update the redemption transaction text

--- src/hooks/tbtc/useRequestRedemption.ts
+++ src/hooks/tbtc/useRequestRedemption.ts
@@ -11,7 +11,7 @@ export const useRequestRedemption = (
 ) => {
   const threshold = useThreshold()
   const pendingText =
-    "Finding an active wallet with sufficient funds to proceed with your unmint. Please wait for the transaction prompt, and then sign in your wallet."
+    "Searching for an active wallet with sufficient funds to proceed with your unmint. Please wait for the transaction prompt, and then sign in your wallet."
 
   return useSendTransactionFromFn(
     threshold.tbtc.requestRedemption,
