c26b965e8b7a03edd6abe8ac57a27c5d9570081c
evandrosaturnino <evandro.saturnino@gmail.com>
2025-03-30 20:42:08 -0300

    refactor: update the redemption transaction pending text

--- src/hooks/tbtc/useRequestRedemption.ts
+++ src/hooks/tbtc/useRequestRedemption.ts
@@ -10,10 +10,13 @@ export const useRequestRedemption = (
   onError?: OnErrorCallback
 ) => {
   const threshold = useThreshold()
+  const pendingText =
+    "Finding an active wallet with sufficient funds to proceed with your unmint. Please wait for the transaction prompt, and then sign in your wallet."
 
   return useSendTransactionFromFn(
     threshold.tbtc.requestRedemption,
     onSuccess,
-    onError
+    onError,
+    pendingText
   )
 }
